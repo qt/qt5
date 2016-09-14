78a764ee65b1b380b73e1800bfeda54598aa4c8b
Konstantin Tokarev <annulen@yandex.ru>
2016-09-14 15:57:48 +0300

    Install conan dependencies if there is conanfile.txt for module
    Change-Id: Ie331331f6383444c4329e7a57cb8a8b25e58bd2f
    Reviewed-by: Simon Hausmann <simon.hausmann@qt.io>

--- coin/provisioning/qtci-windows-10-x86/conan.ps1
+++ coin/provisioning/qtci-windows-10-x86/conan.ps1
@@ -1,1 +1,8 @@
 . "$PSScriptRoot\..\common\conan.ps1"
+
+Run-Conan-Install `
+    -ConanfilesDir "$PSScriptRoot\conanfiles" `
+    -BuildinfoDir msvc-2015-x86 `
+    -Arch x86 `
+    -Compiler "Visual Studio" `
+    -CompilerVersion 14
