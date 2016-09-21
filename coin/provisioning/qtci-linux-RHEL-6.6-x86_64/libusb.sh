05369586148955fbba0df6e3adba6c9c7996dd5d
Kari Oikarinen <kari.oikarinen@qt.io>
2016-09-21 09:14:18 +0300

    Provision libusb-1.0 for Linux
    It is a dependency of tqtc-boot2qt/qdb, which will join CI soon.
    
    Change-Id: I3b205877e705f554202ab7171f9d828491b95ac6
    Reviewed-by: Heikki Halmet <heikki.halmet@qt.io>

---
+++ coin/provisioning/qtci-linux-RHEL-6.6-x86_64/libusb.sh
@@ -0,0 +1,3 @@
+#!/bin/env bash
+# Required for tqtc-boot2qt/qdb
+sudo yum install -y libusb1-devel
---
+++ coin/provisioning/qtci-linux-Ubuntu-14.04-x86_64/libusb.sh
@@ -0,0 +1,3 @@
+#!/bin/env bash
+# Required for tqtc-boot2qt/qdb
+sudo apt-get install -y libusb-1.0-0-dev
