d1374893c8d84ae91f43baea4d512739019348cc
vera <vera@conffx.com>
2017-05-02 13:32:35 -0700

    [Update the result folder path] - update the script so that the result will be put in folder with build id.

--- BUILD_QT.bat
+++ BUILD_QT.bat
@@ -1,4 +1,4 @@
-rem The script assumed that perl, jom and python are already set in PATH. The output dir will be Qt
+rem The script assumed that perl, jom and python are already set in PATH. The output dir will be Qt\[BUILD_ID]
 @SETLOCAL
 @ECHO OFF
 SET OUT_DIR=Qt
@@ -15,8 +15,8 @@ SET PATH=%SRC_DIR%\qtrepotools\bin;%PATH%
 @ECHO ON
 git submodule update --init --recursive
 call perl init-repository -f
-mkdir %OUT_DIR%
-pushd %OUT_DIR%
+mkdir %OUT_DIR%\%BUILD_ID%
+pushd %OUT_DIR%\%BUILD_ID%
 call ..\configure -static -prefix %BUILD_ID% -platform %QMAKESPEC% -debug-and-release -confirm-license -opensource -opengl desktop -static -mp -qt-zlib -qt-pcre -qt-freetype -qt-libpng -qt-libjpeg -direct2d -qt-harfbuzz -skip qt3d -skip qtactiveqt -skip qtandroidextras -skip qtcanvas3d -skip qtconnectivity -skip qtdeclarative -skip qtdoc -skip qtenginio -skip qtgraphicaleffects -skip qtimageformats -skip qtlocation -skip qtmacextras -skip qtmultimedia -skip qtquickcontrols -skip qtscript -skip qtsensors -skip qtserialport -skip qtsvg -skip qttools -skip qttranslations -skip qtwayland -skip qtwebchannel -skip qtwebengine -skip qtwebsockets -skip qtwinextras -skip qtx11extras -skip qtxmlpatterns -nomake examples -nomake tools -nomake tests
 
 CALL jom.exe /j 8 install
