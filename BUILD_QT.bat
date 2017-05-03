c1c9a3f54389669672e48b5f5e101275c9edd731
Krunal Patel <krunal.patel@starbreeze.com>
2017-05-03 10:23:45 -0400

    configure path was wrong

--- BUILD_QT.bat
+++ BUILD_QT.bat
@@ -21,7 +21,7 @@ mkdir %OUT_DIR%\%BUILD_ID%
 if !errorlevel! neq 0 exit /b !errorlevel!
 pushd %OUT_DIR%\%BUILD_ID%
 if !errorlevel! neq 0 exit /b !errorlevel!
-call ..\configure -static -prefix %BUILD_ID% -platform %QMAKESPEC% -debug-and-release -confirm-license -opensource -opengl desktop -static -mp -qt-zlib -qt-pcre -qt-freetype -qt-libpng -qt-libjpeg -direct2d -qt-harfbuzz -skip qt3d -skip qtactiveqt -skip qtandroidextras -skip qtcanvas3d -skip qtconnectivity -skip qtdeclarative -skip qtdoc -skip qtenginio -skip qtgraphicaleffects -skip qtimageformats -skip qtlocation -skip qtmacextras -skip qtmultimedia -skip qtquickcontrols -skip qtscript -skip qtsensors -skip qtserialport -skip qtsvg -skip qttools -skip qttranslations -skip qtwayland -skip qtwebchannel -skip qtwebengine -skip qtwebsockets -skip qtwinextras -skip qtx11extras -skip qtxmlpatterns -nomake examples -nomake tools -nomake tests
+call ..\..\configure -static -prefix %BUILD_ID% -platform %QMAKESPEC% -debug-and-release -confirm-license -opensource -opengl desktop -static -mp -qt-zlib -qt-pcre -qt-freetype -qt-libpng -qt-libjpeg -direct2d -qt-harfbuzz -skip qt3d -skip qtactiveqt -skip qtandroidextras -skip qtcanvas3d -skip qtconnectivity -skip qtdeclarative -skip qtdoc -skip qtenginio -skip qtgraphicaleffects -skip qtimageformats -skip qtlocation -skip qtmacextras -skip qtmultimedia -skip qtquickcontrols -skip qtscript -skip qtsensors -skip qtserialport -skip qtsvg -skip qttools -skip qttranslations -skip qtwayland -skip qtwebchannel -skip qtwebengine -skip qtwebsockets -skip qtwinextras -skip qtx11extras -skip qtxmlpatterns -nomake examples -nomake tools -nomake tests
 if !errorlevel! neq 0 exit /b !errorlevel!
 
 CALL jom.exe /j 8 install
