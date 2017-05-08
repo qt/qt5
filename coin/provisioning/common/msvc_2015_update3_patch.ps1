999b5f5dd7ab11edbb66342b8ecac68532b1486e
Simon Hausmann <simon.hausmann@qt.io>
2017-05-08 16:09:17 +0200

    Speed up Visual Studio update installation
    We have a prepared package on the internal server that has everything
    necessary included. This way we do not have to download the actual
    update content from the internet.
    
    Change-Id: Ie4cb3116a4f91a47df5be1c2b2e473d7ddec9a78
    Reviewed-by: Frederik Gladhorn <frederik.gladhorn@qt.io>
    Reviewed-by: Aapo Keskimolo <aapo.keskimolo@qt.io>

--- coin/provisioning/common/msvc_2015_update3_patch.ps1
+++ coin/provisioning/common/msvc_2015_update3_patch.ps1
@@ -30,5 +30,31 @@
 ## $QT_END_LICENSE$
 ##
 #############################################################################
-. "$PSScriptRoot\..\common\msvc_2015_update3_patch.ps1"
+. "$PSScriptRoot\..\common\helpers.ps1"
 
+# Install Cumulative Servicing Release Visual Studio 2015 update 3
+# Original download page: https://msdn.microsoft.com/en-us/library/mt752379.aspx
+
+$version = "2015 update3 (KB3165756)"
+$package = "C:\Windows\Temp\vs14-kb3165756.exe"
+$url_cache = "http://ci-files01-hki.ci.local/input/windows/vs14-kb3165756.exe"
+$url_official = "http://go.microsoft.com/fwlink/?LinkID=816878"
+$sha1 = "6a21d9b291ca75d44baad95e278fdc0d05d84c02"
+$preparedPackage="\\ci-files01-hki.ci.local\provisioning\windows\vs14-kb3165756-update"
+
+if (Test-Path $preparedPackage) {
+    echo "Using prepared package"
+    pushd $preparedPackage
+    $commandLine = "$preparedPackage\vs14-kb3165756.exe"
+} else {
+    echo "Fetching patch for Visual Studio $version..."
+    Download $url_official $url_cache $package
+    Verify-Checksum $package $sha1
+    $commandLine = $package
+}
+echo "Installing patch for Visual Studio $version..."
+. $commandLine /norestart /passive
+
+if ($commandLine.StartsWith("C:\Windows")) {
+    remove-item $package
+}
