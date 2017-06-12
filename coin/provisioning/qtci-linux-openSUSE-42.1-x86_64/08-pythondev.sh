a7658388892bc70c3c5f3a6f6e31639ef5b766d1
Dominik Holland <dominik.holland@pelagicore.com>
2017-06-12 17:19:20 +0200

    Fix the python3 packages on openSUSE 42.1
    Change-Id: Ie445949ac90bf6a9ce7b519bbe3c32d2d1e1a69b
    Reviewed-by: Simon Hausmann <simon.hausmann@qt.io>

--- coin/provisioning/qtci-linux-openSUSE-42.1-x86_64/08-pythondev.sh
+++ coin/provisioning/qtci-linux-openSUSE-42.1-x86_64/08-pythondev.sh
@@ -5,4 +5,4 @@ sudo pkcon -y refresh
 sudo pkcon -y install python-devel python-virtualenv
 
 # install python3
-sudo pkcon -y install python3 python3-pip python3-virtualenv
+sudo pkcon -y install libpython3_4m1_0 python3-base python3 python3-pip python3-virtualenv
