49ccf716dca2a14d21ce6869cdcd64b861c53834
Simon Hausmann <simon.hausmann@qt.io>
2017-07-25 16:42:44 +0200

    Remove unused provisioning scripts
    RHEL 7.2, Ubuntu 15.04, 16.04 and OpenSuSE 42.1 are not used in the Qt
    5.6 branch. Let's remove the scripts to avoid confusion.
    
    Change-Id: I3829ea320ad77ebd25c9cf5bebfeed89f37f44dc
    Reviewed-by: Simo Fält <simo.falt@qt.io>
    Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@qt.io>

--- coin/provisioning/qtci-linux-RHEL-7.2-x86_64/08-pythondev.sh
+++
@@ -1,5 +0,0 @@
-# provides: python development libraries
-# version: provided by default Linux distribution repository
-# needed to build pyside
-sudo subscription-manager refresh
-sudo yum install -y python-devel python-virtualenv
