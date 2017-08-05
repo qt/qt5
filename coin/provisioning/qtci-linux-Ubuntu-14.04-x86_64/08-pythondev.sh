896bb94c7464eb51db342e63b3cb391e3f08dd48
Frederik Gladhorn <frederik.gladhorn@qt.io>
2017-08-05 19:51:30 +0200

    Remove outdated provisioning scripts
    Ubuntu 14.04 and OS X 10.8 are no longer relevant
    
    Change-Id: Ib18dda605501fbbf2a6cac578a1b34d82102fae4
    Reviewed-by: Simon Hausmann <simon.hausmann@qt.io>
    Reviewed-by: Heikki Halmet <heikki.halmet@qt.io>

--- coin/provisioning/qtci-linux-Ubuntu-14.04-x86_64/08-pythondev.sh
+++
@@ -1,6 +0,0 @@
-# provides: python development libraries
-# version: provided by default Linux distribution repository
-# needed to build pyside
-
-sudo apt-get update
-sudo apt-get install -y python-dev
--- coin/provisioning/qtci-linux-Ubuntu-14.04-x86_64/libusb.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/env bash
-# Required for tqtc-boot2qt/qdb
-sudo apt-get install -y libusb-1.0-0-dev
