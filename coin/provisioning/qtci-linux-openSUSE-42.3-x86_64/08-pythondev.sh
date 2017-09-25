0b251bbdc058808c4264cbcc473c08f6c8a90f02
Simo Fält <simo.falt@qt.io>
2017-09-25 10:04:39 +0300

    Provisioning: Install Python3 development libs to OpenSUSE
    Those are required to build Pyside#5.9 branch.
    
    Change-Id: I315c72155815d8b6b0b2c5eee4d3922374262c7a
    Reviewed-by: Dominik Holland <dominik.holland@pelagicore.com>
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>

--- coin/provisioning/qtci-linux-openSUSE-42.3-x86_64/08-pythondev.sh
+++ coin/provisioning/qtci-linux-openSUSE-42.3-x86_64/08-pythondev.sh
@@ -5,4 +5,4 @@ sudo pkcon -y refresh
 sudo pkcon -y install python-devel python-virtualenv
 
 # install python3
-sudo pkcon -y install python3 python3-pip python3-virtualenv
+sudo pkcon -y install python3 python3-pip python3-devel python3-virtualenv
