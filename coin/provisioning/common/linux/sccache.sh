015c86534c00b46625500e38d2558f72a6c60536
Jędrzej Nowacki <jedrzej.nowacki@qt.io>
2017-10-23 17:01:00 +0200

    Enable use of sccache in the CI
    The sccache may significantly reduce build times.
    We need to evaluate real cases to see how much is to gain.
    
    This patch enables sccache only on cmake, exluding one msvc target
    with debug flag -Zi, which creates separate debug symbols.
    Related sccache issue:
      https://github.com/mozilla/sccache/issues/242
    
    Task-number: COIN-130
    Done-with: Aapo Keskimolo <aapo.keskimolo@qt.io>
    Change-Id: Ic1f599c5194bcdee9528a548dabead1097821cd1
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

---
+++ coin/provisioning/common/linux/sccache.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+#############################################################################
+##
+## Copyright (C) 2018 The Qt Company Ltd.
+## Contact: http://www.qt.io/licensing/
+##
+## This file is part of the provisioning scripts of the Qt Toolkit.
+##
+## $QT_BEGIN_LICENSE:LGPL21$
+## Commercial License Usage
+## Licensees holding valid commercial Qt licenses may use this file in
+## accordance with the commercial license agreement provided with the
+## Software or, alternatively, in accordance with the terms contained in
+## a written agreement between you and The Qt Company. For licensing terms
+## and conditions see http://www.qt.io/terms-conditions. For further
+## information use the contact form at http://www.qt.io/contact-us.
+##
+## GNU Lesser General Public License Usage
+## Alternatively, this file may be used under the terms of the GNU Lesser
+## General Public License version 2.1 or version 3 as published by the Free
+## Software Foundation and appearing in the file LICENSE.LGPLv21 and
+## LICENSE.LGPLv3 included in the packaging of this file. Please review the
+## following information to ensure the GNU Lesser General Public License
+## requirements will be met: https://www.gnu.org/licenses/lgpl.html and
+## http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+##
+## As a special exception, The Qt Company gives you certain additional
+## rights. These rights are described in The Qt Company LGPL Exception
+## version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+##
+## $QT_END_LICENSE$
+##
+#############################################################################
+
+set -ex
+
+source "${BASH_SOURCE%/*}/../unix/sccache.sh"
+
+targetArch=x86_64-unknown-linux-musl
+targetVersion=0.2.13-alpha-0
+sha1=0cb28f7408e5246bc71132f27d86301c30e16c74
+installSccache "$targetArch" "$targetVersion" "$sha1"
---
+++ coin/provisioning/qtci-windows-10-x86/100-install-sccache.ps1
@@ -0,0 +1,1 @@
+. "$PSScriptRoot\..\common\windows\install-sccache.ps1" "x86-pc-windows-gnu" "0.2.13-alpha-0" "287f4c3b7db21b72138704b8fe96827e6b1643a8"
