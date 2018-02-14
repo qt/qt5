ae17b63db218401feee67c17ffdcb7863c3c9e14
Tony Sarajärvi <tony.sarajarvi@qt.io>
2018-02-14 15:07:06 +0200

    Add shebang to launchable script
    Change-Id: I62e33b656eee3a17e1dab4476569d9afa0b62a45
    Reviewed-by: Joni Jäntti <joni.jantti@qt.io>
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>

--- coin/provisioning/common/linux/disable_selinux.sh
+++ coin/provisioning/common/linux/disable_selinux.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # We need to disable selinux while we are overwriting some binaries
 # required by it. If this is not done, ICU provisioning will create
 # template that is not booting.
