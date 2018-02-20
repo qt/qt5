5e31866727b4485f07a17295277be889305c4b3b
Joni Jantti <joni.jantti@qt.io>
2018-02-20 14:51:05 +0200

    Provisioning: Win10 - Add Squish for mingw53
    RTA requires Squish for mingw53 on Windows 10 machines for testing.
    
    Task-number: QTQAINFRA-1729
    Change-Id: I42f2c6c62dd004ebc072835340ce24105cfac5e8
    Reviewed-by: Heikki Halmet <heikki.halmet@qt.io>

--- coin/provisioning/common/windows/squishInstall.ps1
+++ coin/provisioning/common/windows/squishInstall.ps1
@@ -107,6 +107,12 @@ DownloadSquishLicence $licensePackage $squishUrl $targetDir
 
 if(($OSVersion -eq "Windows 10 Enterprise") -or ($OSVersion -eq "Windows 8.1 Enterprise"))
 {
+    # Squish for MinGW
+    $squishPackageMingw = "mingw_gcc53_posix_dwarf"
+    echo "Installing $squishPackageMingw"
+    DownloadAndInstallSquish $version $squishBranchUrl $qtBranch win32 $targetDir $squishPackageMingw
+    mv $targetDir\$squishPackageMingw $targetDir\mingw
+
     # Squish for Visual Studio 2015
     $squishPackage = "msvc14"
     $squishPackage64bit = "msvc14_64"
