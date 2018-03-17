677524620d5e8a8af96fa9395e3b3d342f113158
Tony Sarajärvi <tony.sarajarvi@qt.io>
2018-03-17 11:24:17 +0000

    Revert "Provisioning: openSUSE - Set grub timeout to 0"
    This reverts commit 2e329bc68eb5af977dca1982ed608dc058fdf7f9.
    grub-update command isn't found, and after enforcing script
    results, we have a P0 blocker here.
    
    Change-Id: Iaf4b17a91cdc865974a5f4f196bbb80b085a39c2
    Reviewed-by: Heikki Halmet <heikki.halmet@qt.io>

--- coin/provisioning/qtci-linux-openSUSE-42.3-x86_64/01-systemsetup.sh
+++ coin/provisioning/qtci-linux-openSUSE-42.3-x86_64/01-systemsetup.sh
@@ -7,9 +7,6 @@ source $BASEDIR/../common/shared/network_test_server_ip.txt
 source "${BASH_SOURCE%/*}/../common/unix/check_and_set_proxy.sh"
 
 sed -i '$ a\[Daemon\]\nAutolock=false\nLockOnResume=false' ~/.config/kscreenlockerrc
-echo "Set grub timeout to 0"
-sudo sed -i 's|GRUB_TIMEOUT=10|GRUB_TIMEOUT=0|g' /etc/default/grub
-sudo update-grub
 
 echo "Set Network Test Server address to $network_test_server_ip in /etc/hosts"
 echo "$network_test_server_ip    qt-test-server qt-test-server.qt-test-net" | sudo tee -a /etc/hosts
