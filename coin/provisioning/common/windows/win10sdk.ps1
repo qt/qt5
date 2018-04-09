aa238f9770c721cbb9b971889696a5244f839048
Joni Jantti <joni.jantti@qt.io>
2018-04-09 10:49:55 +0300

    Provisioning: Win10_x86 - Windows 10 SDK with debugging tools
    RTA needs debugging tools for Windows 10 x86 testing.
    
    Change-Id: I69cae92a2529a933a018f22c1b200bdc4d2c0aa0
    Reviewed-by: Tony Sarajärvi <tony.sarajarvi@qt.io>
    (cherry picked from commit 081861de184f0e8b5d2b96d3530d048fa49116b8)

---
+++ coin/provisioning/common/windows/win10sdk.ps1
@@ -0,0 +1,50 @@
+############################################################################
+##
+## Copyright (C) 2018 The Qt Company Ltd.
+## Contact: http://www.qt.io/licensing/
+##
+## This file is part of the provisioning scripts of the Qt Toolkit.
+##
+## $QT_BEGIN_LICENSE:LGPL21$
+## Commercial License Usage
+## Licensees holding valid commercial Qt licenses may use this file in
+## accordance with the commercial license agreement provided with the
+## Software or, alternatively, in accordance with the terms contained in
+## a written agreement between you and The Qt Company. For licensing terms
+## and conditions see http://www.qt.io/terms-conditions. For further
+## information use the contact form at http://www.qt.io/contact-us.
+##
+## GNU Lesser General Public License Usage
+## Alternatively, this file may be used under the terms of the GNU Lesser
+## General Public License version 2.1 or version 3 as published by the Free
+## Software Foundation and appearing in the file LICENSE.LGPLv21 and
+## LICENSE.LGPLv3 included in the packaging of this file. Please review the
+## following information to ensure the GNU Lesser General Public License
+## requirements will be met: https://www.gnu.org/licenses/lgpl.html and
+## http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+##
+## As a special exception, The Qt Company gives you certain additional
+## rights. These rights are described in The Qt Company LGPL Exception
+## version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+##
+## $QT_END_LICENSE$
+##
+#############################################################################
+
+. "$PSScriptRoot\helpers.ps1"
+
+# This script will install Windows 10 SDK
+
+$url_cache = "\\ci-files01-hki.ci.local\provisioning\windows\winsdksetup.exe"
+$url_official = "https://download.microsoft.com/download/8/C/3/8C37C5CE-C6B9-4CC8-8B5F-149A9C976035/windowssdk/winsdksetup.exe"
+$package = "C:\Windows\Temp\winsdksetup.exe"
+$sha1 = "db237323f1779fb143e7cdc558e4345e7004489e"
+
+Copy-Item $url_cache $package
+Verify-Checksum $package $sha1
+Run-Executable $package "/features + /q"
+
+Write-Host "Cleaning $package.."
+Remove-Item -Recurse -Force -Path "$package"
+
+Write-Output "Windows 10 SDK = 10.0.16229.91" >> ~\versions.txt
---
+++ coin/provisioning/qtci-windows-10-x86/80-win10sdk.ps1
@@ -0,0 +1,1 @@
+. "$PSScriptRoot\..\common\windows\win10sdk.ps1"
