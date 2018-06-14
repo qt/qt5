f2354735407596f34ef6757bb0b805daabe0e6bb
Remi Arnaud <remi.arnaud@starbreeze.com>
2018-06-14 16:40:01 -0700

    non static build

--- BUILD_QT.bat
+++ BUILD_QT.bat
@@ -5,7 +5,7 @@ rem The script assumed that perl, jom and python are already set in PATH. The ou
 @ECHO OFF
 SET OUT_DIR=Qt
 SET SRC_DIR=.
-SET BUILD_ID=x64-msvc2015-static
+SET BUILD_ID=x64-msvc2015
 SET QMAKESPEC=win32-msvc2015
 
 call "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat" x64
@@ -21,7 +21,7 @@ mkdir %OUT_DIR%\%BUILD_ID%
 if !errorlevel! neq 0 exit /b !errorlevel!
 pushd %OUT_DIR%\%BUILD_ID%
 if !errorlevel! neq 0 exit /b !errorlevel!
-call ..\..\configure -static -prefix %BUILD_ID% -platform %QMAKESPEC% -debug-and-release -confirm-license -opensource -opengl desktop -static -mp -qt-zlib -qt-pcre -qt-freetype -qt-libpng -qt-libjpeg -direct2d -qt-harfbuzz -skip qt3d -skip qtactiveqt -skip qtandroidextras -skip qtcanvas3d -skip qtconnectivity -skip qtdeclarative -skip qtdoc -skip qtenginio -skip qtgraphicaleffects -skip qtimageformats -skip qtlocation -skip qtmacextras -skip qtmultimedia -skip qtquickcontrols -skip qtscript -skip qtsensors -skip qtserialport -skip qtsvg -skip qttools -skip qttranslations -skip qtwayland -skip qtwebchannel -skip qtwebengine -skip qtwebsockets -skip qtx11extras -skip qtxmlpatterns -nomake examples -nomake tools -nomake tests
+call ..\..\configure -prefix %BUILD_ID% -platform %QMAKESPEC% -debug-and-release -confirm-license -opensource -opengl desktop -static -mp -qt-zlib -qt-pcre -qt-freetype -qt-libpng -qt-libjpeg -direct2d -qt-harfbuzz -skip qt3d -skip qtactiveqt -skip qtandroidextras -skip qtcanvas3d -skip qtconnectivity -skip qtdeclarative -skip qtdoc -skip qtenginio -skip qtgraphicaleffects -skip qtimageformats -skip qtlocation -skip qtmacextras -skip qtmultimedia -skip qtquickcontrols -skip qtscript -skip qtsensors -skip qtserialport -skip qtsvg -skip qttools -skip qttranslations -skip qtwayland -skip qtwebchannel -skip qtwebengine -skip qtwebsockets -skip qtx11extras -skip qtxmlpatterns -nomake examples -nomake tools -nomake tests
 if !errorlevel! neq 0 exit /b !errorlevel!
 
 CALL jom.exe /j 8 install
--- ConsolidateArtifacts.bat
+++ ConsolidateArtifacts.bat
@@ -3,23 +3,22 @@ setlocal EnableDelayedExpansion
 echo off
 
 set destination=artifacts
-set staticConfig=static
 set dynamicConfig=dynamic
 set platform=x64
 set msvcVersion=msvc2015
-set lib_path=Qt\%platform%-%msvcVersion%-%staticConfig%
-set static_lib_path=%lib_path%\qtbase\bin\%platform%-%msvcVersion%-%staticConfig%
+set lib_path=Qt\%platform%-%msvcVersion%
+set lib_path=%lib_path%\qtbase\bin\%platform%-%msvcVersion%
 
 if exist "%destination%" rmdir "%destination%" /s/q
 
 echo "Packaging library"
-xcopy "%static_lib_path%\include"  "%destination%\include" /y/s/q/i
+xcopy "%lib_path%\include"  "%destination%\include" /y/s/q/i
 if !errorlevel! neq 0 exit /b !errorlevel!
-xcopy "%static_lib_path%\lib"  "%destination%\lib" /y/s/q/i
+xcopy "%lib_path%\lib"  "%destination%\lib" /y/s/q/i
 if !errorlevel! neq 0 exit /b !errorlevel!
-xcopy "%static_lib_path%\bin"  "%destination%\bin" /y/s/q/i
+xcopy "%lib_path%\bin"  "%destination%\bin" /y/s/q/i
 if !errorlevel! neq 0 exit /b !errorlevel!
-xcopy "%static_lib_path%\plugins"  "%destination%\plugins" /y/s/q/i
+xcopy "%lib_path%\plugins"  "%destination%\plugins" /y/s/q/i
 if !errorlevel! neq 0 exit /b !errorlevel!
 
 REM remove all intermediate files in the original library folder
