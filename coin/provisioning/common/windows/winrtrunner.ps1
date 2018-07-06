c2c2a140919bc50434924ebab4ccebe89a2394b2
Oliver Wolff <oliver.wolff@qt.io>
2018-07-06 07:16:37 +0200

    provisioning: Update winrtrunner
    winrtrunner is from dev. The only change to the previous version is a
    longer timeout for deployment, as CI runs now and then failed, because
    deployment did not finish in time.
    
    Change-Id: I244e68af447db9298d64bb791048032edd3ec1fd
    Reviewed-by: Simon Hausmann <simon.hausmann@qt.io>

--- coin/provisioning/common/windows/winrtrunner.ps1
+++ coin/provisioning/common/windows/winrtrunner.ps1
@@ -1,11 +1,11 @@
 . "$PSScriptRoot\helpers.ps1"
 
 $zip = Get-DownloadLocation "winrtrunner.zip"
-$url = "http://download.qt.io/development_releases/prebuilt/winrtrunner/winrtrunner_2018-05-24.zip"
-$url_cache = "\\ci-files01-hki.intra.qt.io\provisioning\windows\winrtrunner\winrtrunner_2018-05-24.zip"
+$url = "http://download.qt.io/development_releases/prebuilt/winrtrunner/winrtrunner_2018-07-06.zip"
+$url_cache = "\\ci-files01-hki.intra.qt.io\provisioning\windows\winrtrunner\winrtrunner_2018-07-06.zip"
 
 Download $url $url_cache $zip
-Verify-Checksum $zip "b83f2166b5799910a661d1db02771edf94880785"
+Verify-Checksum $zip "93548e8c3fb8fded2474996ef5e0163f489ce8cf"
 Extract-7Zip $zip C:\Utils\winrtrunner
 Remove-Item -Path $zip
 
