b78812f84e08ac1d9c72ecd290d888142a5c489c
Frederik Gladhorn <frederik.gladhorn@qt.io>
2018-10-23 15:03:11 +0200

    Reduce the number of zypper invocations
    Change-Id: Ibb1c76fc4786d19a921c31b0933746f9dfefcde8
    Reviewed-by: Liang Qi <liang.qi@qt.io>

--- coin/provisioning/qtci-linux-openSUSE-42.3-x86_64/02-zypperpackages.sh
+++ coin/provisioning/qtci-linux-openSUSE-42.3-x86_64/02-zypperpackages.sh
@@ -2,68 +2,47 @@
 
 set -ex
 
-sudo zypper -nq install git
-sudo zypper -nq install gcc7
-sudo zypper -nq install gcc7-c++
+sudo zypper -nq install git gcc7 gcc7-c++
 sudo /usr/sbin/update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 1 \
                                      --slave /usr/bin/g++ g++ /usr/bin/g++-7 \
                                      --slave /usr/bin/cc cc /usr/bin/gcc-7 \
                                      --slave /usr/bin/c++ c++ /usr/bin/g++-7
 
-sudo zypper -nq install bison
-sudo zypper -nq install flex
-sudo zypper -nq install gperf
-
-sudo zypper -nq install zlib-devel
-sudo zypper -nq install libudev-devel
-sudo zypper -nq install glib2-devel
-sudo zypper -nq install libopenssl-devel
-sudo zypper -nq install freetype2-devel
-sudo zypper -nq install fontconfig-devel
-sudo zypper -nq install sqlite3-devel
+sudo zypper -nq install bison flex gperf \
+        zlib-devel \
+        libudev-devel \
+        glib2-devel \
+        libopenssl-devel \
+        freetype2-devel \
+        fontconfig-devel \
+        sqlite3-devel \
+        libxkbcommon-devel \
+        libxkbcommon-x11-devel
 
 # EGL support
-sudo zypper -nq install Mesa-libEGL-devel
-sudo zypper -nq install Mesa-libGL-devel
+sudo zypper -nq install Mesa-libEGL-devel Mesa-libGL-devel
 
-# libxkbcommon
-sudo zypper -nq install libxkbcommon-devel
-sudo zypper -nq install libxkbcommon-x11-devel
 
 # Xinput2
 sudo zypper -nq install libXi-devel
 
 # system provided XCB libraries
-sudo zypper -nq install xcb-util-devel
-sudo zypper -nq install xcb-util-image-devel
-sudo zypper -nq install xcb-util-keysyms-devel
-sudo zypper -nq install xcb-util-wm-devel
-sudo zypper -nq install xcb-util-renderutil-devel
+sudo zypper -nq install xcb-util-devel xcb-util-image-devel xcb-util-keysyms-devel \
+         xcb-util-wm-devel xcb-util-renderutil-devel
 
 # ICU
-sudo zypper -nq install libicu-devel
-sudo zypper -nq install libicu52_1
+sudo zypper -nq install libicu-devel libicu52_1
 
 # qtwebengine
-sudo zypper -nq install alsa-devel
-sudo zypper -nq install dbus-1-devel
-sudo zypper -nq install libXcomposite-devel
-sudo zypper -nq install libXcursor-devel
-sudo zypper -nq install libXrandr-devel
-sudo zypper -nq install libXtst-devel
-sudo zypper -nq install mozilla-nspr-devel
-sudo zypper -nq install mozilla-nss-devel
+sudo zypper -nq install alsa-devel dbus-1-devel \
+         libXcomposite-devel libXcursor-devel libXrandr-devel libXtst-devel \
+         mozilla-nspr-devel mozilla-nss-devel
 
 # qtwebkit
-sudo zypper -nq install libxml2-devel
-sudo zypper -nq install libxslt-devel
+sudo zypper -nq install libxml2-devel libxslt-devel
 
-# GStreamer (qtwebkit and qtmultimedia)
-sudo zypper -nq install gstreamer-devel
-sudo zypper -nq install gstreamer-plugins-base-devel
-
-# pulseaudio (qtmultimedia)
-sudo zypper -nq install libpulse-devel
+# GStreamer (qtwebkit and qtmultimedia), pulseaudio (qtmultimedia)
+sudo zypper -nq install gstreamer-devel gstreamer-plugins-base-devel libpulse-devel
 
 # cups
 sudo zypper -nq install cups-devel
