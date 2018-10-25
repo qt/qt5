7974f26b20060fe429f8508752c9110cb53703d6
Heikki Halmet <heikki.halmet@qt.io>
2018-10-25 14:46:04 +0300

    Provisioning: Disable notifications and remove apport
    These can cause flakiness during tests
    
    Task-number: QTQAINFRA-2307
    Change-Id: Ib8b850e3e1fabe3070bbd27785d043984e085cb7
    Reviewed-by: Tony Sarajärvi <tony.sarajarvi@qt.io>

---
+++ coin/provisioning/common/linux/disable-notifications.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo gsettings set org.gnome.desktop.notifications.application:/update-manager/ enable false
+sudo gsettings set org.gnome.desktop.notifications show-banners false
---
+++ coin/provisioning/qtci-linux-Ubuntu-18.04-x86_64/02-remove-apport.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# shellcheck source=../common/linux/remove-apport.sh
+source "${BASH_SOURCE%/*}/../common/linux/remove-apport.sh"
