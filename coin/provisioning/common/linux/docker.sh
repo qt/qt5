959d9bdb67017819328f086636fed4c00c46d0ad
Ryan Chu <ryan.chu@qt.io>
2018-12-05 12:17:34 +0100

    Docker Provisioning: Install Avahi daemon on Ubuntu
    Avahi is a free Zeroconf implementation and provides mDNS service
    discovery. It enables Docker containers to publish their services on a
    local network. For the host systems supporting mDNS technology, they
    will automatically discover the services when running network tests.
    
    However, some of the Linux distributions don't have avahi-daemon
    installation by default (e.g. Ubuntu Server 18.04). To not surprise the
    user, the provisioning script will help to install avahi-daemon package.
    
    Change-Id: Ibb4c278c552c149b15da8cf7918f7f71ffa3ebec
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    Reviewed-by: Jędrzej Nowacki <jedrzej.nowacki@qt.io>

--- coin/provisioning/common/linux/docker.sh
+++ coin/provisioning/common/linux/docker.sh
@@ -48,5 +48,8 @@ sudo docker info
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+# Install Avahi to discover Docker containers in the test network
+sudo apt-get install avahi-daemon -y
+
 # Start testserver provisioning
 source "${BASH_SOURCE%/*}/docker_testserver.sh"
