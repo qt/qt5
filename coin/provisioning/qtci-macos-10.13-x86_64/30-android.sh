608788de2d10c55b0b3d7c11776f368da015a89a
Heikki Halmet <heikki.halmet@qt.io>
2019-01-17 15:24:27 +0200

    Provisioning: Update Android NDK to version r19
    Also let's start using 64-bit Android NDK for Windows
    
    Task-number: QTQAINFRA-2568
    Change-Id: I2d757aa9e43abf5118abad7bf644e17e88a2ca94
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: Tony Sarajärvi <tony.sarajarvi@qt.io>
    (cherry picked from commit c09135cb56e03dd6a09b79f2c6e15932e9433df2)
    Reviewed-by: Jani Heikkinen <jani.heikkinen@qt.io>

--- coin/provisioning/qtci-macos-10.13-x86_64/30-android.sh
+++ coin/provisioning/qtci-macos-10.13-x86_64/30-android.sh
@@ -51,7 +51,7 @@ toolsVersion="r26.1.1"
 # toolsFile dertermines tools version
 toolsFile="sdk-tools-darwin-4333796.zip"
 
-ndkVersion="r18b"
+ndkVersion="r19c"
 ndkFile="android-ndk-$ndkVersion-darwin-x86_64.zip"
 sdkBuildToolsVersion="28.0.3"
 # this is compile sdk version
