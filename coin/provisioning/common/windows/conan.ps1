716640a4770c8e11ab5f6e6ebb8db64868c2679b
Konstantin Tokarev <annulen@yandex.ru>
2019-07-26 18:57:59 +0300

    Provisioning: update Conan and Conan packages
    [*] Update Conan to 1.17.0, adjust to change in command line arguments
    [*] Update Conan packages to up-to-date versions, add libjpeg-turbo and
        packages for 64-bit MinGW
    [*] Rename 03-conan.ps1 to 11-conan.ps1 so that it is installed after
        Python 3 and MinGW
    [*] Add license header to conan.ps1
    
    Change-Id: Ibc451ddc54a7227eb6d3a007a20351e77a46875f
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>

--- coin/provisioning/common/windows/conan.ps1
+++ coin/provisioning/common/windows/conan.ps1
@@ -1,11 +1,46 @@
+#############################################################################
+##
+## Copyright (C) 2019 The Qt Company Ltd.
+## Copyright (C) 2019 Konstantin Tokarev <annulen@yandex.ru>
+## Contact: http://www.qt.io/licensing/
+##
+## This file is part of the provisioning scripts of the Qt Toolkit.
+##
+## $QT_BEGIN_LICENSE:LGPL21$
+## Commercial License Usage
+## Licensees holding valid commercial Qt licenses may use this file in
+## accordance with the commercial license agreement provided with the
+## Software or, alternatively, in accordance with the terms contained in
+## a written agreement between you and The Qt Company. For licensing terms
+## and conditions see http://www.qt.io/terms-conditions. For further
+## information use the contact form at http://www.qt.io/contact-us.
+##
+## GNU Lesser General Public License Usage
+## Alternatively, this file may be used under the terms of the GNU Lesser
+## General Public License version 2.1 or version 3 as published by the Free
+## Software Foundation and appearing in the file LICENSE.LGPLv21 and
+## LICENSE.LGPLv3 included in the packaging of this file. Please review the
+## following information to ensure the GNU Lesser General Public License
+## requirements will be met: https://www.gnu.org/licenses/lgpl.html and
+## http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+##
+## As a special exception, The Qt Company gives you certain additional
+## rights. These rights are described in The Qt Company LGPL Exception
+## version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+##
+## $QT_END_LICENSE$
+##
+#############################################################################
+
 . "$PSScriptRoot\helpers.ps1"
 
-$scriptsPath = "C:\Python27\Scripts"
+$scriptsPath = "C:\Python36\Scripts"
 
-Run-Executable "$scriptsPath\pip.exe" "install --upgrade conan==0.24.0"
+Run-Executable "$scriptsPath\pip3.exe" "install --upgrade conan==1.17.0"
+Write-Output "Conan = 1.17.0" >> ~\versions.txt
 
 # Use Qt Project repository by default
-Run-Executable "$scriptsPath\conan.exe" "remote add qtproject https://api.bintray.com/conan/qtproject/conan --insert"
+Run-Executable "$scriptsPath\conan.exe" "remote add qtproject https://api.bintray.com/conan/qtproject/conan --insert --force"
 
 Set-EnvironmentVariable "CI_CONAN_BUILDINFO_DIR" "C:\Utils\conanbuildinfos"
 
@@ -40,19 +75,20 @@ function Run-Conan-Install
     }
 
     $manifestsDir = "$PSScriptRoot\conan_manifests"
+    $buildinfoRoot = "C:\Utils\conanbuildinfos"
 
     Get-ChildItem -Path "$ConanfilesDir\*.txt" |
     ForEach-Object {
         $conanfile = $_.FullName
-        $outpwd = "C:\Utils\conanbuildinfos\$($BuildinfoDir)\$($_.BaseName)"
+        $outpwd = "$buildinfoRoot\$BuildinfoDir\$($_.BaseName)"
         New-Item $outpwd -Type directory -Force | Out-Null
 
         for ($i = 1; $i -le 5; $i++) {
             try {
                 Push-Location $outpwd
-                Run-Executable "$scriptsPath\conan.exe" "install -f $conanfile --no-imports --verify $manifestsDir", `
+                Run-Executable "$scriptsPath\conan.exe" "install --no-imports --verify $manifestsDir", `
                     '-s', ('compiler="' + $Compiler + '"'), `
-                    "-s os=Windows -s arch=$Arch -s compiler.version=$CompilerVersion $extraArgs"
+                    "-s os=Windows -s arch=$Arch -s compiler.version=$CompilerVersion $extraArgs $conanfile"
                 break;
             } catch {
                 if ($i -eq 5) {
@@ -66,5 +102,3 @@ function Run-Conan-Install
         Copy-Item -Path $conanfile -Destination "$outpwd\conanfile.txt"
     }
 }
-
-Write-Output "Conan = 0.24.0" >> ~\versions.txt
