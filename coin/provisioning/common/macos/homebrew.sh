b97df21bef1d6a9deab1c62eaa1cbb8f477819ad
Dimitrios Apostolou <jimis@qt.io>
2019-08-14 15:24:32 +0200

    Brew update only once
    Change-Id: I6c2650574b6b62ceedfc95a9caab7ca26a6fa10b
    Reviewed-by: Tony Sarajärvi <tony.sarajarvi@qt.io>

--- coin/provisioning/common/macos/homebrew.sh
+++ coin/provisioning/common/macos/homebrew.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 # Will install homebrew package manager for macOS
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" </dev/null
+
+brew update
