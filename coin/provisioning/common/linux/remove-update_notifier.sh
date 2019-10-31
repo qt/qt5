2eab420e807d5fa4e39d4437879b9a35a2cdc88e
Tony Sarajärvi <tony.sarajarvi@qt.io>
2019-10-31 13:32:28 +0000

    Remove more of update notifiers
    Change-Id: I507f584ef82b5f41f6b1cbc22fdd4ff6eff5e911
    Reviewed-by: Heikki Halmet <heikki.halmet@qt.io>

--- coin/provisioning/common/linux/remove-update_notifier.sh
+++ coin/provisioning/common/linux/remove-update_notifier.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo apt -q -y remove update-notifier update-manager
+sudo apt -q -y remove update-notifier update-manager python3-distupgrade python3-update-manager ubuntu-release-upgrader-core update-manager-core
