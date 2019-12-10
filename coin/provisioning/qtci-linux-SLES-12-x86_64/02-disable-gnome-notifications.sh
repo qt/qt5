11ded96d6e04f809d6c4aeecdf0cdfca0594279f
Tony Sarajärvi <tony.sarajarvi@qt.io>
2019-12-10 07:58:39 +0000

    Remove SLES 12 provisioning scripts, as the platforms is already removed
    Change-Id: I52ded9be93b65b74ac4d42434f82a44831e3ec48
    Reviewed-by: Liang Qi <liang.qi@qt.io>

--- coin/provisioning/qtci-linux-SLES-12-x86_64/02-disable-gnome-notifications.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# shellcheck source=../common/linux/disable-notifications.sh
-source "${BASH_SOURCE%/*}/../common/linux/disable-notifications.sh"
--- coin/provisioning/qtci-linux-SLES-12-x86_64/09-install-openssl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-# shellcheck source=../common/unix/install-openssl.sh
-source "${BASH_SOURCE%/*}/../common/unix/install-openssl.sh" "linux"
