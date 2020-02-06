c0033df4b98eee349fcd7b92f35cb6ee4ca18927
Dimitrios Apostolou <jimis@qt.io>
2020-02-06 19:45:36 +0100

    Execute a specific version of homebrew install script
    Also first try to download it from local mirror.
    
    This install script appears to be doing a lot of git-cloning and downloading
    itself though, so it is still far from the ideal solution: to only download
    from locally cached endpoints.
    
    Task-number: QTQAINFRA-3134
    Change-Id: I32131045d6e62f302ec666b4ac055ca5ee9662d5
    Reviewed-by: Heikki Halmet <heikki.halmet@qt.io>

--- coin/provisioning/common/macos/homebrew.sh
+++ coin/provisioning/common/macos/homebrew.sh
@@ -1,12 +1,21 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 
 # Will install homebrew package manager for macOS.
 #     WARNING: Requires commandlinetools
 
-# TODO audit and cache this file locally, see QTQAINFRA-3134
-curl -L -o /tmp/homebrew_install  https://raw.githubusercontent.com/Homebrew/install/master/install
 
+set -e
+
+. "$(dirname "$0")"/../../common/unix/DownloadURL.sh
+
+
+DownloadURL  \
+    http://ci-files01-hki.intra.qt.io/input/mac/homebrew-install.c744a716f9845988d01e6e238eee7117b8c366c9.rb  \
+    https://raw.githubusercontent.com/Homebrew/install/c744a716f9845988d01e6e238eee7117b8c366c9/install  \
+    b9782cc0b550229de77b429b56ffce04157e60486ab9df00461ccf3dad565b0a  \
+    /tmp/homebrew_install
 /usr/bin/ruby /tmp/homebrew_install  </dev/null
 
-brew update
+# No need to manually do `brew update`, the homebrew installer script does it.
+### brew update
