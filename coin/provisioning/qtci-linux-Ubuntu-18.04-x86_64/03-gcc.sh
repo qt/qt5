7e126af82ffd02550b71d9eaeeb56b387ea89ab0
Heikki Halmet <heikki.halmet@qt.io>
2020-02-10 12:43:31 +0200

    Provisioning: Update GCC to version 9.2 for Ubuntu 18.04
    GCC bug which breaks Webengine build should be fixed in GCC 9.2
    
    Task-number: QTQAINFRA-3534
    Change-Id: Ie0df7c67e0e96cca5632bcd0e6a3db822fcbffbe
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>
    Reviewed-by: Tony Sarajärvi <tony.sarajarvi@qt.io>

--- coin/provisioning/qtci-linux-Ubuntu-18.04-x86_64/03-gcc.sh
+++ coin/provisioning/qtci-linux-Ubuntu-18.04-x86_64/03-gcc.sh
@@ -5,4 +5,4 @@ set -ex
 # shellcheck source=../common/linux/gcc.sh
 source "${BASH_SOURCE%/*}/../common/linux/gcc.sh"
 
-InstallGCC 9.1.0 50 3953fa0d34a467630088d2a43603f74a36a80468 ded538076858926d361af790d59c1f8440dd29b2
+InstallGCC 9.2.0 50 2b3873263d4d6b09b37102078d80dcd7016b9392 44975966b15bca922ba64efa8ec3257726a79153
