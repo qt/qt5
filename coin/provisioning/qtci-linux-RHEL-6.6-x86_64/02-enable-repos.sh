0f0ff063d2994c4ae2c610b570aed1a18bee2a62
Heikki Halmet <heikki.halmet@qt.io>
2020-04-20 12:29:33 +0300

    Provisioning: Enable RPMS for Redhat 6.6
    Needed RPMS are currently in disabled state in Tier 1 image and those
    need to be enabled.
    
    Task-number: COIN-563
    Change-Id: Ib803be03ad859a180bf941a52deaf0cf00412400
    Reviewed-by: Matti Paaso <matti.paaso@qt.io>

---
+++ coin/provisioning/qtci-linux-RHEL-6.6-x86_64/02-enable-repos.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+#############################################################################
+##
+## Copyright (C) 2020 The Qt Company Ltd.
+## Contact: http://www.qt.io/licensing/
+##
+## This file is part of the provisioning scripts of the Qt Toolkit.
+##
+## $QT_BEGIN_LICENSE:LGPL21$
+## Commercial License Usage
+## Licensees holding valid commercial Qt licenses may use this file in
+## accordance with the commercial license agreement provided with the
+## Software or, alternatively, in accordance with the terms contained in
+## a written agreement between you and The Qt Company. For licensing terms
+## and conditions see http://www.qt.io/terms-conditions. For further
+## information use the contact form at http://www.qt.io/contact-us.
+##
+## GNU Lesser General Public License Usage
+## Alternatively, this file may be used under the terms of the GNU Lesser
+## General Public License version 2.1 or version 3 as published by the Free
+## Software Foundation and appearing in the file LICENSE.LGPLv21 and
+## LICENSE.LGPLv3 included in the packaging of this file. Please review the
+## following information to ensure the GNU Lesser General Public License
+## requirements will be met: https://www.gnu.org/licenses/lgpl.html and
+## http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+##
+## As a special exception, The Qt Company gives you certain additional
+## rights. These rights are described in The Qt Company LGPL Exception
+## version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+##
+## $QT_END_LICENSE$
+##
+#############################################################################
+
+set -ex
+
+sudo yum-config-manager --enable rhel-6-workstation-rpms
+sudo yum-config-manager --enable rhel-6-workstation-optional-rpms
+sudo yum-config-manager --enable rhel-workstation-rhscl-6-rpms
+
+sudo yum clean all
+# As well as this fetching the repository data, we also get a printout of the used repos
+sudo yum repolist
