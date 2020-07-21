bdea3c816415f6c7c1c6d1b2a7e41de70e66d44c
Simo Fält <simo.falt@qt.io>
2020-07-21 08:28:08 +0300

    Provisioning: Fix Python3 install in 32 bit win10
    There is no 32 bit win10 configurations in CI, so we do not verify
    any of the provisioning changes on it.
    
    Pick-to: 5.15
    Change-Id: I1c1e183908ffb09be4e2992ccbd1da7af2640181
    Reviewed-by: Toni Saario <toni.saario@qt.io>

--- coin/provisioning/qtci-windows-10-x86/08-python3.ps1
+++ coin/provisioning/qtci-windows-10-x86/08-python3.ps1
@@ -1,1 +1,8 @@
-. "$PSScriptRoot\..\common\windows\python3.ps1"
+# Parameters:
+#  - Arch 32/64
+#  - installer sha1
+#  - install target dir
+#  - version
+#  - Optional true/false if set as default with PYTHON3/PIP3_PATH variables, default false
+
+. "$PSScriptRoot\..\common\windows\python3.ps1" 32 "76c50b747237a0974126dd8b32ea036dd77b2ad1" "C:\Python36" "3.6.1" $true
