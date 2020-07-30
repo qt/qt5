49825be2dc88a7c5fb4a1ab66d0789429bf72685
Simo Fält <simo.falt@qt.io>
2020-07-30 09:10:58 +0300

    Provisioning: Fix PYTHON3_WHEEL_CACHE pointing to wrong dir
    Earlier we set the env variable point to latest Python install. It
    did work by accident, while the default python was installed as last.
    Now each python install has its own matching ENV variable that can
    be used buildtime. The PYTHON3_WHEEL_CACHE will now point to 64 bit
    default Python3, while the variable is already used in Qt codebase.
    
    Change-Id: I6a67936c06ed0209ef9e903120f2416aaa8eb3ff
    Reviewed-by: Assam Boudjelthia <assam.boudjelthia@qt.io>
    (cherry picked from commit 969acbce6e9b1ac1c270363aafc8b20a1f4ee8ff)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- coin/provisioning/common/windows/python3.ps1
+++ coin/provisioning/common/windows/python3.ps1
@@ -96,7 +96,13 @@ Run-Executable "$install_path\Scripts\pip3.exe" "$pip_args install virtualenv wh
 # Install all needed packages in a special wheel cache directory
 $python3_wheel_dir="$install_path\python3-wheels"
 Run-Executable "$install_path\Scripts\pip3.exe" "$pip_args wheel --wheel-dir $python3_wheel_dir -r $PSScriptRoot\..\shared\requirements.txt"
-Set-EnvironmentVariable "PYTHON3_WHEEL_CACHE" "$python3_wheel_dir"
+
+Set-EnvironmentVariable "PYTHON3_WHEEL_CACHE-$version-$archVer" "$python3_wheel_dir"
+# PYTHON3_WHEEL_CACHE is already in use so we should keep it pointing to 64 bit default
+# wheel cache
+if (($setDefault) -And ($archVer -eq 64)) {
+    Set-EnvironmentVariable "PYTHON3_WHEEL_CACHE" "$python3_wheel_dir"
+}
 
 # Install PyPDF2 for QSR documentation
 Run-Executable "$install_path\Scripts\pip3.exe" "$pip_args install PyPDF2"
