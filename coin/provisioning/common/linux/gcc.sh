163c23f97ccfc23a68b8980e6663a5a5da15301c
Heikki Halmet <heikki.halmet@qt.io>
2020-08-06 12:33:34 +0300

    Provisioning: Upgrade GCC version from 9.2 to 9.3
    Task-number: QTQAINFRA-3858
    Change-Id: I5266162d1c144b117e3a61988f3348f2cd3d3c0a
    Reviewed-by: Heikki Halmet <heikki.halmet@qt.io>
    Reviewed-by: Lars Knoll <lars.knoll@qt.io>
    Reviewed-by: Jani Heikkinen <jani.heikkinen@qt.io>

--- coin/provisioning/common/linux/gcc.sh
+++ coin/provisioning/common/linux/gcc.sh
@@ -1,8 +1,9 @@
+
 #!/usr/bin/env bash
 
 #############################################################################
 ##
-## Copyright (C) 2018 The Qt Company Ltd.
+## Copyright (C) 2020 The Qt Company Ltd.
 ## Contact: http://www.qt.io/licensing/
 ##
 ## This file is part of the provisioning scripts of the Qt Toolkit.
@@ -93,10 +94,10 @@ function InstallGCC() {
                                            --slave /usr/bin/cc cc "$installPrefix/bin/gcc${suffixVersion}" \
                                            --slave /usr/bin/c++ c++ "$installPrefix/bin/g++${suffixVersion}"
     else
-        sudo /usr/bin/update-alternatives --install /usr/bin/gcc gcc "$installPrefix/bin/gcc${suffixVersion}" "$priority"
-        sudo /usr/bin/update-alternatives --install /usr/bin/g++ g++ "$installPrefix/bin/g++${suffixVersion}" "$priority"
-        sudo /usr/bin/update-alternatives --install /usr/bin/cc cc "$installPrefix/bin/gcc${suffixVersion}" "$priority"
-        sudo /usr/bin/update-alternatives --install /usr/bin/c++ c++ "$installPrefix/bin/g++${suffixVersion}" "$priority"
+        sudo /usr/bin/update-alternatives --install /usr/bin/gcc gcc "$installPrefix/bin/gcc-${suffixVersion}" "$priority"
+        sudo /usr/bin/update-alternatives --install /usr/bin/g++ g++ "$installPrefix/bin/g++-${suffixVersion}" "$priority"
+        sudo /usr/bin/update-alternatives --install /usr/bin/cc cc "$installPrefix/bin/gcc-${suffixVersion}" "$priority"
+        sudo /usr/bin/update-alternatives --install /usr/bin/c++ c++ "$installPrefix/bin/g++-${suffixVersion}" "$priority"
     fi
 
     echo "/usr/local/lib64" | sudo tee /etc/ld.so.conf.d/gcc-libraries.conf
--- coin/provisioning/qtci-linux-Ubuntu-18.04-x86_64/03-gcc.sh
+++ coin/provisioning/qtci-linux-Ubuntu-18.04-x86_64/03-gcc.sh
@@ -5,4 +5,4 @@ set -ex
 # shellcheck source=../common/linux/gcc.sh
 source "${BASH_SOURCE%/*}/../common/linux/gcc.sh"
 
-InstallGCC 9.2.0 50 2b3873263d4d6b09b37102078d80dcd7016b9392 44975966b15bca922ba64efa8ec3257726a79153
+InstallGCC 9.3.0 50 526bc0ed135e65366080350d0f991157752223c0 b746688bf045a316fc92c3528138ad10d0822b6b
