268fda09d8fc7328fa0a6e4f6679ef664b1b6cad
Heikki Halmet <heikki.halmet@qt.io>
2020-09-11 15:52:06 +0300

    Provisioning: Update MSVC 2015 version number
    Add correct version for Visual Studio 2015 and also
    update the script name to point to MSVC 2015
    
    Change-Id: Ice9b8359fbdcd885227eb94f6ef8cbbb5a02f099
    Reviewed-by: Tony Sarajärvi <tony.sarajarvi@qt.io>

--- coin/provisioning/qtci-windows-10-x86_64/05-msvc2015.ps1
+++ coin/provisioning/qtci-windows-10-x86_64/05-msvc2015.ps1
@@ -34,7 +34,7 @@
 # Visual Studios are pre-provisioned to tier1 images
 
 # MSVC 2015 Update 3
-Write-Output "Visual Studio 2015 = Version 14.0.25431.01 Update 3" >> ~\versions.txt
+Write-Output "Visual Studio 2015 = Version 14.0.25420.1 Update 3" >> ~\versions.txt
 
 # MSVC 2019 and Build Tools are pre-provisioned, but the updating happens with "$PSScriptRoot\..\common\windows\update-msvc2019.ps1"
 
