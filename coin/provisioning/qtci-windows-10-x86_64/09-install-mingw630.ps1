2fb5125e5bd6a92039d087d26e1d3195223f55c9
Tony Sarajärvi <tony.sarajarvi@qt.io>
2020-11-06 07:42:22 +0000

    Clean up unused provisioning scripts
    Change-Id: Id6c5de28bde4a7870eb33d685c948bfcf48cb367
    Reviewed-by: Timur Pocheptsov <timur.pocheptsov@qt.io>
    Reviewed-by: Heikki Halmet <heikki.halmet@qt.io>

--- coin/provisioning/qtci-windows-10-x86_64/09-install-mingw630.ps1
+++
@@ -1,8 +0,0 @@
-. "$PSScriptRoot\..\common\windows\install-mingw.ps1"
-
-# This script will install 64-bit MinGW 6.3.0
-
-$release = "x86_64-6.3.0-release-posix-seh-rt_v5-rev2"
-$sha1    = "49E7F8997E3D15C75B1A4DE1C380ABE1FB9B7533"
-
-InstallMinGW $release $sha1
