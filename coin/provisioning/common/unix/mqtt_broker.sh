9a82a87954fda2e5f6420d346798d9a841e5c810
Maurice Kalinowski <maurice.kalinowski@qt.io>
2020-11-10 22:08:37 +0100

    Update MQTT test broker
    Latest additions of CentOS cause troubles due to
    - Updated SSL to set up tls servers
    - Updated Python version, which removed API calls
    
    For the latter, we only need to update the broker. TLS test servers
    are not used in our setup. However, qtmqtt.git requires on further
    update to load a different configuration.
    
    Task-number: QTBUG-88189
    Change-Id: I2d6e559a21dff41deca94aeec8c5db6fdf72a0ac
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: Karsten Heimrich <karsten.heimrich@qt.io>

--- coin/provisioning/common/unix/mqtt_broker.sh
+++ coin/provisioning/common/unix/mqtt_broker.sh
@@ -39,10 +39,10 @@
 source "${BASH_SOURCE%/*}/InstallFromCompressedFileFromURL.sh"
 source "${BASH_SOURCE%/*}/../unix/SetEnvVar.sh"
 
-CommitSHA="20bad2475c27a6e1d24a56d90a9fceb40963261e"
+CommitSHA="2873885d7e840b4e06483f36f170c609eb30527d"
 PrimaryUrl="http://ci-files01-hki.intra.qt.io/input/mqtt_broker/paho.mqtt.testing-$CommitSHA.zip"
 AltUrl="https://github.com/eclipse/paho.mqtt.testing/archive/$CommitSHA.zip"
-SHA1="a0ac88715c2aebb9573a113dc13925a90da19233"
+SHA1="1fcc4e61b12f11a1421cc8c3f379276d732e62b7"
 targetFolder="/opt/paho_broker"
 appPrefix="paho.mqtt.testing-$CommitSHA"
 
