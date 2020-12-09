5e135cd7a531c163aa78b1944b23eab338526c14
Heikki Halmet <heikki.halmet@qt.io>
2020-12-09 13:53:18 +0200

    Provisioning: Fix GCC installation for linux
    Pre-built package was missing from cache and source package
    was named as pre-built package
    
    Change-Id: I6d14b198ab7e3795e5f70986c9a50662225ac666
    Reviewed-by: Samuli Piippo <samuli.piippo@qt.io>
    (cherry picked from commit 4aa14ea9d7ce4438f2891ddbaf34cea2bdcd559a)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- coin/provisioning/qtci-linux-Ubuntu-20.04-x86_64/03-gcc.sh
+++ coin/provisioning/qtci-linux-Ubuntu-20.04-x86_64/03-gcc.sh
@@ -5,4 +5,4 @@ set -ex
 # shellcheck source=../common/linux/gcc.sh
 source "${BASH_SOURCE%/*}/../common/linux/gcc.sh"
 
-InstallGCC 9.3.0 50 526bc0ed135e65366080350d0f991157752223c0 b746688bf045a316fc92c3528138ad10d0822b6b
+InstallGCC 9.3.0 50 5038e8752407d14e5a70c8efc80c20a6d4219aaa 212f77d7b7fe1fdf01a1c0b0ebc9d82aeda5e1e0
