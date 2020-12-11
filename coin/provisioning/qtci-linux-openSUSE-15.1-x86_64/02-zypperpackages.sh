0a33a4152a232996288039b443df3b5afe78a7f2
Allan Sandfeld Jensen <allan.jensen@qt.io>
2020-12-11 10:33:51 +0100

    Add glproto-devel to openSUSE for webengine
    We now need glxtokens.h
    
    Change-Id: I55bc2d195cb27b53ff0a2573e558e4cd40e67451
    Reviewed-by: Heikki Halmet <heikki.halmet@qt.io>
    (cherry picked from commit d5eeee1cd242d4571ca3a251cb5c681869c1e268)
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- coin/provisioning/qtci-linux-openSUSE-15.1-x86_64/02-zypperpackages.sh
+++ coin/provisioning/qtci-linux-openSUSE-15.1-x86_64/02-zypperpackages.sh
@@ -36,7 +36,7 @@ sudo zypper -nq install libicu-devel libicu60_2
 # qtwebengine
 sudo zypper -nq install alsa-devel dbus-1-devel \
          libXcomposite-devel libXcursor-devel libXrandr-devel libXtst-devel \
-         mozilla-nspr-devel mozilla-nss-devel nodejs10 nodejs10-devel
+         mozilla-nspr-devel mozilla-nss-devel nodejs10 nodejs10-devel glproto-devel
 
 # qtwebkit
 sudo zypper -nq install libxml2-devel libxslt-devel
