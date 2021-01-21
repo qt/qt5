e67fa087dba2292fc9e5d9298c9c6ec73e2a92e9
Heikki Halmet <heikki.halmet@qt.io>
2021-01-21 11:15:35 +0200

    Provisioning: Install Conan package manager
    Conan package manager is need for addtional libraries
    
    Task-number: QTQAINFRA-4190
    Change-Id: I5cb07a137505f15d536989c6d5127f607735a17e
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: Ville-Pekka Karhu <ville-pekka.karhu@qt.io>
    Reviewed-by: Toni Saario <toni.saario@qt.io>

---
+++ coin/provisioning/common/unix/install-conan.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# This script will install Conan
+# Note! Python3 is required for Conan installation
+
+# Install Conan to Python user install directory (typically ~./local/)
+pip3 install conan --user
+
