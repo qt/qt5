a1521e472bcbcbe5369e1e8342f7a96bac1fa375
Heikki Halmet <heikki.halmet@qt.io>
2021-01-24 13:02:44 +0200

    Provisioning: get-pip.py address changed for python 2.7
    For python27 get-pip file's location has been changed and we
    need to use two different get-pip files for different python
    versions.
    Let's also cache these python files and use cache as primary
    url.
    
    Task-number: QTQAINFRA-4210
    Change-Id: Iebc00f6f319bb914f7548d4737e13fed9ac2b292
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: Tony Sarajärvi <tony.sarajarvi@qt.io>
    (cherry picked from commit 6d20944daf78c50b0de03e516293c87959616623)
    Reviewed-by: Simo Fält <simo.falt@qt.io>

--- coin/provisioning/common/macos/pip.sh
+++ coin/provisioning/common/macos/pip.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
+# shellcheck source=../unix/DownloadURL.sh
+source "${BASH_SOURCE%/*}/../unix/DownloadURL.sh"
+
 function InstallPip {
 
     python=$1
 
     # Will install pip utility for python
-    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    if [[ $python == "python2.7" ]]; then
+        DownloadURL "http://ci-files01-hki.intra.qt.io/input/mac/python27/get-pip.py" "https://bootstrap.pypa.io/2.7/get-pip.py" "c4c5f74586cffe49804f167d95d1710b9750ddf0"
+    else
+        DownloadURL "http://ci-files01-hki.intra.qt.io/input/mac/get-pip.py" "https://bootstrap.pypa.io/get-pip.py" "209ddf0bb8d1cf06a1f17dd9f21970c76b3d2be2"
+    fi
     sudo "$python" get-pip.py
     rm get-pip.py
 }
