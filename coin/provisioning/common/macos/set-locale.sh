031031cd2b16affd4fd28789300a76fb24d521dd
Tor Arne Vestbø <tor.arne.vestbo@qt.io>
2021-02-04 19:12:11 +0100

    Set up region and language settings on macOS to match United States
    The tst_qlocale::macDefaultLocale() test was failing on macOS 11 because
    the VM images had been set up with Finland as the region, resulting in
    a different number separator than what the test expected.
    
    Fixes: QTBUG-90696
    Change-Id: Ia2f68adc1401f21721d1786c5279aa0918a311a8
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit 4ad4befe4e08ca726f001318d1ad9d5102259e22)
    Reviewed-by: Tony Sarajärvi <tony.sarajarvi@qt.io>

--- coin/provisioning/common/macos/set-locale.sh
+++ coin/provisioning/common/macos/set-locale.sh
@@ -39,3 +39,10 @@ set -ex
 
 source "${BASH_SOURCE%/*}/../unix/SetEnvVar.sh"
 SetEnvVar "LANG" "en_US.UTF-8"
+
+# The following settings match the "United States" region default
+defaults write -globalDomain AppleLocale "en_US"
+defaults write -globalDomain AppleLanguages "(en)"
+defaults write -globalDomain AppleMeasurementUnits "Inches"
+defaults write -globalDomain AppleTemperatureUnit "Fahrenheit"
+defaults write -globalDomain AppleMetricUnits -bool false
