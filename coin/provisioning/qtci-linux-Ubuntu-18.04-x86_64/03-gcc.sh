8843f5186646722a48e1915cf2844d6d5b7f6466
Heikki Halmet <heikki.halmet@qt.io>
2021-02-25 08:53:08 +0200

    Change GCC version to 8.3.0 for Ubuntu 18.04
    We need to use gcc 8 instead of 9 so that the Qt installer
    will work with CentOS 8.3 which have gcc 8 in the system.
    If we use newer gcc version with the binaries there will be issues
    with libstdc++.
    Using gcc-toolset-9 in CentOS won't help either because it will link
    libstdc++ against the one in system.
    
    Pick-to: 6.1
    Task-number: QTBUG-91352
    Change-Id: I2a3d186bc04b1f54f14ed95f0588d18bd63814c4
    Reviewed-by: Ville Voutilainen <ville.voutilainen@qt.io>

--- coin/provisioning/qtci-linux-Ubuntu-18.04-x86_64/03-gcc.sh
+++ coin/provisioning/qtci-linux-Ubuntu-18.04-x86_64/03-gcc.sh
@@ -5,4 +5,4 @@ set -ex
 # shellcheck source=../common/linux/gcc.sh
 source "${BASH_SOURCE%/*}/../common/linux/gcc.sh"
 
-InstallGCC 9.3.0 50 5038e8752407d14e5a70c8efc80c20a6d4219aaa 212f77d7b7fe1fdf01a1c0b0ebc9d82aeda5e1e0
+InstallGCC 8.3.0 50 ccccfe4fe9206d111a173c19a21f8700d1133ae8 c27f4499dd263fe4fb01bcc5565917f3698583b2
