d1601aa0f94de81fe6b1651a2a3f5ce0a52a1ed3
Toni Saario <toni.saario@qt.io>
2021-03-19 12:47:15 +0200

    Add conan to path
    Change-Id: I90e028409f606a703fd9b567a221f3f946f11d61
    Reviewed-by: Heikki Halmet <heikki.halmet@qt.io>
    (cherry picked from commit 32579bc3c429931d257265e69d828f85a55f599b)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- coin/provisioning/common/unix/install-conan.sh
+++ coin/provisioning/common/unix/install-conan.sh
@@ -6,7 +6,15 @@ source "${BASH_SOURCE%/*}/../unix/SetEnvVar.sh"
 # This script will install Conan
 # Note! Python3 is required for Conan installation
 
+os="$1"
+
 # Install Conan to Python user install directory (typically ~./local/)
 pip3 install conan --user
 
 SetEnvVar "CONAN_REVISIONS_ENABLED" "1"
+
+if [ "$os" == "linux" ]; then
+    SetEnvVar "PATH" "/home/qt/.local/bin:\$PATH"
+elif [ "$os" == "macos" ]; then
+    SetEnvVar "PATH" "/Users/qt/Library/Python/3.7/bin:\$PATH"
+fi
