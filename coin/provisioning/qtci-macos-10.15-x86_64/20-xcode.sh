7a027b4dfdf4baa65ebb07f748849aa47d5079b2
Heikki Halmet <heikki.halmet@qt.io>
2021-03-23 09:07:10 +0200

    Provisioning: Upgrade Xcode to version 12.4 for macOS 10.15 and 11.0
    Pick-to: 6.0
    Pick-to: 6.1
    Task-number: QTQAINFRA-4230
    Change-Id: Id3de8771e18da5d40cf62ad501a23fce89e845c4
    Reviewed-by: Ville-Pekka Karhu <ville-pekka.karhu@qt.io>
    Reviewed-by: Tony Sarajärvi <tony.sarajarvi@qt.io>

--- coin/provisioning/qtci-macos-10.15-x86_64/20-xcode.sh
+++ coin/provisioning/qtci-macos-10.15-x86_64/20-xcode.sh
@@ -50,4 +50,4 @@ set -ex
 # shellcheck source=../common/macos/install_xcode.sh
 source "${BASH_SOURCE%/*}/../common/macos/install_xcode.sh"
 
-InstallXCode /net/ci-files01-hki.intra.qt.io/hdd/www/input/mac/macos_10.15_catalina/Xcode_12.3.xip 12.3
+InstallXCode /net/ci-files01-hki.intra.qt.io/hdd/www/input/mac/macos_10.15_catalina/Xcode_12.4.xip 12.4
