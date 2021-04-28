97aa262db53ca6982482b7a0f595351d1f5e5b2f
Heikki Halmet <heikki.halmet@qt.io>
2021-04-28 11:43:37 +0300

    Provisioning: Upgrade python 3.6 version to 3.6.2
    typing.NoReturn is not included in python 3.6.1 version. We need at
    least 3.6.2 version
    
    Task-number: QTQAINFRA-4433
    Change-Id: I040e72e253852c7789d4f9e781d18e6b9a431a50
    Reviewed-by: Dan Ackers <dan.ackers@qt.io>
    Reviewed-by: Tony Sarajärvi <tony.sarajarvi@qt.io>
    (cherry picked from commit 6ae5052d57ad2438813a5d824ef601aab8c06935)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- coin/provisioning/qtci-windows-10-x86_64/08-python3.ps1
+++ coin/provisioning/qtci-windows-10-x86_64/08-python3.ps1
@@ -8,5 +8,5 @@
 . "$PSScriptRoot\..\common\windows\python3.ps1" 64 "a8ac14ee5486547caf84abdf151be22d9d069c0a" "C:\Python38_64" "3.8.1"
 . "$PSScriptRoot\..\common\windows\python3.ps1" 32 "14ff2c2e5538b03a012cb4c9d519d970444ebd42" "C:\Python38_32" "3.8.1"
 # default ones
-. "$PSScriptRoot\..\common\windows\python3.ps1" 64 "bf54252c4065b20f4a111cc39cf5215fb1edccff" "C:\Python36" "3.6.1" $true
-. "$PSScriptRoot\..\common\windows\python3.ps1" 32 "76c50b747237a0974126dd8b32ea036dd77b2ad1" "C:\Python36_32" "3.6.1" $true
+. "$PSScriptRoot\..\common\windows\python3.ps1" 64 "bcf9bda733a9153811209c62d628c41ab6cedbe2" "C:\Python36" "3.6.2" $true
+. "$PSScriptRoot\..\common\windows\python3.ps1" 32 "cd9744b142eca832f9534390676e6cfb84bf655d" "C:\Python36_32" "3.6.2" $true
