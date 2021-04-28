6ae5052d57ad2438813a5d824ef601aab8c06935
Heikki Halmet <heikki.halmet@qt.io>
2021-04-28 11:43:37 +0300

    Provisioning: Upgrade python 3.6 version to 3.6.2
    typing.NoReturn is not included in python 3.6.1 version. We need at
    least 3.6.2 version
    
    Pick-to: 6.1
    Pick-to: 6.0
    Task-number: QTQAINFRA-4433
    Change-Id: I040e72e253852c7789d4f9e781d18e6b9a431a50
    Reviewed-by: Dan Ackers <dan.ackers@qt.io>
    Reviewed-by: Tony Sarajärvi <tony.sarajarvi@qt.io>

--- coin/provisioning/qtci-windows-10-x86/08-python3.ps1
+++ coin/provisioning/qtci-windows-10-x86/08-python3.ps1
@@ -5,4 +5,4 @@
 #  - version
 #  - Optional true/false if set as default with PYTHON3/PIP3_PATH variables, default false
 
-. "$PSScriptRoot\..\common\windows\python3.ps1" 32 "76c50b747237a0974126dd8b32ea036dd77b2ad1" "C:\Python36" "3.6.1" $true
+. "$PSScriptRoot\..\common\windows\python3.ps1" 32 "cd9744b142eca832f9534390676e6cfb84bf655d" "C:\Python36" "3.6.2" $true
