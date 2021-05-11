6b9f328dc4431a20edaf6c12163e8bbc10e7bd9f
Heikki Halmet <heikki.halmet@qt.io>
2021-05-11 16:24:29 +0300

    Provisioning: Temporary fix for installing Conan packages in Windows
    Task-number: QTQAINFRA-4443
    Change-Id: Iba4b240edaf4a3ec8732ddbe1ae4ac5c1166b676
    Reviewed-by: Tony Sarajärvi <tony.sarajarvi@qt.io>
    (cherry picked from commit 99a81d0c6c4b3f8b5045d3b93f74b3cade64ab11)

--- coin/provisioning/qtci-windows-7-x86/11-conan.ps1
+++ coin/provisioning/qtci-windows-7-x86/11-conan.ps1
@@ -1,11 +1,1 @@
 . "$PSScriptRoot\..\common\windows\conan.ps1"
-
-Run-Conan-Install `
-    -ConanfilesDir "$PSScriptRoot\conanfiles" `
-    -BuildinfoDir Mingw-x86 `
-    -Arch x86 `
-    -Compiler "gcc" `
-    -CompilerVersion 8 `
-    -CompilerLibcxx "libstdc++" `
-    -CompilerException "dwarf2" `
-    -CompilerThreads "posix"
