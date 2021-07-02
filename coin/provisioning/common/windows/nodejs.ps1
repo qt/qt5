de0104b3ed25661ae5f62ae9d73e509c9b3e5563
Allan Sandfeld Jensen <allan.jensen@qt.io>
2021-07-02 13:27:29 +0200

    Update node.js on Windows to 18.16.0
    Needs to be at least 14.19 for Qt WebEngine 6.6
    
    Pick-to: 6.6 6.5
    Change-Id: I1d4b8b241d18eeeffa1b2a9eed0e85cbf54e49ba
    Reviewed-by: Heikki Halmet <heikki.halmet@qt.io>

--- coin/provisioning/common/windows/nodejs.ps1
+++ coin/provisioning/common/windows/nodejs.ps1
@@ -6,24 +6,24 @@
 # This script will install Node.js
 # Needed by QtWebengine
 
-$version = "12.18.0"
+$version = "18.16.0"
 if (Is64BitWinHost) {
     $arch = "x64"
-    $sha1 = "457b1527d249ee471a9445953a906cb10c75378d"
+    $sha256 = "007848640ba414f32d968d303e75d9841ecd2cd95d6fdd81f80bc3dcbd74ae44"
 } else {
     $arch = "x86"
-    $sha1 = "58801900f5bddca9c00feed6b84fed729426fc92"
+    $sha256 = "681be28e0acd057b4798f357d21eec5f49e21bc803bbbefeb1072bb4f166025a"
 }
 
-$package = "C:\Windows\temp\nodejs-$version.zip"
+$package = "C:\Windows\temp\nodejs-$version.7z"
 $targetFolder = "C:\Utils"
 $installFolder = "C:\Utils\node-v$version-win-$arch"
-$externalUrl = "https://nodejs.org/dist/v$version/node-v$version-win-$arch.zip"
-$internalUrl = "http://ci-files01-hki.ci.qt.io/input/windows/node-v$version-win-$arch.zip"
+$externalUrl = "https://nodejs.org/dist/v$version/node-v$version-win-$arch.7z"
+$internalUrl = "http://ci-files01-hki.ci.qt.io/input/windows/node-v$version-win-$arch.7z"
 
 Write-Host "Installing Node.js"
 Download $externalUrl $internalUrl $package
-Verify-Checksum $package $sha1
+Verify-Checksum $package $sha256 "sha256"
 Extract-7Zip $package $targetFolder
 Add-Path $installFolder
 Remove $package
