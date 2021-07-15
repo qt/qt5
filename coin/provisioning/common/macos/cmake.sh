9218d50a6e207bc976569573d3449c319dcbd333
Alexandru Croitor <alexandru.croitor@qt.io>
2021-07-15 07:51:00 +0200

    CMake: Update provisioned CMake to version 3.21.1
    Task-number: QTBUG-95018
    Task-number: QTBUG-95229
    Pick-to: 6.2
    Change-Id: Ibed6ff323ca1633444ed29423ce1e94c99e68f77
    Reviewed-by: Kai Koehne <kai.koehne@qt.io>

--- coin/provisioning/common/macos/cmake.sh
+++ coin/provisioning/common/macos/cmake.sh
@@ -42,11 +42,11 @@ source "${BASH_SOURCE%/*}/InstallAppFromCompressedFileFromURL.sh"
 # shellcheck source=../unix/SetEnvVar.sh
 source "${BASH_SOURCE%/*}/../unix/SetEnvVar.sh"
 
-majorminorversion="3.20"
-version="3.20.5"
+majorminorversion="3.21"
+version="3.21.1"
 PrimaryUrl="http://ci-files01-hki.intra.qt.io/input/cmake/cmake-$version-macos-universal.tar.gz"
 AltUrl="https://cmake.org/files/v$majorminorversion/cmake-$version-macos-universal.tar.gz"
-SHA1="5632fed86dbe0e5157fd233b1db6db738dbe4d1f"
+SHA1="5845da632d0f073c7761df4e78efb4405e00ac5a"
 appPrefix="cmake-$version-macos-universal"
 
 InstallAppFromCompressedFileFromURL "$PrimaryUrl" "$AltUrl" "$SHA1" "$appPrefix"
