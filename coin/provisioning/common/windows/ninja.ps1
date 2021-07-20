200542194ae3c7e84492ef1ab62afe3fce0fa686
Alexandru Croitor <alexandru.croitor@qt.io>
2021-07-20 16:06:58 +0200

    Provisioning: Install ninja 1.10.2 for Windows and macOS
    To avoid error 'too many path components' when building Qt on Windows.
    
    Task-number: QTBUG-95286
    Change-Id: I122cfddc8c19201f4c04fde94d051c5e1278c040
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: Simo Fält <simo.falt@qt.io>
    (cherry picked from commit e8b0be77118e9ee6bec3887807cb7e37d334c0a3)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- coin/provisioning/common/windows/ninja.ps1
+++ coin/provisioning/common/windows/ninja.ps1
@@ -1,13 +1,13 @@
 . "$PSScriptRoot\helpers.ps1"
 
-$zip = Get-DownloadLocation "ninja-1.6.0-win-x86.zip"
+$zip = Get-DownloadLocation "ninja-1.10.2-win-x86.zip"
 
-Download https://github.com/ninja-build/ninja/releases/download/v1.6.0/ninja-win.zip \\ci-files01-hki.intra.qt.io\provisioning\ninja\ninja-1.6.0-win-x86.zip $zip
-Verify-Checksum $zip "E01093F6533818425F8EFB0843CED7DCAABEA3B2"
+Download https://github.com/ninja-build/ninja/releases/download/v1.10.2/ninja-win.zip \\ci-files01-hki.intra.qt.io\provisioning\ninja\ninja-1.10.2-win-x86.zip $zip
+Verify-Checksum $zip "ccacdf88912e061e0b527f2e3c69ee10544d6f8a"
 
 Extract-7Zip $zip C:\Utils\Ninja
 Remove "$zip"
 
 Add-Path "C:\Utils\Ninja"
 
-Write-Output "Ninja = 1.6.0" >> ~/versions.txt
+Write-Output "Ninja = 1.10.2" >> ~/versions.txt
