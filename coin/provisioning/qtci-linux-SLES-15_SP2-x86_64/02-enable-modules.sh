dcab4e83f35a7972ed0b565f02efc864d5f28df7
Tony Sarajärvi <tony.sarajarvi@qt.io>
2021-08-16 06:35:47 +0000

    Revert "Replace SLES 15 SP2 with SP3"
    This reverts commit 5ecf01ade7293195b11200dc1ed6ff2a5939b948.
    
    Reason for revert: Wicked doesn't work well with our Windows 2012's DHCP
    
    Change-Id: I062fbfb50a68af308db9cb810583ca85c2c3e627
    Reviewed-by: Toni Saario <toni.saario@qt.io>

--- coin/provisioning/qtci-linux-SLES-15_SP2-x86_64/02-enable-modules.sh
+++ coin/provisioning/qtci-linux-SLES-15_SP2-x86_64/02-enable-modules.sh
@@ -8,10 +8,10 @@ sudo SUSEConnect --cleanup
 sudo sh rmt-client-setup https://repo-clones.ci.qt.io:8082 --yes --fingerprint C9:4F:0B:81:DE:84:AF:F2:50:3E:89:B9:7F:BC:63:BB:A7:AC:BE:97
 
 # Activate these modules
-sudo SUSEConnect -p sle-module-basesystem/15.3/x86_64
-sudo SUSEConnect -p sle-module-server-applications/15.3/x86_64
-sudo SUSEConnect -p sle-module-desktop-applications/15.3/x86_64
-sudo SUSEConnect -p sle-module-development-tools/15.3/x86_64
-sudo SUSEConnect -p sle-module-python2/15.3/x86_64
+sudo SUSEConnect -p sle-module-basesystem/15.2/x86_64
+sudo SUSEConnect -p sle-module-server-applications/15.2/x86_64
+sudo SUSEConnect -p sle-module-desktop-applications/15.2/x86_64
+sudo SUSEConnect -p sle-module-development-tools/15.2/x86_64
+sudo SUSEConnect -p sle-module-python2/15.2/x86_64
 
 sudo zypper lr -u
