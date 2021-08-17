aaf0fad6b54dfc17620583f7d02ea781b5a58985
Eike Ziller <eike.ziller@qt.io>
2021-08-17 09:59:39 +0200

    Provisioning: Install 32bit version of Ninja on Windows
    So it also runs on 32bit Windows. Ninja upstream switched their
    binary builds to 64bit with Ninja 1.7, so we need to provide our own.
    
    Amends e8b0be77118e9ee6bec3887807cb7e37d334c0a3
    
    Change-Id: Id3329ca481b7743b4d40dad8faaa62285dbe53b0
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>
    (cherry picked from commit f945fdc735c65e18f9cc9fff17efa6ba225851f7)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- coin/provisioning/common/windows/ninja.ps1
+++ coin/provisioning/common/windows/ninja.ps1
@@ -2,8 +2,8 @@
 
 $zip = Get-DownloadLocation "ninja-1.10.2-win-x86.zip"
 
-Download https://github.com/ninja-build/ninja/releases/download/v1.10.2/ninja-win.zip \\ci-files01-hki.intra.qt.io\provisioning\ninja\ninja-1.10.2-win-x86.zip $zip
-Verify-Checksum $zip "ccacdf88912e061e0b527f2e3c69ee10544d6f8a"
+Download http://master.qt.io/development_releases/prebuilt/ninja/v1.10.2/ninja-win-x86.zip \\ci-files01-hki.intra.qt.io\provisioning\ninja\ninja-1.10.2-win-really-x86.zip $zip
+Verify-Checksum $zip "1a22ee9269df8ed69c4600d7ee4ccd8841bb99ca"
 
 Extract-7Zip $zip C:\Utils\Ninja
 Remove "$zip"
