2aa83032365b30768ff7c137e45e4e0cba33a302
Heikki Halmet <heikki.halmet@qt.io>
2021-10-12 12:21:31 +0300

    Provisioning: Add OpenSSL for Android to macOS
    Task-number: QTBUG-97243
    Pick-to: 6.2
    Change-Id: Ibf67b7d713904edb9f8ee83b35eb096e2d3f1eb1
    Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>

--- coin/provisioning/qtci-linux-RHEL-8.2-x86_64/50-openssl_for_android_linux.sh
+++ coin/provisioning/qtci-linux-RHEL-8.2-x86_64/50-openssl_for_android_linux.sh
@@ -2,7 +2,7 @@
 
 #############################################################################
 ##
-## Copyright (C) 2020 The Qt Company Ltd.
+## Copyright (C) 2021 The Qt Company Ltd.
 ## Contact: http://www.qt.io/licensing/
 ##
 ## This file is part of the provisioning scripts of the Qt Toolkit.
@@ -34,5 +34,5 @@
 #############################################################################
 set -ex
 
-# shellcheck source=../common/linux/openssl_for_android_linux.sh
-source "${BASH_SOURCE%/*}/../common/linux/openssl_for_android_linux.sh"
+# shellcheck source=../common/unix/openssl_for_android.sh
+source "${BASH_SOURCE%/*}/../common/unix/openssl_for_android.sh"
