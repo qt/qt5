72997d7039f5ce77fc5eca06152d38f70510bde4
Heikki Halmet <heikki.halmet@qt.io>
2021-11-08 11:46:40 +0200

    Upgrade Xcode and Command Line Tools version to 13.2
    Xcode 13.2 works only in macOS 11.3/12.0 or newer
    
    Task-number: QTQAINFRA-4607
    Pick-to: 6.2
    Pick-to: 6.3
    Pick-to: 5.15
    Change-Id: Ic2034a2091a041a6daa0f342e5820641488c068b
    Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>

--- coin/provisioning/qtci-macos-11-x86_64/20-xcode.sh
+++ coin/provisioning/qtci-macos-11-x86_64/20-xcode.sh
@@ -56,4 +56,4 @@ set -ex
 # shellcheck source=../common/macos/install_xcode.sh
 source "${BASH_SOURCE%/*}/../common/macos/install_xcode.sh"
 
-InstallXCode /net/ci-files01-hki.intra.qt.io/hdd/www/input/mac/Xcode_12.5.1.xip 12.5.1
+InstallXCode /net/ci-files01-hki.intra.qt.io/hdd/www/input/mac/Xcode_13.2.xip 13.2
