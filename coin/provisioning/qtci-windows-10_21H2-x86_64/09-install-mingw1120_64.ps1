64d08c5039185e2afa743b696287b9a23e55a7a1
Cristian Adam <cristian.adam@gmail.com>
2022-03-21 11:03:50 +0100

    Update MinGW 11.2.0 to rev3
    Revision 3 updates gdb to version 11.2.0.
    
    This is not necessarily needed for building, but I think it's better to
    have the same toolchain as the Qt SDK.
    
    Task-number: QTBUG-101653
    Change-Id: I92a319a943d18ec10e5e0da3cd770ba7cde26f13
    Reviewed-by: Tony Sarajärvi <tony.sarajarvi@qt.io>
    (cherry picked from commit c76e26a5fa94777a426cfdcedc78cc0f09df4c95)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- coin/provisioning/qtci-windows-10_21H2-x86_64/09-install-mingw1120_64.ps1
+++ coin/provisioning/qtci-windows-10_21H2-x86_64/09-install-mingw1120_64.ps1
@@ -3,9 +3,9 @@
 # This script will install 64-bit MinGW 11.2.0
 # Note! MinGW version is 9.0.0 but the GCC version is 11.2 which is used with the naming of MinGW
 
-$release = "MinGW-w64-x86_64-11.2.0-release-posix-seh-rt_v9-rev1"
+$release = "MinGW-w64-x86_64-11.2.0-release-posix-seh-rt_v9-rev3"
 
-$sha1    = "76bbfd135633d3d414e06cd7bf70cae94d2311db"
+$sha1    = "50174112ceca6f9fe42debbedda54ce6e9187302"
 
 InstallMinGW $release $sha1
 
--- coin/provisioning/qtci-windows-11_21H2-x86_64/09-install-mingw1120_64.ps1
+++ coin/provisioning/qtci-windows-11_21H2-x86_64/09-install-mingw1120_64.ps1
@@ -3,9 +3,9 @@
 # This script will install 64-bit MinGW 11.2.0
 # Note! MinGW version is 9.0.0 but the GCC version is 11.2 which is used with the naming of MinGW
 
-$release = "mingw-w64-x86_64-11.2.0-release-posix-seh-rt_v9-rev1"
+$release = "MinGW-w64-x86_64-11.2.0-release-posix-seh-rt_v9-rev3"
 
-$sha1    = "5554791dc13468bf44e2e519c6691f2deecd000c"
+$sha1    = "50174112ceca6f9fe42debbedda54ce6e9187302"
 
 InstallMinGW $release $sha1
 
