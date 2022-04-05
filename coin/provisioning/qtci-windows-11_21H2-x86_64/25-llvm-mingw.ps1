66725b2895d52380b5223bd80a88f419dd9fef21
Heikki Halmet <heikki.halmet@qt.io>
2022-04-05 10:59:39 +0300

    Update llvm-mingw package to official version
    Pick-to: 6.3 6.2
    Change-Id: I6083bd46871bc9f08e5b856aea5ed3b567cb5e53
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: Tony Sarajärvi <tony.sarajarvi@qt.io>

--- coin/provisioning/qtci-windows-11_21H2-x86_64/25-llvm-mingw.ps1
+++ coin/provisioning/qtci-windows-11_21H2-x86_64/25-llvm-mingw.ps1
@@ -1,6 +1,6 @@
 #############################################################################
 ##
-## Copyright (C) 2021 The Qt Company Ltd.
+## Copyright (C) 2022 The Qt Company Ltd.
 ## Contact: https://www.qt.io/licensing/
 ##
 ## This file is part of the provisioning scripts of the Qt Toolkit.
@@ -40,16 +40,17 @@
 . "$PSScriptRoot\..\common\windows\helpers.ps1"
 
 # This script installs LLVM-Mingw by mstorsjo
+# https://github.com/mstorsjo/llvm-mingw/releases/tag/20220323
 
-$zip = Get-DownloadLocation "llvm-mingw-20220209-ucrt-x86_64.zip"
-$url_cache = "http://ci-files01-hki.intra.qt.io/input/windows/llvm-mingw-20220209-ucrt-x86_64.zip"
-$url_official = "https://github.com/mstorsjo/llvm-mingw/releases/download/20220209/llvm-mingw-20220209-ucrt-x86_64.zip"
+$zip = Get-DownloadLocation "llvm-mingw-20220323-ucrt-x86_64.zip"
+$url_cache = "http://ci-files01-hki.intra.qt.io/input/windows/llvm-mingw-20220323-ucrt-x86_64.zip"
+$url_official = "https://github.com/mstorsjo/llvm-mingw/releases/download/20220323/llvm-mingw-20220323-ucrt-x86_64.zip"
 
 Download $url_official $url_cache $zip
-Verify-Checksum $zip "e24078c0d742b39eef4d739b345a0b6b49bc348b"
+Verify-Checksum $zip "2b55846635f9e57fd37a19e0ba4183b3176ad57f"
 Extract-7Zip $zip C:\
 
-Rename-Item C:\llvm-mingw-20220209-ucrt-x86_64 C:\llvm-mingw
+Rename-Item C:\llvm-mingw-20220323-ucrt-x86_64 C:\llvm-mingw
 
-Write-Output "llvm-mingw = 14.0.0 RC1" >> ~/versions.txt
+Write-Output "llvm-mingw = 14.0.0" >> ~/versions.txt
 Remove-Item -Path $zip
