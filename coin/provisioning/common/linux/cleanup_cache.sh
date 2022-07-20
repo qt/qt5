aa9095d4cd235918118f6ec8f524806e3510e6ca
Axel Spoerl <axel.spoerl@qt.io>
2022-07-20 09:49:24 +0200

    Harden cache delete script and extend to all Linux CI platforms
    Log script activities and deleted toplevel cache files/directories.
    Use sudo to override permissions in cache.
    Remove legacy single file based cache if it exists by error.
    Create cache directory with appropriate permissions if missing.
    
    Pick-to: 6.4 6.3 6.2 5.15
    Change-Id: Id6ad930bf8ba9233eeea8a7602d25c98d0058409
    Reviewed-by: Simo Fält <simo.falt@qt.io>

--- coin/provisioning/common/linux/cleanup_cache.sh
+++ coin/provisioning/common/linux/cleanup_cache.sh
@@ -39,6 +39,58 @@
 ##
 #############################################################################
 
-set -ex
+# This script needs to be called at the end of provisioning, to clean the cache directory
 
-"$(dirname "$0")"/../common/linux/cleanup_cache.sh
+set -e
+set -f
+QT_USER="qt"
+CACHE=".cache"
+
+echo "---- starting cache cleanup."
+
+# skip if user qt does not exist
+echo "---- checking user $QT_USER"
+if grep -q "^$QT_USER:" /etc/passwd; then
+    echo "(**) found user $QT_USER"
+else
+    echo "(WW) user $QT_USER not found."
+    echo "---- skipping cache cleanup."
+    exit 0;
+fi
+
+# assume /home/qt as ~ won't expand into sudo
+CACHEDIR="/home/$QT_USER/$CACHE"
+
+# delete files from a directory if it exists
+echo "---- checking cache directory  $CACHEDIR"
+if sudo [ -d "$CACHEDIR" ]; then
+    if [ "$(sudo ls -A $CACHEDIR)" ]; then
+       echo "(WW) cache in $CACHEDIR is not empty."
+       echo "---- removing content:"
+
+       # List files and delete in a loop as wildcard won't expand into sudo
+       FILES=`sudo ls -A1 $CACHEDIR`
+       while read FILE;
+           do
+           echo "--- rm -rf $FILE"
+           sudo rm -rf "$CACHEDIR/$FILE"
+       done <<< $FILES
+    else
+       echo "(**) cache in $CACHEDIR is empty."
+    fi
+else
+    if sudo [ -f "$CACHEDIR" ]; then
+        # replace a cache file with a directory
+        echo "(WW) $CACHEDIR is a file."
+        echo "---- removing $CACHEDIR."
+        sudo rm -rf "$CACHEDIR"
+    else
+        echo "(WW) cache directory $CACHEDIR not found."
+    fi
+
+    # create new cache directory and assign rights
+    echo "---- creating cache director $CACHEDIR."
+    sudo mkdir "$CACHEDIR"
+    sudo chown $QT_USER:users $CACHEDIR
+    sudo chmod 700 $CACHEDIR
+fi
