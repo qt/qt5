97f8d33586b80731d0ad752f8f3b0b78afd1840a
Heikki Halmet <heikki.halmet@qt.io>
2022-08-18 16:23:57 +0300

    Remove SLES 15 SP3 from CI
    SLES 15 SP3 will be replaced by SLES 15 SP4
    
    Task-number: QTQAINFRA-5068
    Change-Id: I60f0ec02d71d1db162f1238df7ad853ff0df2d6e
    Reviewed-by: Tony Sarajärvi <tony.sarajarvi@qt.io>

--- coin/provisioning/qtci-linux-SLES-15_SP3-x86_64/01-disable_net_lso.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-sudo mkdir /etc/wicked/scripts
-echo "ethtool -K \$2 tso off" | sudo tee -a /etc/wicked/scripts/net_tso_off
-sudo chmod 744 /etc/wicked/scripts/net_tso_off
-echo "PRE_UP_SCRIPT='wicked:/etc/wicked/scripts/net_tso_off'" | sudo tee -a /etc/sysconfig/network/ifcfg-eth0
--- coin/provisioning/qtci-linux-SLES-15_SP3-x86_64/08-libclang-v100-dyn.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-BASEDIR=$(dirname "$0")
-# shellcheck source=../common/unix/libclang-v100-dyn.sh
-"$BASEDIR/../common/unix/libclang-v100-dyn.sh"
