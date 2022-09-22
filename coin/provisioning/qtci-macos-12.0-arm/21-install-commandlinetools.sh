c1a1e65cdc478a12a726ae2c0132c9eea9c9995d
Ville-Pekka Karhu <ville-pekka.karhu@qt.io>
2022-09-22 15:24:27 +0300

    Provisioning: Xcode update to ARM macs
    Change-Id: I71828f5b126c6243163238aef2000dfb91cdbcb2
    Reviewed-by: Heikki Halmet <heikki.halmet@qt.io>

--- coin/provisioning/qtci-macos-12.0-arm/21-install-commandlinetools.sh
+++ coin/provisioning/qtci-macos-12.0-arm/21-install-commandlinetools.sh
@@ -2,7 +2,7 @@
 
 #############################################################################
 ##
-## Copyright (C) 2021 The Qt Company Ltd.
+## Copyright (C) 2022 The Qt Company Ltd.
 ## Contact: https://www.qt.io/licensing/
 ##
 ## This file is part of the provisioning scripts of the Qt Toolkit.
@@ -43,9 +43,9 @@ set -ex
 
 # shellcheck source=../common/macos/install-commandlinetools.sh
 source "${BASH_SOURCE%/*}/../common/macos/install-commandlinetools.sh"
-version="13.2"
+version="14"
 packageName="Command_Line_Tools_for_Xcode_$version.dmg"
 url="http://ci-files01-hki.intra.qt.io/input/mac/$packageName"
-sha1="b3a0b597435cfbc5c09ad5772cf7313c87032252"
+sha1="0d9cb1708d178866b3d56b88445bdf4d8ff984e3"
 
 InstallCommandLineTools $url $url $sha1 $packageName $version
