487c92a3d086cc3b086899c5a212c1a64b27881f
Luca Di Sera <luca.disera@qt.io>
2022-10-06 14:08:04 +0200

    Update QDoc libclang to 15.0.0
    The `libclang_version` variable is now updated to the current version, 15.0.0,
    in both "coin/provisioning/common/unix/libclang.sh" and
    "coin/provisioning/common/windows/libclang.ps1".
    
    The naming of the artifacts for the provided library was slightly
    changed from previous versions.
    
    Before, the version number would contain two integers separated by a
    dot.
    The generated artifact would be named after this version number, with
    the dot removed.
    
    That is, for example, for version 12.0, a release artifact would be
    called "libclang-release_120.*".
    
    The updated two version uses a three segments version number and,
    furthermore, the artifacts name do not collapse the separating dots such
    that a release artifact would be called "libclang-release_15.0.0.*".
    
    To support the new naming scheme, some processing was removed from both
    provisioning scripts.
    
    For Windows, the line removing all dots from the `libclang_version`
    variable was removed.
    
    For Unix, the removal of the dots in the `libclang_version`
    variable when expanding the variable to generate the target urls to
    retrieve the artifact was removed, using the bare value of the variable
    itself.
    
    For Unix provisioning, the artifacts for some of the platform, namely
    "CentOS" and "Ubuntu 22.04", are not provided anymore.
    
    "CentOS" was replaced by "Rhel8.4" on "gcc10.0", hence the target url
    for the redhat platform was modified based on the new name.
    
    The "Ubuntu 22.04" artifacts were not provided, such that the platform
    was removed.
    
    The sha1 of the target artifacts on both provisioning script was updated
    for all platforms.
    
    Task-number: QTBUG-107199
    Pick-to: 6.4
    Change-Id: I873680825b0953e897c610fb0b47f3cd90625646
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: Kai Köhne <kai.koehne@qt.io>
    Reviewed-by: Topi Reiniö <topi.reinio@qt.io>

--- coin/provisioning/common/unix/libclang.sh
+++ coin/provisioning/common/unix/libclang.sh
@@ -53,28 +53,23 @@ source "${BASH_SOURCE%/*}/SetEnvVar.sh"
 # shellcheck source=./DownloadURL.sh
 source "${BASH_SOURCE%/*}/DownloadURL.sh"
 
-libclang_version=12.0
+libclang_version="15.0.0"
 
 if uname -a |grep -q Darwin; then
     version=$libclang_version
-    url="https://download.qt.io/development_releases/prebuilt/libclang/qt/libclang-release_${version//\./}-based-mac.7z"
-    url_cached="http://ci-files01-hki.intra.qt.io/input/libclang/qt/libclang-release_${version//\./}-based-mac.7z"
-    sha1="bb9223450c1c36ee37d8c91e876dba82db117a7a"
+    url="https://download.qt.io/development_releases/prebuilt/libclang/qt/libclang-release_${version}-based-mac.7z"
+    url_cached="http://ci-files01-hki.intra.qt.io/input/libclang/qt/libclang-release_${version}-based-mac.7z"
+    sha1="6d916a17459c81551dde47580ae3f071e93338a5"
 elif test -f /etc/redhat-release && cat /etc/redhat-release | grep "Red Hat" | grep -v "8" ; then
     version=$libclang_version
-    url="https://download.qt.io/development_releases/prebuilt/libclang/qt/libclang-release_${version//\./}-based-linux-CentOS8.3-gcc9.2-x86_64.7z"
-    url_cached="http://ci-files01-hki.intra.qt.io/input/libclang/qt/libclang-release_${version//\./}-based-linux-CentOS8.3-gcc9.2-x86_64.7z"
-    sha1="d02e4dd30fe3f810c3a0cdcbed5870dec56ed91b"
-elif [[ "$(cat /etc/os-release |grep VERSION_ID)" == "VERSION_ID=\"22.04\"" ]]; then
-    version=$libclang_version
-    url="https://download.qt.io/development_releases/prebuilt/libclang/qt/libclang-release_${libclang_version//\./}-based-linux-Ubuntu22.04-gcc11.2-x86_64.7z"
-    url_cached="http://ci-files01-hki.intra.qt.io/input/libclang/qt/libclang-release_${libclang_version//\./}-based-linux-Ubuntu22.04-gcc11.2-x86_64.7z"
-    sha1="7bd311b941df4087621bab306d507a3b1375cdfe"
+    url="https://download.qt.io/development_releases/prebuilt/libclang/qt/libclang-release_${version}-based-linux-Rhel8.4-gcc10.0-x86_64.7z"
+    url_cached="http://ci-files01-hki.intra.qt.io/input/libclang/qt/libclang-release_${version}-based-linux-Rhel8.4-gcc10.0-x86_64.7z"
+    sha1="6ca035bb522022d34d61759e0460845832933b5c"
 else
     version=$libclang_version
-    url="https://download.qt.io/development_releases/prebuilt/libclang/qt/libclang-release_${version//\./}-based-linux-Ubuntu20.04-gcc9.3-x86_64.7z"
-    url_cached="http://ci-files01-hki.intra.qt.io/input/libclang/qt/libclang-release_${version//\./}-based-linux-Ubuntu20.04-gcc9.3-x86_64.7z"
-    sha1="81b3e640befa23b5cdebd59e206dc79e4de20ba3"
+    url="https://download.qt.io/development_releases/prebuilt/libclang/qt/libclang-release_${version}-based-linux-Ubuntu20.04-gcc9.3-x86_64.7z"
+    url_cached="http://ci-files01-hki.intra.qt.io/input/libclang/qt/libclang-release_${version}-based-linux-Ubuntu20.04-gcc9.3-x86_64.7z"
+    sha1="bd6615012b8bdb2720a45ede56e05f6db7191843"
 fi
 
 zip="/tmp/libclang.7z"
