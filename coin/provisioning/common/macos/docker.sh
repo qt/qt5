cde0e8a450fd5e2e89381341343a56c43d5023d1
Heikki Halmet <heikki.halmet@qt.io>
2022-10-31 11:45:00 +0000

    Provisioning: Add Docker for macOS
    Needed by RTA tests
    
    Task-number: QTQAINFRA-5171
    Change-Id: I26ac10421ba32d68ffdef523fd97f710cd6bcaab
    Reviewed-by: Samuli Piippo <samuli.piippo@qt.io>
    (cherry picked from commit e266a9e6040e07cace330ba43b0372e04f39e102)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

---
+++ coin/provisioning/common/macos/docker.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+#############################################################################
+##
+## Copyright (C) 2022 The Qt Company Ltd.
+## Contact: https://www.qt.io/licensing/
+##
+## This file is part of the provisioning scripts of the Qt Toolkit.
+##
+## $QT_BEGIN_LICENSE:LGPL$
+## Commercial License Usage
+## Licensees holding valid commercial Qt licenses may use this file in
+## accordance with the commercial license agreement provided with the
+## Software or, alternatively, in accordance with the terms contained in
+## a written agreement between you and The Qt Company. For licensing terms
+## and conditions see https://www.qt.io/terms-conditions. For further
+## information use the contact form at https://www.qt.io/contact-us.
+##
+## GNU Lesser General Public License Usage
+## Alternatively, this file may be used under the terms of the GNU Lesser
+## General Public License version 3 as published by the Free Software
+## Foundation and appearing in the file LICENSE.LGPL3 included in the
+## packaging of this file. Please review the following information to
+## ensure the GNU Lesser General Public License version 3 requirements
+## will be met: https://www.gnu.org/licenses/lgpl-3.0.html.
+##
+## GNU General Public License Usage
+## Alternatively, this file may be used under the terms of the GNU
+## General Public License version 2.0 or (at your option) the GNU General
+## Public license version 3 or any later version approved by the KDE Free
+## Qt Foundation. The licenses are as published by the Free Software
+## Foundation and appearing in the file LICENSE.GPL2 and LICENSE.GPL3
+## included in the packaging of this file. Please review the following
+## information to ensure the GNU General Public License requirements will
+## be met: https://www.gnu.org/licenses/gpl-2.0.html and
+## https://www.gnu.org/licenses/gpl-3.0.html.
+##
+## $QT_END_LICENSE$
+##
+#############################################################################
+
+# shellcheck source=./../unix/DownloadURL.sh
+source "${BASH_SOURCE%/*}/../unix/DownloadURL.sh"
+
+# This script will install Docker which is needed by RTA
+
+chip=$1
+
+if [[ $chip == "amd64" ]]; then
+    sha="158eae1d2f81cc8a0754d2ea3af8c6e6e555f69b"
+else
+    sha="6adf6cc8558af69296208b045187406a95b020bf"
+fi
+
+echo "Installing Docker for $chip chip"
+urlOccifical="https://desktop.docker.com/mac/main/${chip}/Docker.dmg?utm_source=docker"
+urlCache="http://ci-files01-hki.ci.qt.io/input/mac/Docker_${chip}.dmg"
+
+DownloadURL $urlCache $urlOccifical $sha "/tmp/Docker_${chip}.dmg"
+
+sudo hdiutil attach "/tmp/Docker_${chip}.dmg"
+sudo /Volumes/Docker/Docker.app/Contents/MacOS/install --accept-license --user qt
+sudo hdiutil detach /Volumes/Docker
---
+++ coin/provisioning/qtci-macos-13-arm/90-docker.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# shellcheck source=../common/macos/docker.sh
+source "${BASH_SOURCE%/*}/../common/macos/docker.sh" "arm64"
+
