af2cb2edc4e6654f319708b23a8eaa87ee676d7e
Simo Fält <simo.falt@qt.io>
2022-11-03 11:57:47 +0200

    Provisioning: Update default python3 version
    Removing python3.6 and replacing it with more recent versions.
    
    Change-Id: If8cd4c369e4fc9ff8dc7d53865b80b588f278091
    Reviewed-by: Tony Sarajärvi <tony.sarajarvi@qt.io>
    Reviewed-by: Jukka Jokiniva <jukka.jokiniva@qt.io>
    (cherry picked from commit 95e9f8da9ae84858e58af659735cc43d177af12e)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- coin/provisioning/qtci-windows-11_21H2-x86_64/08-python3.ps1
+++ coin/provisioning/qtci-windows-11_21H2-x86_64/08-python3.ps1
@@ -5,9 +5,8 @@
 #  - version
 #  - Optional true/false if set as default with PYTHON3/PIP3_PATH variables, default false
 
-. "$PSScriptRoot\..\common\windows\python3.ps1" 64 "a8ac14ee5486547caf84abdf151be22d9d069c0a" "C:\Python38_64" "3.8.1"
-. "$PSScriptRoot\..\common\windows\python3.ps1" 32 "14ff2c2e5538b03a012cb4c9d519d970444ebd42" "C:\Python38_32" "3.8.1"
 . "$PSScriptRoot\..\common\windows\python3.ps1" 64 "f8d42a421db25a01b53f0c135a9a74ddae042643" "C:\Python37_64" "3.7.9"
+. "$PSScriptRoot\..\common\windows\python3.ps1" 64 "3ee4e92a8ef94c70fb56859503fdc805d217d689" "C:\Python310_64" "3.10.0"
 # default ones
-. "$PSScriptRoot\..\common\windows\python3.ps1" 64 "bcf9bda733a9153811209c62d628c41ab6cedbe2" "C:\Python36" "3.6.2" $true
-. "$PSScriptRoot\..\common\windows\python3.ps1" 32 "cd9744b142eca832f9534390676e6cfb84bf655d" "C:\Python36_32" "3.6.2" $true
+. "$PSScriptRoot\..\common\windows\python3.ps1" 64 "a8ac14ee5486547caf84abdf151be22d9d069c0a" "C:\Python38_64" "3.8.1" $true
+. "$PSScriptRoot\..\common\windows\python3.ps1" 32 "14ff2c2e5538b03a012cb4c9d519d970444ebd42" "C:\Python38_32" "3.8.1" $true
