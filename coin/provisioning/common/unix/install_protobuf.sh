76a44df8415c167583651a2ffdd44a4b7c7a0243
Alexey Edelev <alexey.edelev@qt.io>
2022-11-24 18:15:21 +0100

    Adjust CMAKE_OSX_DEPLOYMENT_TARGET for protobuf package across MacOS platforms
    Change-Id: Ie9f1b2273c437016fd51173e330a5df4cc924ebc
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>

--- coin/provisioning/common/unix/install_protobuf.sh
+++ coin/provisioning/common/unix/install_protobuf.sh
@@ -77,7 +77,7 @@ echo "Configuring and building protobuf"
 
 installPrefix="/usr/local"
 if uname -a |grep -q Darwin; then
-    extraCMakeArgs=-DCMAKE_OSX_ARCHITECTURES="x86_64;arm64"
+    extraCMakeArgs="-DCMAKE_OSX_ARCHITECTURES=x86_64;arm64 -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15"
     SetEnvVar PATH "\$PATH:$installPrefix/bin"
 fi
 
