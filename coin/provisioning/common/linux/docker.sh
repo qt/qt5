33605ad71676e7118876ec311dfb152ffc6d310e
Heikki Halmet <heikki.halmet@qt.io>
2023-02-02 13:42:56 +0000

    Provisioning: Update Docker
    This change will upgrade docker-compose version to v2.15.1
    Also 'containerd', 'docker-ce' and 'docker-ce-cli' versions will be
    upgraded to support newer Ubuntu version (20.04)
    
    Task-number: QTQAINFRA-5396
    Change-Id: I282a1cbc5870db765409985983d29f57936d1798
    Reviewed-by: Simo Fält <simo.falt@qt.io>
    Reviewed-by: Dimitrios Apostolou <jimis@qt.io>
    (cherry picked from commit 739cc29b7777bedc70c20dcd83eb5d05033d7bec)
    Reviewed-by: Katja Marttila <katja.marttila@qt.io>

--- coin/provisioning/common/linux/docker.sh
+++ coin/provisioning/common/linux/docker.sh
@@ -2,7 +2,7 @@
 
 #############################################################################
 ##
-## Copyright (C) 2019 The Qt Company Ltd.
+## Copyright (C) 2023 The Qt Company Ltd.
 ## Contact: http://www.qt.io/licensing/
 ##
 ## This file is part of the provisioning scripts of the Qt Toolkit.
@@ -42,12 +42,12 @@ PROVISIONING_DIR="$(dirname "$0")/../../"
 
 
 localRepo=http://ci-files01-hki.intra.qt.io/input/docker
-upstreamRepo=https://download.docker.com/linux/ubuntu/dists/bionic/pool/stable/amd64
+upstreamRepo=https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/
 
 echo '
-    f4c941807310e3fa470dddfb068d599174a3daec containerd.io_1.2.10-3_amd64.deb
-    ee640d9258fd4d3f4c7017ab2a71da63cbbead55 docker-ce_19.03.4~3-0~ubuntu-bionic_amd64.deb
-    09402bf5dac40f0c50f1071b17f38f6584a42ad1 docker-ce-cli_19.03.4~3-0~ubuntu-bionic_amd64.deb
+    2666840157bab3b77a850236fbf323c423fb564a containerd.io_1.6.9-1_amd64.deb
+    ec6a1ad99b19b6a674349fb13bcc10d62c54c404 docker-ce_23.0.0-1~ubuntu.20.04~focal_amd64.deb
+    130774916fa7e2c9997b8fcb4e7696a343f12fb0 docker-ce-cli_23.0.0-1~ubuntu.20.04~focal_amd64.deb
 ' \
     | xargs -n2 | while read  sha f
 do
@@ -62,10 +62,11 @@ sudo docker --version
 
 # Download and install the docker-compose extension from https://github.com/docker/compose/releases
 f=docker-compose-$(uname -s)-$(uname -m)
+dockerComposeVersion="v2.15.1"
 DownloadURL  \
-    $localRepo/$f-1.24.1  \
-    https://github.com/docker/compose/releases/download/1.24.1/$f \
-    cfb3439956216b1248308141f7193776fcf4b9c9b49cbbe2fb07885678e2bb8a
+    $localRepo/$f-${dockerComposeVersion}  \
+    https://github.com/docker/compose/releases/download/${dockerComposeVersion}/$f \
+    bcfd9ea51dee4c19dccdfaeef0e7956ef68bf14f3d175933742061a7271ef0f5
 sudo install -m 755 ./docker-compose* /usr/local/bin/docker-compose
 sudo docker-compose --version
 rm ./docker-compose*
