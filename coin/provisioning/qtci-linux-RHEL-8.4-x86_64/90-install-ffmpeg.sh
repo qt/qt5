e3b200d728e2b9bf36afe0f5e2445566c37fe688
Samuel Mira <samuel.mira@qt.io>
2023-04-17 10:56:30 +0300

    Fix FFmpeg provisioning for Android in arm64, x86 and x86_64
    Added install-ffmpeg-android script for the android cross-compile
    Removed android stuff from install-ffmpeg
    Changed the yaml variables to include the target arch
    Added the arm64 target arch
    
    Task-number: QTQAINFRA-5476
    Change-Id: Id9609ed0427f2fc42f7f27e7787a81ad145f59be
    Reviewed-by: Ville Voutilainen <ville.voutilainen@qt.io>
    Reviewed-by: Rami Potinkara <rami.potinkara@qt.io>
    Reviewed-by: Lars Knoll <lars@knoll.priv.no>
    (cherry picked from commit 99afe7199ecc09e56cb5d779db31bf65ba98fa3d)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- coin/provisioning/qtci-linux-RHEL-8.4-x86_64/90-install-ffmpeg.sh
+++ coin/provisioning/qtci-linux-RHEL-8.4-x86_64/90-install-ffmpeg.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 source "${BASH_SOURCE%/*}/../common/unix/install-ffmpeg.sh" "linux"
-source "${BASH_SOURCE%/*}/../common/unix/install-ffmpeg.sh" "android"
+source "${BASH_SOURCE%/*}/../common/unix/install-ffmpeg-android.sh" "android-x86"
+source "${BASH_SOURCE%/*}/../common/unix/install-ffmpeg-android.sh" "android-x86_64"
