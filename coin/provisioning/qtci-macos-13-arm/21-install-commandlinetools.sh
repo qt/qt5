08ac26856178491cd38327ba26e9d853bd770de5
Tero Heikkinen <tero.heikkinen@qt.io>
2023-05-03 18:28:05 +0300

    Provisioning: Upgrade Xcode version to 14.3 for macOS 13
    Change also upgrades command line tools
    
    Task-number: QTQAINFRA-5485
    Pick-to: 6.5
    Change-Id: I057e4c850c3c7ba938002a2ec444637ae9f51cf4
    Reviewed-by: Heikki Halmet <heikki.halmet@qt.io>
    Reviewed-by: Tero Heikkinen <tero.heikkinen@qt.io>

--- coin/provisioning/qtci-macos-13-arm/21-install-commandlinetools.sh
+++ coin/provisioning/qtci-macos-13-arm/21-install-commandlinetools.sh
@@ -43,9 +43,9 @@ set -ex
 
 # shellcheck source=../common/macos/install-commandlinetools.sh
 source "${BASH_SOURCE%/*}/../common/macos/install-commandlinetools.sh"
-version="14.2"
+version="14.3"
 packageName="Command_Line_Tools_for_Xcode_$version.dmg"
 url="http://ci-files01-hki.intra.qt.io/input/mac/$packageName"
-sha1="f9d18da696bc54755bd85fb4f42c0a8866bdade6"
+sha1="bd255759d8733c7ea820ebc6ad8f65114e42d36d"
 
 InstallCommandLineTools $url $url $sha1 $packageName $version
