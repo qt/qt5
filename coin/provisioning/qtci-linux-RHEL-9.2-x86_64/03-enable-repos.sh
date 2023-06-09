eb0c9fa8f015e9044f26732ea1978cbbe45722fb
Tero Heikkinen <tero.heikkinen@qt.io>
2023-06-09 10:49:57 +0300

    Upgrade RHEL 9.0 to version 9.2
    RHEL 9.2 will replace RHEL 9.0 in yaml configurations
    at first and RHEL 9.0 scripts are still kept for some time.
    
    RHEL 9.0 scripts will be removed later.
    
    Task-number: QTQAINFRA-5853
    Change-Id: Iff463f26ba6bcb7962b4f8bafbf88aa7c13392c1
    (cherry picked from commit 9ea71ea245644a2c0d78b0a3f9a9964e151ef318)
    Reviewed-by: Heikki Halmet <heikki.halmet@qt.io>
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>

---
+++ coin/provisioning/qtci-linux-RHEL-9.2-x86_64/03-enable-repos.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+#############################################################################
+##
+## Copyright (C) 2023 The Qt Company Ltd.
+## Contact: https://www.qt.io/licensing/
+##
+## This file is part of the provisioning scripts of the Qt Toolkit.
+##
+## $QT_BEGIN_LICENSE:LGPL$
+## Commercial License Usage
+## Licensees holding valid commercial Qt licenses may use this file in
+## accordance with the commercial license agreement provided with the
+## Software or, alternatively, in accordance with the terms contained in
+## a written agreement between you and The Qt Company. For licensing terms
+## and conditions see https://www.qt.io/terms-conditions. For further
+## information use the contact form at https://www.qt.io/contact-us.
+##
+## GNU Lesser General Public License Usage
+## Alternatively, this file may be used under the terms of the GNU Lesser
+## General Public License version 3 as published by the Free Software
+## Foundation and appearing in the file LICENSE.LGPL3 included in the
+## packaging of this file. Please review the following information to
+## ensure the GNU Lesser General Public License version 3 requirements
+## will be met: https://www.gnu.org/licenses/lgpl-3.0.html.
+##
+## GNU General Public License Usage
+## Alternatively, this file may be used under the terms of the GNU
+## General Public License version 2.0 or (at your option) the GNU General
+## Public license version 3 or any later version approved by the KDE Free
+## Qt Foundation. The licenses are as published by the Free Software
+## Foundation and appearing in the file LICENSE.GPL2 and LICENSE.GPL3
+## included in the packaging of this file. Please review the following
+## information to ensure the GNU General Public License requirements will
+## be met: https://www.gnu.org/licenses/gpl-2.0.html and
+## https://www.gnu.org/licenses/gpl-3.0.html.
+##
+## $QT_END_LICENSE$
+##
+#############################################################################
+
+set -ex
+
+echo "set WritePreparedUpdates=false" | sudo tee -a /etc/PackageKit/PackageKit.conf
+sudo systemctl stop packagekit
+sudo systemctl disable packagekit
+sudo yum -y remove PackageKit gnome-software
+
+sudo subscription-manager config --rhsm.manage_repos=1
+sudo subscription-manager refresh
+
+# sudo yum config-manager --enable 'rhceph-4-tools-for-rhel-8-x86_64-rpms'
+sudo yum config-manager --enable 'codeready-builder-for-rhel-9-x86_64-rpms'
+sudo yum config-manager --enable 'rhel-9-for-x86_64-baseos-rpms'
+sudo yum config-manager --enable 'rhel-9-for-x86_64-appstream-rpms'
+# Epel is required for 'double-conversion-devel', 'libsqlite3x' and 'p7zip'
+sudo dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
+
+sudo yum clean all
+# As well as this fetching the repository data, we also get a printout of the used repos
+sudo yum repolist
