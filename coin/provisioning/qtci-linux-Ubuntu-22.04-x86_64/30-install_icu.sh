54eb300535a828ea6778a882319a7cf691cde2e7
Matti Paaso <matti.paaso@qt.io>
2023-06-21 10:28:07 +0300

    Change icu version into 73.2
    Task-number: QTQAINFRA-5654
    Change-Id: I93e671447569ff2c34204b645f39c8b4ec841ac6
    Reviewed-by: Jani Heikkinen <jani.heikkinen@qt.io>

--- coin/provisioning/qtci-linux-Ubuntu-22.04-x86_64/30-install_icu.sh
+++ coin/provisioning/qtci-linux-Ubuntu-22.04-x86_64/30-install_icu.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Copyright (C) 2021 The Qt Company Ltd.
+# Copyright (C) 2023 The Qt Company Ltd.
 # SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 # shellcheck source=../common/unix/DownloadURL.sh
@@ -9,16 +9,16 @@ set -ex
 
 # This script will install ICU
 
-icuVersion="60.2"
+icuVersion="73.2"
 icuLocationLib="/opt/icu/lib64"
 icuLocationInclude="/opt/icu/include"
-sha1="2b972d8897783c08dfe1e52af49216ed92656736"
-baseBinaryPackageURL="http://ci-files01-hki.ci.qt.io/input/icu/$icuVersion/icu-linux-g++-Ubuntu18.04-x64_60_2.7z"
-baseBinaryPackageExternalURL="http://master.qt.io/development_releases/prebuilt/icu/prebuilt/$icuVersion/icu-linux-g++-Ubuntu18.04-x64_60_2.7z"
+sha1="cb00d9594eea82e7164e3c2311307ad364e19ce8"
+baseBinaryPackageURL="http://ci-files01-hki.ci.qt.io/input/icu/$icuVersion/icu-linux-g++-Ubuntu22.04-x64.7z"
+baseBinaryPackageExternalURL="http://master.qt.io/development_releases/prebuilt/icu/prebuilt/$icuVersion/icu-linux-g++-Ubuntu22.04-x64.7z"
 
-sha1Dev="416c89d3ded143ea1d4fcc688dce02b01aaa9ee2"
-develPackageURL="http://ci-files01-hki.ci.qt.io/input/icu/$icuVersion/icu-linux-g++-Ubuntu18.04-x64-devel_60_2.7z"
-develPackageExternalURL="http://master.qt.io/development_releases/prebuilt/icu/prebuilt/$icuVersion/icu-linux-g++-Ubuntu18.04-x64-devel_60_2.7z"
+sha1Dev="6b9da2fa5fd88db88e9957ee5e3cf9dbcd08fe6b"
+develPackageURL="http://ci-files01-hki.ci.qt.io/input/icu/$icuVersion/icu-linux-g++-Ubuntu22.04-x64-devel.7z"
+develPackageExternalURL="http://master.qt.io/development_releases/prebuilt/icu/prebuilt/$icuVersion/icu-linux-g++-Ubuntu22.04-x64-devel.7z"
 
 echo "Installing custom ICU $icuVersion $sha1 packages on CentOS to $icuLocationLib"
 
