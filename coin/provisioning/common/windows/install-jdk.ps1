d8d615259a2d17639ba138cac3982389d8a40bfb
Matti Paaso <matti.paaso@qt.io>
2023-06-26 09:17:35 +0300

    Change Java 8 into Java 11 for Windows x86
    Jenkins dropped Java 8 support and Windows x86 is still used in
    packaging and RTA.
    
    Change-Id: I001aacd38147a21944ecaa7223414226a6c500ff
    Reviewed-by: Jani Heikkinen <jani.heikkinen@qt.io>
    (cherry picked from commit 28e1b89e42184bf8a268c217411cb9f45a89e999)

--- coin/provisioning/common/windows/install-jdk.ps1
+++ coin/provisioning/common/windows/install-jdk.ps1
@@ -45,19 +45,24 @@ if (Is64BitWinHost) {
     $version = "11.0.12"
     $arch = "x64"
     $sha1 = "135ffd1c350509729551876232a5354070732e92"
+    $installdir = "C:\Program Files\Java\jdk-$version"
+    $url_cache = "\\ci-files01-hki.ci.qt.io\provisioning\windows\jdk-" + $version + "-windows-" + $arch + ".exe"
 } else {
-    $version = "8u144"
-    $arch = "i586"
-    $sha1 = "3b9ab95914514eaefd72b815c5d9dd84c8e216fc"
+    $version = "11.0.11.9"
+    $arch = "x86-32"
+    $sha1 = "a861e994208ee85bf83a76105f6858feeb6fbb33"
+    $installdir = "C:\Program Files\AdoptOpenJDK\jdk-$version-hotspot"
+    $url_cache = "\\ci-files01-hki.ci.qt.io\provisioning\windows\OpenJDK11U-jdk_x86-32_windows_hotspot_11.0.11_9.msi"
 }
 
-$installdir = "C:\Program Files\Java\jdk-$version"
-
-$url_cache = "\\ci-files01-hki.intra.qt.io\provisioning\windows\jdk-" + $version + "-windows-" + $arch + ".exe"
 # NOTE! Official URL is behind login portal. It can't be used whit this script instead it need to be fetched to $url_cache first
 # java 11: https://www.oracle.com/java/technologies/downloads/#java11-windows
 # java 8: $official_url = "http://download.oracle.com/otn-pub/java/jdk/8u144-b01/090f390dda5b47b9b721c7dfaa008135/jdk-" + $version + "-windows-" + $arch + ".exe"
-$javaPackage = "C:\Windows\Temp\jdk-$version.exe"
+if (Is64BitWinHost) {
+    $javaPackage = "C:\Windows\Temp\jdk-$version.exe"
+} else {
+    $javaPackage = "C:\Windows\Temp\jdk-$version.msi"
+}
 
 Write-Host "Fetching Java SE $version..."
 $ProgressPreference = 'SilentlyContinue'
@@ -65,7 +70,11 @@ Write-Host "...from local cache"
 Download $url_cache $url_cache $javaPackage
 Verify-Checksum $javaPackage $sha1
 
-Run-Executable "$javaPackage" "/s SPONSORS=0"
+if (Is64BitWinHost) {
+    Run-Executable "$javaPackage" "/s SPONSORS=0"
+} else {
+    Run-Executable "msiexec" "/quiet /i $javaPackage"
+}
 Remove "$javaPackage"
 
 Write-Host "Remove Java update from startup"
