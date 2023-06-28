a2d926fdd118504fa3536e837d7bef92684e0b33
Simo Fält <simo.falt@qt.io>
2023-06-28 12:40:43 +0300

    Provisioning: Copy backported .deb files for sbuild
    These are required to successfully build debian packages for
    Ubuntu Focal.
    
    Change-Id: I708f3716ba8d1251bfae22da325e8e27f0fb80fe
    Reviewed-by: Toni Saario <toni.saario@qt.io>
    (cherry picked from commit 5b30b2f4321ba84c4b34a1198a6d194af164881e)
    Reviewed-by: Jukka Jokiniva <jukka.jokiniva@qt.io>

---
+++ coin/provisioning/qtci-linux-Debian-11.6-aarch64/93-download-files-for-sbuild.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# Copyright (C) 2023 The Qt Company Ltd.
+# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+# Get required deb files for sbuild to build qt debian packages for Ubuntu focal
+mkdir -p /home/qt/debian_packages
+cd /home/qt/debian_packages || exit
+# Backported cmake 3.24
+wget https://ci-files01-hki.intra.qt.io/input/debian/cmake/amd64-focal/cmake-3.24-deb.tar.gz
+tar xzf cmake-3.24-deb.tar.gz
+# get rest of ready made Ubuntu focal arm debian packages
+# so that sbuild can find those
+# QtWebEngine dependencies
+wget http://ci-files01-hki.ci.qt.io/input/debian/libuv1/arm64-focal/libuv1_1.43.0.tar.gz
+tar -xzf libuv1_1.43.0.tar.gz
+rm -rf libuv1_1.43.0.tar.gz
+wget http://ci-files01-hki.ci.qt.io/input/debian/nghttp2/arm64-focal/nghttp2_1.43.0.tar.gz
+tar -xzf nghttp2_1.43.0.tar.gz
+rm -rf nghttp2_1.43.0.tar.gz
+wget http://ci-files01-hki.ci.qt.io/input/debian/nodejs/arm64-focal/nodejs_12.22.9.tar.gz
+tar -xzf nodejs_12.22.9.tar.gz
+rm -rf nodejs_12.22.9.tar.gz
+# get ICU
+wget http://ci-files01-hki.ci.qt.io/input/debian/icu/arm64-focal/libicu-56.1-qt_56.1-1_arm64.deb
+wget http://ci-files01-hki.ci.qt.io/input/debian/icu/arm64-focal/libicu-56.1-qt-dev_56.1-1_arm64.deb
+
+
+
+
+
+
