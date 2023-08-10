51e36a6c807286010cce28d9f107849671fe4605
Fredrik Ålund <fredrik.alund@mimer.com>
2023-08-10 15:37:43 +0200

    Fix provisioning script for Mimer SQL on Windows
    When building on Windows in the CI environment the Mimer SQL libraries
    where not found. The updated script set an environment variable that is
    is when building the QtSql Mimer SQL plugin
    
    Fixes: QTBUG-111219
    Change-Id: Id486a5a3b37d90cd19180f7900456eeae910d1de
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit 7200dcc0166edd7cb3353db3efcde72b9c5f6063)

--- coin/provisioning/common/windows/mimersql.ps1
+++ coin/provisioning/common/windows/mimersql.ps1
@@ -44,20 +44,24 @@
 
 $version = "1107b"
 
-$url_cache = "\\ci-files01-hki.intra.qt.io\provisioning\windows\MimerSqlX64Windows" + $version + ".exe"
-$url_official = "https://download.mimer.com/pub/dist/windows/MimerSqlX64Windows" + $version + ".exe"
-$mimersqlPackage = "C:\Windows\Temp\MimerSqlX64Windows" + $version + ".exe"
-$sha1 = "e27bb6bdbd5cbd895a64b70051e3e5346f738957"
+$url_cache = "\\ci-files01-hki.ci.qt.io\provisioning\windows\MimerSQLInstaller_x64_" + $version + ".exe"
+$url_official = "https://install.mimer.com/qt/windows_" + $version + "/MimerSQLInstaller_x64.exe"
+$mimersqlPackage = "C:\Windows\Temp\MimerSQLInstaller_x64_" + $version + ".exe"
+$sha1 = "A709A06EA1D897B13FA10DBDD4BE3BD0FEB04B28"
+$mimer_dir="c:\MimerSQL"
+
 Download $url_official $url_cache $mimersqlPackage
 Verify-Checksum $mimersqlPackage $sha1
-Run-Executable "$mimersqlPackage" "/install /passive"
+Run-Executable "$mimersqlPackage" "/install InstallFolder=$mimer_dir /passive"
+
+Set-EnvironmentVariable "MIMERSQL_DEV_ROOT" "$mimer_dir\dev"
 
 Write-Host "Cleaning $mimersqlPackage.."
-Remove "$mimersqlPackage"
-Remove "C:\Program Files\Mimer SQL Experience 11.0\dev\include\odbcinst.h"
-Remove "C:\Program Files\Mimer SQL Experience 11.0\dev\include\Sql.h"
-Remove "C:\Program Files\Mimer SQL Experience 11.0\dev\include\sqlext.h"
-Remove "C:\Program Files\Mimer SQL Experience 11.0\dev\include\sqltypes.h"
-Remove "C:\Program Files\Mimer SQL Experience 11.0\dev\include\sqlucode.h"
+#Remove "$mimersqlPackage"
+Remove "$mimer_dir\dev\include\odbcinst.h"
+Remove "$mimer_dir\dev\include\Sql.h"
+Remove "$mimer_dir\dev\include\sqlext.h"
+Remove "$mimer_dir\dev\include\sqltypes.h"
+Remove "$mimer_dir\dev\include\sqlucode.h"
 
 Write-Output "Mimer SQL = $version" >> ~\versions.txt
