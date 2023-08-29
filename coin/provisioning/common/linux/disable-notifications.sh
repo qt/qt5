54f555c6a45f27984a95f55687f28a0fe3403ad9
Lucie Gérard <lucie.gerard@qt.io>
2023-08-29 13:59:54 +0200

    Add license header to provisioning scripts missing one
    Task-number: QTBUG-108364
    Change-Id: I5b1d53aad917ed38f9e26e1fcc1b97b921c0603a
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>
    (cherry picked from commit ae21de72f32d612f152e19428844fc9f7f4a413f)

--- coin/provisioning/common/linux/disable-notifications.sh
+++ coin/provisioning/common/linux/disable-notifications.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+#Copyright (C) 2023 The Qt Company Ltd
+#SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 gsettings set org.gnome.desktop.notifications.application:/update-manager/ enable false
 gsettings set org.gnome.desktop.notifications show-banners false
