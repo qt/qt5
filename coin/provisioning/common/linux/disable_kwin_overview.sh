6ffc33803162f5f3f5966cd5840c0d8a94dbae1a
Axel Spoerl <axel.spoerl@qt.io>
2023-08-31 13:50:16 +0200

    OpenSuSE: disable kwin overview
    The KDE window manager kwin launches window overview, when the mouse
    remains in the top left for about 1 second. This causes flakiness in
    autotests, if they move the mouse to the top left.
    
    => Disable the overview function, which is enabled by default.
    
    Change-Id: I43925d15db99f5d40b6cc0cdba39203513e8c59f
    Reviewed-by: Toni Saario <toni.saario@qt.io>
    (cherry picked from commit 71b55915f02bfe035215494d941fcdb842e22c02)
    Reviewed-by: Tero Heikkinen <tero.heikkinen@qt.io>

---
+++ coin/provisioning/common/linux/disable_kwin_overview.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+# Copyright (C) 2023 The Qt Company Ltd.
+# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+set -e
+set -f
+QT_USER="qt"
+CONFDIR=".config"
+KWINRC="kwinrc"
+KWC5=`which kwriteconfig5`
+
+if [ -z "$KWC5" ]; then
+    echo "(WW) kwriteconfig5 script not found."
+    echo "---- skipping overview disabling."
+    exit 0;
+fi
+
+echo "---- ensuring window overview is disabled in kwin."
+
+# skip if user qt does not exist
+echo "---- checking user $QT_USER"
+if grep -q "^$QT_USER:" /etc/passwd; then
+    echo "(**) found user $QT_USER"
+else
+    echo "(WW) user $QT_USER not found."
+    echo "---- skipping overview disabling."
+    exit 0;
+fi
+
+CONFIGFILE="/home/$QT_USER/$CONFDIR/$KWINRC"
+
+# Check kwinrc existence
+echo "---- checking for kwinrc."
+if [ -f "$CONFIGFILE" ]; then
+   echo "(**) found kwinrc at $CONFIGFILE. Disabling overview."
+
+   $KWC5 --file "$CONFIGFILE" --group Effect-windowview --key BorderActivateAll "9"
+   $KWC5 --file "$CONFIGFILE" --group Plugins --key windowOverview "false"
+
+else
+    echo "(WW) no kwinrc found at $CONFIGFILE."
+    echo "(WW) exiting."
+fi
