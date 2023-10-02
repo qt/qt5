185b7219a4f8b65e3dab71d14eeaacbb3d4fed76
Bartlomiej Moskal <bartlomiej.moskal@qt.io>
2023-10-02 12:45:16 +0200

    Coin: Fix install-ffmpeg calls for android
    There is a specialized script to install FFmpeg for Android. Use it
    instead of common install-ffmpeg.sh (which does not work for "android"
    parameter).
    
    Task-number: QTBUG-110805
    Task-number: QTBUG-114954
    Task-number: QTBUG-113980
    Pick-to: 6.6 6.5
    Change-Id: I2409e0f8ffe0f8b28eadce4738c2551fe00a7fbf
    Reviewed-by: Assam Boudjelthia <assam.boudjelthia@qt.io>

--- coin/provisioning/qtci-linux-RHEL-8.6-x86_64/90-install-ffmpeg.sh
+++ coin/provisioning/qtci-linux-RHEL-8.6-x86_64/90-install-ffmpeg.sh
@@ -3,4 +3,4 @@
 #SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 source "${BASH_SOURCE%/*}/../common/unix/install-ffmpeg.sh" "linux"
-source "${BASH_SOURCE%/*}/../common/unix/install-ffmpeg.sh" "android"
+source "${BASH_SOURCE%/*}/../common/unix/install-ffmpeg-android.sh" "android-x86_64"
