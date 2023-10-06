2a7c5cdf71df69b00407bf5bfa92d5d3a39b3d05
Ville-Pekka Karhu <ville-pekka.karhu@qt.io>
2023-10-06 10:15:32 +0300

    Provisioning: A better fix for enabling zsh history
    Change-Id: I089ae9ea42b51b66009c1e2c50910ea689500aa1
    Reviewed-by: Simo Fält <simo.falt@qt.io>

--- coin/provisioning/common/macos/enable_zsh_history_and_viminfo.sh
+++ coin/provisioning/common/macos/enable_zsh_history_and_viminfo.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 # Copyright (C) 2023 The Qt Company Ltd.
 
-[ -f /Users/qt/.zsh_history ] && sudo chown qt:staff /Users/qt/.zsh_history
-[ -f /Users/qt/.viminfo ] && sudo chown qt:staff /Users/qt/.viminfo
+if [ -f /Users/qt/.zsh_history ]
+then
+  sudo chown qt:staff /Users/qt/.zsh_history
+fi
+
+if [ -f /Users/qt/.viminfo ]
+then
+  sudo chown qt:staff /Users/qt/.viminfo
+fi
