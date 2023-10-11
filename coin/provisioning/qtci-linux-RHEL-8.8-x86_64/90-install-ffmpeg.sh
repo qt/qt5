2d3245e0eacf14dd0c488b436cf8160a49723bd6
Bartlomiej Moskal <bartlomiej.moskal@qt.io>
2023-10-11 10:53:15 +0200

    Coin: Install FFmpeg for Android on missing targets
    On qtci-macos-13-x86_64 host CI tries to build and run the test on
    Android (arm64) while we still haven't managed to install FFmpeg for it.
    The same situation happened with qtci-linux-RHEL-8.8-x86_64 and Android
    x86.
    
    Task-number: QTBUG-110805
    Task-number: QTBUG-114954
    Task-number: QTBUG-113980
    Pick-to: 6.6 6.5
    Change-Id: If953221b104d5bb8fce9d48f8ecd977c2e80c41d
    Reviewed-by: Assam Boudjelthia <assam.boudjelthia@qt.io>

--- coin/provisioning/qtci-linux-RHEL-8.8-x86_64/90-install-ffmpeg.sh
+++ coin/provisioning/qtci-linux-RHEL-8.8-x86_64/90-install-ffmpeg.sh
@@ -4,3 +4,4 @@
 
 source "${BASH_SOURCE%/*}/../common/unix/install-ffmpeg.sh" "linux"
 source "${BASH_SOURCE%/*}/../common/unix/install-ffmpeg-android.sh" "android-x86_64"
+source "${BASH_SOURCE%/*}/../common/unix/install-ffmpeg-android.sh" "android-x86"
--- coin/provisioning/qtci-macos-13-x86_64/90-install-ffmpeg.sh
+++ coin/provisioning/qtci-macos-13-x86_64/90-install-ffmpeg.sh
@@ -5,3 +5,4 @@
 set -ex
 
 source "${BASH_SOURCE%/*}/../common/unix/install-ffmpeg.sh" "macos-universal"
+source "${BASH_SOURCE%/*}/../common/unix/install-ffmpeg-android.sh" "android-arm64"
