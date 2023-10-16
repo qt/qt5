bae2268c4751f4b8948e5e17f30cb1349b69d010
Alexandru Croitor <alexandru.croitor@qt.io>
2023-10-16 11:56:28 +0200

    CMake: Update provisioned CMake to version 3.27.7
    We want access to the debugger, and also to be in sync with the CMake
    shipped with the Qt SDK.
    
    Task-number: QTBUG-118138
    Change-Id: I4b43b9543e8e6ef23c9608a8e1c623ee16edfbd5
    Reviewed-by: Alexey Edelev <alexey.edelev@qt.io>
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

--- coin/provisioning/common/linux/cmake_linux.sh
+++ coin/provisioning/common/linux/cmake_linux.sh
@@ -11,11 +11,11 @@ source "${BASH_SOURCE%/*}/../unix/InstallFromCompressedFileFromURL.sh"
 # shellcheck source=../unix/SetEnvVar.sh
 source "${BASH_SOURCE%/*}/../unix/SetEnvVar.sh"
 
-majorminorversion="3.24"
-version="3.24.2"
+majorminorversion="3.27"
+version="3.27.7"
 PrimaryUrl="http://ci-files01-hki.ci.qt.io/input/cmake/cmake-$version-linux-x86_64.tar.gz"
 AltUrl="https://cmake.org/files/v$majorminorversion/cmake-$version-linux-x86_64.tar.gz"
-SHA1="12627c808674ee5f5bd3a6982176613bbd3ad9db"
+SHA1="83ab00c8432a71cfc81e933ee7b3c3e14d50ad6f"
 targetFolder="/opt/cmake-$version"
 appPrefix="cmake-$version-linux-x86_64"
 
