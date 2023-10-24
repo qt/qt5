f7fb9e99f7f5704a8dbb9de14de738eff514df0d
Tony Sarajärvi <tony.sarajarvi@qt.io>
2023-10-24 08:10:56 +0000

    Update llvm-mingw to 17.0.3
    Change-Id: I2a44fd8d5dd75771539926a55c5da784267d595f
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: Matti Paaso <matti.paaso@qt.io>

--- coin/provisioning/qtci-windows-10_22H2-x86_64/25-llvm-mingw.ps1
+++ coin/provisioning/qtci-windows-10_22H2-x86_64/25-llvm-mingw.ps1
@@ -4,17 +4,17 @@
 . "$PSScriptRoot\..\common\windows\helpers.ps1"
 
 # This script installs LLVM-Mingw by mstorsjo
-# https://github.com/mstorsjo/llvm-mingw/releases/tag/20220906
+# https://github.com/mstorsjo/llvm-mingw/releases/tag/20220323
 
-$zip = Get-DownloadLocation "llvm-mingw-20220906-ucrt-x86_64.zip"
-$url_cache = "http://ci-files01-hki.ci.qt.io/input/windows/llvm-mingw-20220906-ucrt-x86_64.zip"
-$url_official = "https://github.com/mstorsjo/llvm-mingw/releases/download/20220906/llvm-mingw-20220906-ucrt-x86_64.zip"
+$zip = Get-DownloadLocation "llvm-mingw-20231017-ucrt-x86_64.zip"
+$url_cache = "http://ci-files01-hki.ci.qt.io/input/windows/llvm-mingw-20231017-ucrt-x86_64.zip"
+$url_official = "https://github.com/mstorsjo/llvm-mingw/releases/download/20231017/llvm-mingw-20231017-ucrt-x86_64.zip"
 
 Download $url_official $url_cache $zip
-Verify-Checksum $zip "51ff525eefa4f5db905cc7b4c8b56079c3baed65"
+Verify-Checksum $zip "1a39032b5add2f06fe0dfa56d0279b313b974da3"
 Extract-7Zip $zip C:\
 
-Rename-Item C:\llvm-mingw-20220906-ucrt-x86_64 C:\llvm-mingw
+Rename-Item C:\llvm-mingw-20231017-ucrt-x86_64 C:\llvm-mingw
 
-Write-Output "llvm-mingw = 15.0.0" >> ~/versions.txt
+Write-Output "llvm-mingw = 17.0.3" >> ~/versions.txt
 Remove-Item -Path $zip
--- coin/provisioning/qtci-windows-11_22H2-x86_64/25-llvm-mingw.ps1
+++ coin/provisioning/qtci-windows-11_22H2-x86_64/25-llvm-mingw.ps1
@@ -6,15 +6,15 @@
 # This script installs LLVM-Mingw by mstorsjo
 # https://github.com/mstorsjo/llvm-mingw/releases/tag/20220323
 
-$zip = Get-DownloadLocation "llvm-mingw-20220323-ucrt-x86_64.zip"
-$url_cache = "http://ci-files01-hki.ci.qt.io/input/windows/llvm-mingw-20220323-ucrt-x86_64.zip"
-$url_official = "https://github.com/mstorsjo/llvm-mingw/releases/download/20220323/llvm-mingw-20220323-ucrt-x86_64.zip"
+$zip = Get-DownloadLocation "llvm-mingw-20231017-ucrt-x86_64.zip"
+$url_cache = "http://ci-files01-hki.ci.qt.io/input/windows/llvm-mingw-20231017-ucrt-x86_64.zip"
+$url_official = "https://github.com/mstorsjo/llvm-mingw/releases/download/20231017/llvm-mingw-20231017-ucrt-x86_64.zip"
 
 Download $url_official $url_cache $zip
-Verify-Checksum $zip "2b55846635f9e57fd37a19e0ba4183b3176ad57f"
+Verify-Checksum $zip "1a39032b5add2f06fe0dfa56d0279b313b974da3"
 Extract-7Zip $zip C:\
 
-Rename-Item C:\llvm-mingw-20220323-ucrt-x86_64 C:\llvm-mingw
+Rename-Item C:\llvm-mingw-20231017-ucrt-x86_64 C:\llvm-mingw
 
-Write-Output "llvm-mingw = 14.0.0" >> ~/versions.txt
+Write-Output "llvm-mingw = 17.0.3" >> ~/versions.txt
 Remove-Item -Path $zip
