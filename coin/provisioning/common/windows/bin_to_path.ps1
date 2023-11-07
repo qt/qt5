854bc07fc2a6e36a9eb595bdeb42cb746822cf2e
Aku Pietikäinen <aku.pietikainen@qt.io>
2023-11-07 13:39:42 +0000

    Revert "Add usr\bin\ to path on windows"
    This reverts commit 50aa0031a449bbeff8a1350ded038cf85ab5fc30.
    
    Reason for revert: Change replaces PATH and causes some built-in
    commands to be replaced with breaking Git equivalents.
    
    Change-Id: I9e25238c8f84cfc5ad44e1cdfb0a0fe33a60dcfe
    Reviewed-by: Eike Ziller <eike.ziller@qt.io>

--- coin/provisioning/common/windows/bin_to_path.ps1
+++
@@ -1,4 +0,0 @@
-if (-not ($env:PATH -split ';' | Where-Object { $_ -eq "C:\Program Files\Git\usr\bin" })) {
-    & setx PATH "C:\Program Files\Git\usr\bin"
-    Write-Host "Added C:\Program Files\Git\usr\bin to PATH."
-}
--- coin/provisioning/qtci-windows-10_22H2-x86_64/09-bin-to-path.ps1
+++
@@ -1,1 +0,0 @@
-. "$PSScriptRoot\..\common\windows\bin_to_path.ps1"
