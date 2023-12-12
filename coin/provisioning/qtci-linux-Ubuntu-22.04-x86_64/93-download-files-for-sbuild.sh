9e9dbeafa4b065cf60a1df98426377fa0301e69e
Simo Fält <simo.falt@qt.io>
2023-12-12 12:31:00 +0200

    COIN: Update used chroot for Debian packages
    Use Ubuntu 22.04 instead of 20.04
    
    Change-Id: If03ba14555fdcd0c98fab3843bdfd5c2ca1eba3f
    Reviewed-by: Toni Saario <toni.saario@qt.io>
    (cherry picked from commit 54245a4888c6f1e05a5f7a8568e2659507d62aa0)
    Reviewed-by: Jani Heikkinen <jani.heikkinen@qt.io>

--- coin/provisioning/qtci-linux-Ubuntu-22.04-x86_64/93-download-files-for-sbuild.sh
+++ coin/provisioning/qtci-linux-Ubuntu-22.04-x86_64/93-download-files-for-sbuild.sh
@@ -2,24 +2,15 @@
 # Copyright (C) 2023 The Qt Company Ltd.
 # SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
-# Get required deb files for sbuild to build qt debian packages for Ubuntu focal
+# Get required deb files for sbuild to build qt debian packages for Ubuntu jammy
 mkdir -p /home/qt/debian_packages
 cd /home/qt/debian_packages || exit
 # Backported cmake 3.24
-wget https://ci-files01-hki.intra.qt.io/input/debian/cmake/amd64-focal/cmake-3.24-deb.tar.gz
+wget https://ci-files01-hki.intra.qt.io/input/debian/cmake/amd64-jammy/cmake-3.24-deb.tar.gz
 tar xzf cmake-3.24-deb.tar.gz
-# get rest of ready made Ubuntu focal arm debian packages
+rm -rf cmake-3.24-deb.tar.gz
+# get rest of ready made Ubuntu jammy arm debian packages
 # so that sbuild can find those
-# QtWebEngine dependencies
-wget http://ci-files01-hki.ci.qt.io/input/debian/libuv1/amd64-focal/libuv1_1.43.0.tar.gz
-tar -xzf libuv1_1.43.0.tar.gz
-rm -rf libuv1_1.43.0.tar.gz
-wget http://ci-files01-hki.ci.qt.io/input/debian/nghttp2/amd64-focal/nghttp2_1.43.0.tar.gz
-tar -xzf nghttp2_1.43.0.tar.gz
-rm -rf nghttp2_1.43.0.tar.gz
-wget http://ci-files01-hki.ci.qt.io/input/debian/nodejs/amd64-focal/nodejs_12.22.9.tar.gz
-tar -xzf nodejs_12.22.9.tar.gz
-rm -rf nodejs_12.22.9.tar.gz
-# get ICU
-wget http://ci-files01-hki.ci.qt.io/input/debian/icu/amd64-focal/libicu-56.1-qt_56.1-1_amd64.deb
-wget http://ci-files01-hki.ci.qt.io/input/debian/icu/amd64-focal/libicu-56.1-qt-dev_56.1-1_amd64.deb
+
+wget http://ci-files01-hki.ci.qt.io/input/debian/icu/amd64-jammy/libicu-56.1-qt_56.1-1_amd64.deb
+wget http://ci-files01-hki.ci.qt.io/input/debian/icu/amd64-jammy/libicu-56.1-qt-dev_56.1-1_amd64.deb
