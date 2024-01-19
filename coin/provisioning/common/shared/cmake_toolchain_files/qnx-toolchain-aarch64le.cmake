1cdaa78534a2dc1a90aa0fd7ba1fa0340bae8156
Marianne Yrjänä <marianne.yrjana@qt.io>
2024-01-19 17:22:54 +0200

    Set CMAKE_STRIP for QNX builds in toolchain file
    Task-number: QTBUG-120594
    Change-Id: Ie39a51616d1c1bb03382bc633d9cda11f80fc80f
    Reviewed-by: Cristian Adam <cristian.adam@qt.io>
    (cherry picked from commit 02199c9f752e79525c0348f8dc23cf91f2359959)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- coin/provisioning/common/shared/cmake_toolchain_files/qnx-toolchain-aarch64le.cmake
+++ coin/provisioning/common/shared/cmake_toolchain_files/qnx-toolchain-aarch64le.cmake
@@ -23,4 +23,6 @@ set(CMAKE_C_COMPILER_TARGET ${_qnx_toolchain_arch})
 set(CMAKE_CXX_COMPILER q++)
 set(CMAKE_CXX_COMPILER_TARGET ${_qnx_toolchain_arch})
 
+set(CMAKE_STRIP $ENV{QNX_HOST}/usr/bin/ntoaarch64-strip)
+
 set(CMAKE_SYSROOT $ENV{QNX_TARGET})
