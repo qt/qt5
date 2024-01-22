25b2301eecfbc522b58c52969e8b1961c6632c11
Artem Dyomin <artem.dyomin@qt.io>
2024-01-22 13:48:30 +0100

    Retain symlinks upon making universal macOS binaries
    Saving symlinks is important when we build dynamic libs,
    it will be needed for building QtMM + dynamic ffmpeg.
    
    Task-number: QTBUG-120989
    Pick-to: 6.5
    Change-Id: I58c040a81f1fa6a3b316ace864fda0687415fd0a
    Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>
    (cherry picked from commit 231d089553e3924bd75e3adf9802bb1c7478c760)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 0abefd8bba53b4e5a11c6b9db729b355bf8724e3)
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>

--- coin/provisioning/common/macos/makeuniversal.sh
+++ coin/provisioning/common/macos/makeuniversal.sh
@@ -45,18 +45,24 @@ set -e
 
 for dir in "$@"; do
     echo "Processing files in $dir ..."
-    pushd $dir >/dev/null
-    find . -type f | while read f; do
+    pushd "$dir" >/dev/null
+    find . -type f -or -type l | while read -r f; do
         dst="${f:1}"
-        dstdir=$(dirname $dst)
-        mkdir -p $dstdir
-        if [[ ! -f $dst ]]; then
+        dstdir="$(dirname "$dst")"
+        mkdir -p "$dstdir"
+
+        if [[ -L "$f" ]]; then
+            if [[ ! -L "$dst" && ! -d "$dst" && ! -f "$dst" ]]; then
+                echo "Copying symlink $dir/$f to $dst"
+                cp -P -n "$f" "$dst"
+            fi
+        elif [[ ! -f "$dst" ]]; then
             echo "Copying $dir/$f to $dst"
-            cp -c $f $dst
-        elif lipo -info $f >/dev/null 2>&1; then
+            cp -c "$f" "$dst"
+        elif lipo -info "$f" >/dev/null 2>&1; then
             echo "Lipoing $dir/$f into $dst"
-            lipo -create -output $dst $dst $f
-        elif ! diff $f $dst; then
+            lipo -create -output "$dst" "$dst" "$f"
+        elif ! diff "$f" "$dst"; then
             echo "Error: File $f in $dir doesn't match destination $dst"
             exit 1
         fi
