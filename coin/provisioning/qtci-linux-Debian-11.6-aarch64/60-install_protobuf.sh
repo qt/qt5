7b104e18f86cdf7faa4431802d8f409903452ec5
Alexey Edelev <alexey.edelev@qt.io>
2024-02-09 16:43:13 +0100

    Add libprotoc libprotobuf and libgrpc to Debian aarch64 provisioning
    Use the system libraries.
    
    Pick-to: 6.5 6.6 6.7
    Task-number: QTBUG-121579
    Change-Id: I15e0199a8aa41f607af43569a685cced3b522aec
    Reviewed-by: Tatiana Borisova <tatiana.borisova@qt.io>

---
+++ coin/provisioning/qtci-linux-Debian-11.6-aarch64/60-install_protobuf.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Copyright (C) 2024 The Qt Company Ltd.
+# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+sudo DEBIAN_FRONTEND=noninteractive apt -q -y install libprotobuf-dev libgrpc-dev libprotoc-dev protobuf-compiler
