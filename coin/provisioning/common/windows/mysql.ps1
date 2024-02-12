86c53b6cce6dd903fb0a602481dad06cd4f3f50f
Elias Toivola <elias.toivola@qt.io>
2024-02-12 16:40:46 +0200

    Update MySQL and enable it for more Windows targets
    Currently MySQL is enabled only for Windows 10 22h2 msvc2022 and it
    uses deprecated MySQL Connector/C. This change updates MySQL client
    library by replacing MySQL Connector/C with MySQL 8.0 Server package
    which contains a newer client library that works in other
    Windows targets.
    
    This change also enables MySQL in other non-packaging
    Windows 10/11 targets except ARM, as Windows on ARM still
    lacks MySQL support.
    
    Task-number: QTQAINFRA-4999
    Fixes: QTBUG-106097
    Change-Id: I4472d3e441668bada18268fc4486bbbeefc4afd6
    Reviewed-by: Dimitrios Apostolou <jimis@qt.io>
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>

--- coin/provisioning/common/windows/mysql.ps1
+++ coin/provisioning/common/windows/mysql.ps1
@@ -6,16 +6,12 @@
 # This script installs MySQL $version.
 # Both x86 and x64 versions needed when x86 integrations are done on x64 machine
 
-$version = "6.1.11"
+$version = "8.0.36"
 $installFolder = "C:\Utils"
-$officialUrl = "https://downloads.mysql.com/archives/get/p/19/file/mysql-connector-c-${version}-winx64.zip"
-$officialUrlDebug = "https://downloads.mysql.com/archives/get/p/19/file/mysql-connector-${version}-winx64-debug.zip"
-$cacheURl = "http://ci-files01-hki.ci.qt.io/input/windows/mysql-connector-c-${version}-winx64.zip"
-$cacheURlDebug = "http://ci-files01-hki.ci.qt.io/input/windows/mysql-connector-c-${version}-winx64-debug.zip"
-$sha = "93e22a1ba3944a6c8e01d3ea04c1bfb005b238f9"
-$shaDebug = "d54088a9182e2f03b4d6f44c327e341eeab16367"
-$zip = Get-DownloadLocation ("mysql-connector-c-" + $version + "-winx64.zip")
-$zipDebug = Get-DownloadLocation ("mysql-connector-c-" + $version + "-winx64-debug.zip")
+$officialUrl = "https://dev.mysql.com/get/Downloads/MySQL-8.0/mysql-${version}-winx64.zip"
+$cacheURl = "http://ci-files01-hki.ci.qt.io/input/windows/mysql-${version}-winx64.zip"
+$sha = "e5003569386006ccde9000c98e28e28073c1433d"
+$zip = Get-DownloadLocation ("mysql-" + $version + "-winx64.zip")
 
 function Install {
     param(
@@ -32,11 +28,10 @@ function Install {
 }
 
 Install $officialUrl $cacheURl $zip $sha
-Install $officialUrlDebug $cacheURlDebug $zipDebug $shaDebug
 
 # Can't set MySQL_ROOT & MySQL_LIBRARY_DIR variables. Those will enable mysql in every windows target.
 # Let's use ENV_MySQL_* and use it in platform_configs
-Set-EnvironmentVariable "ENV_MySQL_ROOT" "${installFolder}\mysql-connector-c-${version}-winx64"
-Set-EnvironmentVariable "ENV_MySQL_LIBRARY_DIR" "${installFolder}\mysql-connector-c-${version}-winx64\lib\vs14"
+Set-EnvironmentVariable "ENV_MySQL_ROOT" "${installFolder}\mysql-${version}-winx64"
+Set-EnvironmentVariable "ENV_MySQL_LIBRARY_DIR" "${installFolder}\mysql-${version}-winx64\lib"
 
 Write-Output "MySQL = $version" >> ~/versions.txt
