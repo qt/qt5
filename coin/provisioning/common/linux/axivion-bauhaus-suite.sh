507385089194ce6a536f449fd416d694c54f7043
Matti Paaso <matti.paaso@qt.io>
2024-02-29 08:35:27 +0200

    Provisioning: Update Axivion bauhaus-suite
    Update tool from version 7.7.1 into version 7.7.4. License file
    is also updated.
    
    Change-Id: Ia38ed9d58bd4b5e5c1efdacca866faf4bfe95a09
    Reviewed-by: Jukka Jokiniva <jukka.jokiniva@qt.io>

--- coin/provisioning/common/linux/axivion-bauhaus-suite.sh
+++ coin/provisioning/common/linux/axivion-bauhaus-suite.sh
@@ -24,15 +24,15 @@ DownloadAndExtract () {
 
 
 # Axivion Bauhaus Suite
-sourceFile="http://ci-files01-hki.ci.qt.io/input/axivion/bauhaus-suite-7_7_1-x86_64-gnu_linux.tar.gz"
-targetFile="bauhaus-suite-7_7_1-x86_64-gnu_linux.tar.gz"
-sha1="b8de6ed43c302acece835bbf2ef0d173f0f70287"
+sourceFile="http://ci-files01-hki.ci.qt.io/input/axivion/bauhaus-suite-7_7_4-x86_64-gnu_linux.tar.gz"
+targetFile="bauhaus-suite-7_7_4-x86_64-gnu_linux.tar.gz"
+sha1="df17ac0521864a7f1c0b5b3bfded1817cbf47bae"
 cd $HOME
 DownloadAndExtract "$sourceFile" "$sha1" "$targetFile" "$HOME"
 
 mkdir $HOME/.bauhaus
 cd "$HOME/.bauhaus"
-wget http://ci-files01-hki.ci.qt.io/input/axivion/Qt_Evaluation_20240331.key
+wget http://ci-files01-hki.ci.qt.io/input/axivion/Qt_Evaluation_QSR_INTERN_20250118.key
 cd "$HOME"
 
 #Axivion configuration
@@ -41,4 +41,4 @@ configurationTargetFile="axivion_config.tar.gz"
 configSha1="db77f376e0b3ee0f7a74701790d1c8abe792bebe"
 DownloadAndExtract "$configurationFile" "$configSha1" "$configurationTargetFile" "$HOME"
 
-echo "Axivion Bauhaus Suite = 7.7.1" >> ~/versions.txt
+echo "Axivion Bauhaus Suite = 7.7.4" >> ~/versions.txt
