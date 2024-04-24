d4046310f81ea3e88bf0d434385115da18135529
Pavel Dubsky <pavel.dubsky@qt.io>
2024-04-24 17:55:08 +0200

    Update FFmpeg version to n7.0
    Fixes: QTBUG-124482
    Pick-to: 6.7 6.5
    Change-Id: I69727981344d935151f19148960c711b65cb2307
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    Reviewed-by: Jøger Hansegård <joger.hansegard@qt.io>
    (cherry picked from commit eb8bbfe971accc1e1fbdf0f59bec9308c56e13e1)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- coin/provisioning/common/windows/install-ffmpeg.ps1
+++ coin/provisioning/common/windows/install-ffmpeg.ps1
@@ -6,9 +6,9 @@
 # This script will install FFmpeg
 $msys = "C:\Utils\msys64\usr\bin\bash"
 
-$version = "n6.1.1"
+$version = "n7.0"
 $ffmpeg_name = "ffmpeg-" + $version;
-$sha1 = "7AECCED8A0366BE407329B4E176167534233DA12"
+$sha1 = "CC948A547113469E284CA085B9A236F1ECC50843"
 
 $url_cached = "https://ci-files01-hki.ci.qt.io/input/ffmpeg/" + $version + ".zip"
 $url_public = "https://github.com/FFmpeg/FFmpeg/archive/refs/tags/" +$version + ".zip"
