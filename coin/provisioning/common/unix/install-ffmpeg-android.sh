bc854ea729a54ce6c4260e344a56bbefd85bdaf2
Pavel Dubsky <pavel.dubsky@qt.io>
2024-04-24 17:55:08 +0200

    Update FFmpeg version to n7.0
    Fixes: QTBUG-124482
    Pick-to: 6.5
    Change-Id: I69727981344d935151f19148960c711b65cb2307
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    Reviewed-by: Jøger Hansegård <joger.hansegard@qt.io>
    (cherry picked from commit eb8bbfe971accc1e1fbdf0f59bec9308c56e13e1)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit d4046310f81ea3e88bf0d434385115da18135529)
    Reviewed-by: Liang Qi <liang.qi@qt.io>

--- coin/provisioning/common/unix/install-ffmpeg-android.sh
+++ coin/provisioning/common/unix/install-ffmpeg-android.sh
@@ -11,9 +11,9 @@ source "${BASH_SOURCE%/*}/../unix/InstallFromCompressedFileFromURL.sh"
 # shellcheck source=../unix/SetEnvVar.sh
 source "${BASH_SOURCE%/*}/../unix/SetEnvVar.sh"
 
-version="n6.1.1"
+version="n7.0"
 url_public="https://github.com/FFmpeg/FFmpeg/archive/refs/tags/$version.tar.gz"
-sha1="59e0c3c4cc48e9c60073495f8c045329bb21f446"
+sha1="042260ec5013eb29c89c13443a0f42cbe6fbceaa"
 url_cached="http://ci-files01-hki.ci.qt.io/input/ffmpeg/$version.tar.gz"
 ffmpeg_name="FFmpeg-$version"
 
