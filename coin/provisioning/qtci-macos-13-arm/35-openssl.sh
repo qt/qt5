b86099c993d535d947ac2ca885b0a072077047df
Tor Arne Vestbø <tor.arne.vestbo@qt.io>
2024-04-29 13:00:35 +0200

    coin: Build OpenSSL as universal build for macOS ARM
    Change-Id: I6769c4b1c62d9d89c502e90339841191cdfb674f
    Reviewed-by: Timur Pocheptsov <timur.pocheptsov@qt.io>

--- coin/provisioning/qtci-macos-13-arm/35-openssl.sh
+++ coin/provisioning/qtci-macos-13-arm/35-openssl.sh
@@ -5,5 +5,5 @@
 set -ex
 
 # shellcheck source=../common/unix/install-openssl.sh
-source "${BASH_SOURCE%/*}/../common/unix/install-openssl.sh" "macos"
+source "${BASH_SOURCE%/*}/../common/unix/install-openssl.sh" "macos-universal"
 
