d8e6489cef95d128760d225b250a98d4a8ac51cf
Jukka Jokiniva <jukka.jokiniva@qt.io>
2024-04-30 11:01:39 +0000

    Revert "Add Git\usr\bin\ to path on windows"
    This reverts commit 0477a26495538586e23a0e4147013cda23d9ee96.
    
    Reason for revert: This change breaks qtinterfaceframework mingw build
    
    Change-Id: Ib0790c2bee99aa0133925b7663999e80906d882d
    Reviewed-by: Jani Heikkinen <jani.heikkinen@qt.io>

--- coin/provisioning/common/windows/git_usr_bin_to_path.ps1
+++
@@ -1,6 +0,0 @@
-$existingPath = [System.Environment]::GetEnvironmentVariable("PATH", [System.EnvironmentVariableTarget]::Machine)
-if ($existingPath -notlike "*C:\Program Files\Git\usr\bin*") {
-    $newPath = $existingPath + ";C:\Program Files\Git\usr\bin"
-    [System.Environment]::SetEnvironmentVariable("PATH", $newPath, [System.EnvironmentVariableTarget]::Machine)
-    Write-Host "Added C:\Program Files\Git\usr\bin to PATH."
-}
--- coin/provisioning/qtci-windows-10_22H2-x86_64/98-git_usr_bin_to_path.ps1
+++
@@ -1,1 +0,0 @@
-. "$PSScriptRoot\..\common\windows\git_usr_bin_to_path.ps1"
