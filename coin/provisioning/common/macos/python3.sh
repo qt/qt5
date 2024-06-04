65ab75dbe96f507b950b7a90e86a616ad80d548f
Simo Fält <simo.falt@qt.io>
2024-06-04 07:45:27 +0300

    Provisioning: Update Python to 3.11.9 on macOS x86_64
    This is required to create Qt for Python wheels for Android.
    
    Pick-to: 6.8
    Change-Id: Ifc4bab4ef90c81096d8d1c7026b6dead894439d2
    Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>

--- coin/provisioning/common/macos/python3.sh
+++ coin/provisioning/common/macos/python3.sh
@@ -12,25 +12,23 @@ source "${BASH_SOURCE%/*}/../unix/SetEnvVar.sh"
 # shellcheck source=./pip.sh
 source "${BASH_SOURCE%/*}/pip.sh"
 
-PrimaryUrl="http://ci-files01-hki.ci.qt.io/input/mac/python-3.9.6-macos11.pkg"
-AltUrl="https://www.python.org/ftp/python/3.9.6/python-3.9.6-macos11.pkg"
-SHA1="2af5277c2e197719eb4b820430dee5d89e2577b6"
+PrimaryUrl="http://ci-files01-hki.ci.qt.io/input/mac/python-3.11.9-macos11.pkg"
+AltUrl="https://www.python.org/ftp/python/3.11.9/python-3.11.9-macos11.pkg"
+SHA1="d156e22e4f8902c0ebdf466a3a01832e0f0a85d8"
 DestDir="/"
 
 InstallPKGFromURL "$PrimaryUrl" "$AltUrl" "$SHA1" "$DestDir"
 
-InstallPip python3.9
-
 echo "Configure pip"
-/Library/Frameworks/Python.framework/Versions/3.9/bin/pip config --user set global.index https://ci-files01-hki.ci.qt.io/input/python_module_cache
-/Library/Frameworks/Python.framework/Versions/3.9/bin/pip config --user set global.extra-index-url https://pypi.org/simple/
+/Library/Frameworks/Python.framework/Versions/3.11/bin/pip3 config --user set global.index https://ci-files01-hki.ci.qt.io/input/python_module_cache
+/Library/Frameworks/Python.framework/Versions/3.11/bin/pip3 config --user set global.extra-index-url https://pypi.org/simple/
 
-/Library/Frameworks/Python.framework/Versions/3.9/bin/pip3 install virtualenv wheel html5lib
+/Library/Frameworks/Python.framework/Versions/3.11/bin/pip3 install virtualenv wheel html5lib
 
-SetEnvVar "PYTHON3_PATH" "/Library/Frameworks/Python.framework/Versions/3.9/bin"
-SetEnvVar "PIP3_PATH" "/Library/Frameworks/Python.framework/Versions/3.9/bin"
+SetEnvVar "PYTHON3_PATH" "/Library/Frameworks/Python.framework/Versions/3.11/bin"
+SetEnvVar "PIP3_PATH" "/Library/Frameworks/Python.framework/Versions/3.11/bin"
 
 # Install Python certificates. Required at least for emsdk installation
-open /Applications/Python\ 3.9/Install\ Certificates.command
+open /Applications/Python\ 3.11/Install\ Certificates.command
 
-echo "python3 = 3.9.6" >> ~/versions.txt
+echo "python3 = 3.11.9" >> ~/versions.txt
