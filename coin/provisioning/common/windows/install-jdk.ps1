84bf41ba5bfb5be43a0b70ee920e369e94881ca1
Tero Heikkinen <tero.heikkinen@qt.io>
2024-06-07 10:51:18 +0300

    Provisioning: Update Java on Windows x64 to 17
    Java 17 for Android, needed by RTA
    
    Task-number: QTQAINFRA-6385
    Pick-to: 6.8
    Change-Id: I1200fc5fc4dfdac1faea6b262277aae525efc835
    Reviewed-by: Simo Fält <simo.falt@qt.io>

--- coin/provisioning/common/windows/install-jdk.ps1
+++ coin/provisioning/common/windows/install-jdk.ps1
@@ -4,37 +4,32 @@
 . "$PSScriptRoot\helpers.ps1"
 
 # This script will install Java SE
+# https://www.oracle.com/java/technologies/javase/jdk17-archive-downloads.html
+$version = "17.0.10"
+$version_major = "17"
 
-if (Is64BitWinHost) {
-    $version = "11.0.12"
-    $arch = "x64"
-    $sha1 = "135ffd1c350509729551876232a5354070732e92"
-    $installdir = "C:\Program Files\Java\jdk-$version"
-    $url_cache = "\\ci-files01-hki.ci.qt.io\provisioning\windows\jdk-" + $version + "-windows-" + $arch + ".exe"
-} else {
-    $version = "11.0.11.9"
-    $arch = "x86-32"
-    $sha1 = "a861e994208ee85bf83a76105f6858feeb6fbb33"
-    $installdir = "C:\Program Files\AdoptOpenJDK\jdk-$version-hotspot"
-    $url_cache = "\\ci-files01-hki.ci.qt.io\provisioning\windows\OpenJDK11U-jdk_x86-32_windows_hotspot_11.0.11_9.msi"
+$cpu_arch = Get-CpuArchitecture
+switch ($cpu_arch) {
+    x64 {
+        $arch = "x64"
+        $sha1 = "d573091930076c3ffa9f74273cb41cb5c75c5400"
+        $installdir = "C:\Program Files\Java\jdk-$version_major"
+        $url_official = "https://download.oracle.com/java/17/archive/jdk-${version}_windows-${arch}_bin.exe"
+        $url_cache = "\\ci-files01-hki.ci.qt.io\provisioning\windows\jdk-$version-windows-$arch.exe"
+        $javaPackage = "C:\Windows\Temp\jdk-$version.exe"
+        Break
+    }
+    default {
+        throw "Unknown architecture $cpu_arch"
+    }
 }
 
-# NOTE! Official URL is behind login portal. It can't be used whit this script instead it need to be fetched to $url_cache first
-# java 11: https://www.oracle.com/java/technologies/downloads/#java11-windows
-# java 8: $official_url = "http://download.oracle.com/otn-pub/java/jdk/8u144-b01/090f390dda5b47b9b721c7dfaa008135/jdk-" + $version + "-windows-" + $arch + ".exe"
-if (Is64BitWinHost) {
-    $javaPackage = "C:\Windows\Temp\jdk-$version.exe"
-} else {
-    $javaPackage = "C:\Windows\Temp\jdk-$version.msi"
-}
-
-Write-Host "Fetching Java SE $version..."
+Write-Host "Fetching Java SE $version"
 $ProgressPreference = 'SilentlyContinue'
-Write-Host "...from local cache"
-Download $url_cache $url_cache $javaPackage
+Download $url_official $url_cache $javaPackage
 Verify-Checksum $javaPackage $sha1
 
-if (Is64BitWinHost) {
+if ($javaPackage.EndsWith(".exe")) {
     Run-Executable "$javaPackage" "/s SPONSORS=0"
 } else {
     Run-Executable "msiexec" "/quiet /i $javaPackage"
@@ -45,6 +40,6 @@ Write-Host "Remove Java update from startup"
 reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Run" /v SunJavaUpdateSched /f
 
 Set-EnvironmentVariable "JAVA_HOME" "$installdir"
-Add-Path "$installdir\bin"
+Prepend-Path "$installdir\bin"
 
 Write-Output "Java SE = $version $arch" >> ~\versions.txt
