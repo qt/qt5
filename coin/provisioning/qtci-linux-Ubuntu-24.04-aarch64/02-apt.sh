522ac7fd76d51bc024b4ba2fce3b515cb4a16d96
Liang Qi <liang.qi@qt.io>
2024-06-13 11:08:26 +0200

    add support for ffmpeg on Ubuntu 24.04 arm64
    This follows
    * 5a79cf916821e0382dbdec3c544af1d280bbd618 adding 90-install-ffmpeg.sh
    * 83adc86c532f826565729cd963996222033e840c patchelf and -DQT_DEPLOY_FFMPEG=TRUE
    
    Task-number: QTBUG-126289
    Pick-to: 6.8
    Change-Id: I15300c5be750a784490d1e6cce935aeb423609ab
    Reviewed-by: Jøger Hansegård <joger.hansegard@qt.io>
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>

--- coin/provisioning/qtci-linux-Ubuntu-24.04-aarch64/02-apt.sh
+++ coin/provisioning/qtci-linux-Ubuntu-24.04-aarch64/02-apt.sh
@@ -233,6 +233,8 @@ installPackages+=(jq)
 installPackages+=(cmake)
 # extra linkers
 installPackages+=(lld)
+# Fix dependencies in shared ffmpeg libs
+installPackages+=(patchelf)
 
 echo "Running update for apt"
 waitLoop
