f43db8663476af232dd06dba821de682f05cc6a3
Liang Qi <liang.qi@qt.io>
2024-06-20 15:23:33 +0200

    coin: install clang-20 on Ubuntu 24.04 arm64
    Task-number: QTQAINFRA-6203
    Change-Id: If1350343969694940d610f042405248c5aa080a9
    Reviewed-by: Tero Heikkinen <tero.heikkinen@qt.io>

---
+++ coin/provisioning/qtci-linux-Ubuntu-24.04-aarch64/04-clang.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Copyright (C) 2024 The Qt Company Ltd.
+# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+set -ex
+
+curl -L https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
+
+# https://apt.llvm.org
+# Ubuntu
+# Noble (24.04) LTS
+
+# 20
+sudo apt-add-repository 'deb http://apt.llvm.org/noble/ llvm-toolchain-noble-20 main'
+
+sudo apt update
+sudo apt -y install clang-20 lldb-20 lld-20
