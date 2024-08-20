9be8bb7ea6ba125cbbda0b8326e42013b1ea7805
Tero Heikkinen <tero.heikkinen@qt.io>
2024-08-20 13:53:22 +0300

    Provisioning: Install nodejs to Ubuntu 24.04 arm64
    Fixes: QTQAINFRA-6509
    Pick-to: 6.8
    Change-Id: I91bfbb24dadaf74707496ed1167de684b2f9dd61
    Reviewed-by: Liang Qi <liang.qi@qt.io>

---
+++ coin/provisioning/qtci-linux-Ubuntu-24.04-aarch64/03-install-nodejs.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Copyright (C) 2024 The Qt Company Ltd.
+# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+# shellcheck source=../common/linux/install-nodejs.sh
+source "${BASH_SOURCE%/*}/../common/linux/install-nodejs.sh"
