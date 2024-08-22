31583f86a736f73fa7f10a38ce9f34efc6895d68
Alexey Edelev <alexey.edelev@qt.io>
2024-08-22 14:47:27 +0200

    Add the missing provisioning of protobuf on the Ubuntu 24.04 aarch64 machine
    Pick-to: 6.8
    Fixes: QTBUG-128285
    Change-Id: I2b0cd6098fdbcfb07d5210316d95749557970d07
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>

---
+++ coin/provisioning/qtci-linux-Ubuntu-24.04-aarch64/60-install_protobuf.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Copyright (C) 2018 The Qt Company Ltd.
+# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+set -ex
+
+# shellcheck source=../common/unix/install_protobuf.sh
+source "${BASH_SOURCE%/*}/../common/unix/install_protobuf.sh"
+
