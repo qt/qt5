8aa15db27fc81666746c6565b0989de8b03b72b9
Alexandru Croitor <alexandru.croitor@qt.io>
2024-08-27 15:43:43 +0200

    CMake: Provision llvm-mingw for latest Windowss 11 23H2 image
    It was previously missed, and thus qdoc wasn't built for the
    windows-11_23H2-llvm Packaging configuration.
    
    Amends 9975595a5e74e2abc38e4d721d5cd2740e814cf2
    
    Pick-to: 6.7 6.8
    Fixes: QTBUG-121548
    Task-number: QTBUG-111580
    Change-Id: I1142b593f36c8366ceaeac91eba81e16f1a2557d
    Reviewed-by:  Alexey Edelev <alexey.edelev@qt.io>

--- coin/provisioning/qtci-windows-11_23H2-x86_64/08-libclang.ps1
+++ coin/provisioning/qtci-windows-11_23H2-x86_64/08-libclang.ps1
@@ -1,3 +1,4 @@
 # Do not set the default LLVM_INSTALL_DIR for mingw, leave it with msvc for compat
 . "$PSScriptRoot\..\common\windows\libclang.ps1" 64 mingw $False
+. "$PSScriptRoot\..\common\windows\libclang.ps1" 64 llvm-mingw $False
 . "$PSScriptRoot\..\common\windows\libclang.ps1" 64 vs2019
