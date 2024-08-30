963d72200853a808e7f84815056bb422dde2e5cf
Piotr Wierciński <piotr.wiercinski@qt.io>
2024-08-30 12:52:17 +0200

    Provisioning: Pin version of Chrome and chromedriver for WASM tests
    Install Chrome and chromedriver from our cache with
    specific version for better CI stability.
    
    Fixes: QTQAINFRA-6465
    Change-Id: I34f84563c6eb5feea4f112adbd20136974b211c8
    Reviewed-by: Morten Johan Sørvig <morten.sorvig@qt.io>

--- coin/provisioning/qtci-linux-openSUSE-15.5-x86_64/80-install-chrome.sh
+++ coin/provisioning/qtci-linux-openSUSE-15.5-x86_64/80-install-chrome.sh
@@ -5,6 +5,9 @@ set -ex
 # This script will install up-to-date google Chrome needed for Webassembly auto tests.
 
 # shellcheck source=../common/unix/DownloadURL.sh
+source "${BASH_SOURCE%/*}/../common/unix/SetEnvVar.sh"
+
+# shellcheck source=../common/unix/DownloadURL.sh
 source "${BASH_SOURCE%/*}/../common/unix/DownloadURL.sh"
 
 # Webassembly auto tests run requires latest Chrome. Let's use the latest stable one which means we can't cache this
@@ -22,3 +25,14 @@ sudo zypper -nq install --no-confirm google-chrome-stable
 # Install Chromedriver Chromium
 sudo zypper -nq install chromedriver
 
+chromeVersion="chrome-for-testing-115"
+sha="7242ece1055bdbf503527f8e87c4b5da37c3c60e"
+chromeUrl="https://ci-files01-hki.ci.qt.io/input/wasm/chrome/${chromeVersion}.tar.gz"
+target="/tmp/chrome-for-testing-115.tar.gz"
+
+DownloadURL "$chromeUrl" "" "$sha" "$target"
+sudo tar -xzf "$target" -C "${HOME}"
+sudo rm -f "$target"
+
+SetEnvVar "BROWSER_FOR_WASM" "${HOME}/${chromeVersion}/chrome"
+SetEnvVar "CHROMEDRIVER_PATH" "${HOME}/${chromeVersion}/chromedriver"
