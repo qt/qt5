d6402b31f879577706acbb81a101f9b984688321
Alexey Edelev <alexey.edelev@qt.io>
2024-09-07 13:59:35 +0200

    Add the missing archiving tool to QNX toolchain files
    Pick-to: 6.7
    Fixes: QTBUG-128747
    Change-Id: Iee9eca2ef8c53e1171b8d88ded6d313a6b096bea
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>
    (cherry picked from commit f747d2dec7e98fe0ea550cc6165925a1c9ce1451)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- coin/provisioning/common/shared/cmake_toolchain_files/qnx-toolchain-x8664.cmake
+++ coin/provisioning/common/shared/cmake_toolchain_files/qnx-toolchain-x8664.cmake
@@ -24,5 +24,6 @@ set(CMAKE_CXX_COMPILER q++)
 set(CMAKE_CXX_COMPILER_TARGET ${_qnx_toolchain_arch})
 
 set(CMAKE_STRIP $ENV{QNX_HOST}/usr/bin/ntox86_64-strip)
+set(CMAKE_AR $ENV{QNX_HOST}/usr/bin/ntox86_64-ar)
 
 set(CMAKE_SYSROOT $ENV{QNX_TARGET})
