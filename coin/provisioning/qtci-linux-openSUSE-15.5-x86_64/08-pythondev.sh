ab8d3fad65dbf6ab86f19bdc579df472507456e7
Piotr Wierciński <piotr.wiercinski@qt.io>
2024-09-18 10:05:06 +0200

    Provisioning: Remove unnecessary Chrome instalation for WASM testing
    Task-number: QTQAINFRA-6465
    Change-Id: Icec3933871800ef691361da10bd065b72b8c9bb2
    Reviewed-by: Morten Johan Sørvig <morten.sorvig@qt.io>

--- coin/provisioning/qtci-linux-openSUSE-15.5-x86_64/08-pythondev.sh
+++ coin/provisioning/qtci-linux-openSUSE-15.5-x86_64/08-pythondev.sh
@@ -13,7 +13,7 @@ sudo zypper -nq install python-devel python-xml
 
 # install python3
 sudo zypper -nq install python311-base python311-devel python311-pip python311-virtualenv python311-wheel
-python3.11 -m pip install selenium netifaces scache webdriver-manager
+python3.11 -m pip install selenium netifaces scache
 python3.11 -m pip install -r "${BASH_SOURCE%/*}/../common/shared/sbom_requirements.txt"
 
 SetEnvVar "PYTHON3_EXECUTABLE" "/usr/bin/python3.11"
