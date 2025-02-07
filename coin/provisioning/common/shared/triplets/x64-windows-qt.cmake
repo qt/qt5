48cf342e2d0859a794e34c8b53c8453229af6fd9
Alexey Edelev <alexey.edelev@qt.io>
2025-02-07 11:04:26 +0100

    Switch to the static linking of vpkg ports by default
    Prefer static linking for now, to bypass the deployment issues.
    We will switch to dynamic libraries once online installer deliver the
    vcpkg installed libraries.
    
    Change-Id: Iac9a888237742ae886df8fb9b742b45f4727e28a
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- coin/provisioning/common/shared/triplets/x64-windows-qt.cmake
+++ coin/provisioning/common/shared/triplets/x64-windows-qt.cmake
@@ -2,6 +2,9 @@ set(VCPKG_TARGET_ARCHITECTURE x64)
 
 # Default settings of the triplet from the official vcpkg registry
 set(VCPKG_CRT_LINKAGE dynamic)
-set(VCPKG_LIBRARY_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
 
 # Qt custom per-port customizations
+if(PORT MATCHES "openssl")
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
+endif()
