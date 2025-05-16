00d2f82e35ad0827a19bcacce0c182d75361414e
Elias Toivola <elias.toivola@qt.io>
2025-05-16 12:51:08 +0300

    Provisioning: Disable Windows multicast device discoveries
    Our CI does not need some mDNS multicast services by Windows and they
    are spammed by Windows VMs in network with mdns.mcast.net requests.
    
    Pick-to: 6.9 6.8 6.5
    Task-number: QTQAINFRA-6761
    Change-Id: Ie676f94b81e7281ec84d9fbbfa2fd7f1d28dfbca
    Reviewed-by: Tero Heikkinen <tero.heikkinen@qt.io>
    (cherry picked from commit 066371f7cd2618246c7e4fb34d96c77a943a2a53)
    Reviewed-by: Jukka Jokiniva <jukka.jokiniva@qt.io>

---
+++ coin/provisioning/qtci-windows-11_21H2-x86_64/01-disable-windows-mdns.ps1
@@ -0,0 +1,5 @@
+# Copyright (C) 2025 The Qt Company Ltd
+# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+# QTQAINFRA-6761
+. "$PSScriptRoot\..\common\windows\disable-windows-mdns.ps1"
