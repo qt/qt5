0e5937c8a41e30bfe0995a71479027dc1f474fbc
Nils Petter Skålerud <nils.petter.skalerud@qt.io>
2025-05-23 14:19:26 +0200

    Coin, FFmpeg, iOS: Add output parameter to 'install-ffmpeg-ios.sh'
    This patch adds an optional parameter to the provisioning script
    'install-ffmpeg-ios.sh', allowing us to override the output directory
    for which the finalized library is installed.
    
    This is particularly useful for local builds, where we often need
    multiple different versions of FFmpeg installed, for different Qt
    versions.
    
    This patch has no impact for existing coin workflows.
    
    Pick-to: 6.10 6.9 6.8
    Change-Id: Ib0764eb29b84ae8badcfb48424f86316f3b2b650
    Reviewed-by: Simo Fält <simo.falt@qt.io>

--- coin/provisioning/common/unix/install-ffmpeg-ios.sh
+++ coin/provisioning/common/unix/install-ffmpeg-ios.sh
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 # This script will build and install FFmpeg static libs
+# Can take an optional final parameter to control installation directory
 set -ex
 
 # Must match or be lower than the minimum iOS version supported by the version of Qt that is
@@ -13,7 +14,8 @@ source "${BASH_SOURCE%/*}/../unix/ffmpeg-installation-utils.sh"
 
 ffmpeg_source_dir=$(download_ffmpeg)
 ffmpeg_config_options=$(get_ffmpeg_config_options "shared")
-prefix="/usr/local/ios/ffmpeg"
+default_prefix="/usr/local/ios/ffmpeg"
+prefix="${1:-$default_prefix}"
 dylib_regex="^@rpath/.*\.dylib$"
 
 build_ffmpeg_ios() {
