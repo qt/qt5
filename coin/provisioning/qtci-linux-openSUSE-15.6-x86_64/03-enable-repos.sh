2b1b4fdb37cf6f43f32ba4cb30b4ee1e5b3eb0a2
Tim Blechmann <tim.blechmann@qt.io>
2025-08-08 09:43:09 +0800

    provisioning: openSUSE-15.6 - fix gstreamer h264 codec installation
    the gstreamer-1.20-plugin-openh264 package seems to be outdated, as
    opensuse's openh264 repo seems to have moved to gstreamer-1.24 and
    renamed the package to gstreamer-plugin-openh264:
    https://codecs.opensuse.org/openh264/openSUSE_Leap/x86_64/
    
    This partially reverts 69f651b01794c93da20894a7ea5728a332903850.
    
    Pick-to: 6.9
    Change-Id: Ie159ec48f83a7b7d10671d2200f7eb4fbcf93db3
    Reviewed-by: Lars Sutterud <lars.sutterud@qt.io>
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    (cherry picked from commit c52c9ef8b8214c8372715f93a97340c7b2c0c1c2)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- coin/provisioning/qtci-linux-openSUSE-15.6-x86_64/03-enable-repos.sh
+++ coin/provisioning/qtci-linux-openSUSE-15.6-x86_64/03-enable-repos.sh
@@ -10,5 +10,6 @@ sudo sed -i "s#baseurl=.*#baseurl=http://repo-clones.ci.qt.io/repos/opensuse/upd
 sudo sed -i "s#baseurl=.*#baseurl=http://repo-clones.ci.qt.io/repos/opensuse/update/leap/15.6/non-oss/#g" /etc/zypp/repos.d/repo-update-non-oss.repo
 sudo sed -i "s#baseurl=.*#baseurl=http://repo-clones.ci.qt.io/repos/opensuse/update/leap/15.6/backports/#g" /etc/zypp/repos.d/repo-backports-update.repo
 sudo sed -i "s#baseurl=.*#baseurl=http://repo-clones.ci.qt.io/repos/opensuse/update/leap/15.6/sle/#g" /etc/zypp/repos.d/repo-sle-update.repo
+sudo sed -i "s#baseurl=.*#baseurl=http://repo-clones.ci.qt.io/repos/codecs.opensuse.org/openh264/openSUSE_Leap/#g" /etc/zypp/repos.d/repo-openh264.repo
 
 sudo zypper lr -u
