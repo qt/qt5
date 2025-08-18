3e8edbc4ca5222795325b2a44436460e4fc7fe46
Tatiana Borisova <tatiana.borisova@qt.io>
2025-08-18 12:56:38 +0200

    Add openapi-generator-cli.jar for playground/qtopenapi provisioning
    Related to 33dd0de8fb51bb83230b8c2524d5aae28ffc799b
    
    Task-number: QTBUG-138144
    Change-Id: I2aa4a67f8c14de6645f5e821d269474646e616f3
    Reviewed-by: Toni Saario <toni.saario@qt.io>

---
+++ coin/provisioning/common/unix/install_openapi.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# Copyright (C) 2025 The Qt Company Ltd.
+# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+# This script installs openapi generator
+
+# openapi is needed for qtopenapi playground project
+source "${BASH_SOURCE%/*}/DownloadURL.sh"
+source "${BASH_SOURCE%/*}/SetEnvVar.sh"
+
+version="7.15.0"
+PrimaryUrl="http://ci-files01-hki.ci.qt.io/input/qtopenapi/openapi_client_generators/openapi-generator-cli-$version.jar"
+AltUrl="https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/$version/openapi-generator-cli-$version.jar"
+SHA1="bb58e257f724fb46b7f2b309a9fa98e63fd7199f"
+targetFolder="/opt/qt-openapi/"
+targetFile="openapi-generator-cli.jar"
+
+DownloadURL "$PrimaryUrl" "$AltUrl" "$SHA1" "$targetFile"
+
+sudo mkdir "$targetFolder"
+sudo mv "$targetFile" "$targetFolder"
+
+SetEnvVar "PATH" "$targetFolder:\$PATH"
+
