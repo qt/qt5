f9d8882a51a7d175f80c9457db11921d49cea86d
Frederic Lefebvre <frederic.lefebvre@qt.io>
2025-08-21 16:35:44 +0200

    Provisioning: Do not hardcode dynamic libclang version in file names
    libclang-v100-dyn.sh had the libclang version 10.0 hard coded in the
    file name for the Windows scripts. All local provisioning scripts
    calling the latter also had the same version hard coded in their
    respective filenames. libclang-v100-dyn.sh also sets the version to be
    downloaded in a local variable
    
    There is a mismatch between file names and actually installed version
    when the download version is modified.
    
    Remote version from common installer and related local scripts.
    
    Change-Id: I81773ff5a834b6f396ce17af93c6510991de129a
    Reviewed-by: Axel Spoerl <axel.spoerl@qt.io>

---
+++ coin/provisioning/qtci-windows-11_21H2-x86_64/08-libclang-dyn.ps1
@@ -0,0 +1,2 @@
+. "$PSScriptRoot\..\common\windows\libclang-dyn.ps1" 64 vs2019
+. "$PSScriptRoot\..\common\windows\libclang-dyn.ps1" 32 vs2019
--- coin/provisioning/qtci-windows-11_21H2-x86_64/08-libclang-v100-dyn.ps1
+++
@@ -1,2 +0,0 @@
-. "$PSScriptRoot\..\common\windows\libclang-v100-dyn.ps1" 64 vs2019
-. "$PSScriptRoot\..\common\windows\libclang-v100-dyn.ps1" 32 vs2019
