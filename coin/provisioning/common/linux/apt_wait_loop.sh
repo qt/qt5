d657543377836c8bab854c58de893bac32b62750
Tero Heikkinen <tero.heikkinen@qt.io>
2025-08-26 12:07:44 +0300

    Linux: Fix Debian packagekit failing in provisioning
    Target jammy-arm64-debian-pkg-packaging fails in
    waitLoop function, which is also used by Ubuntu platforms.
    
    Fix by extending wait loop for lock paths:
      - /var/lib/dpkg/lock
      - /var/lib/apt/lists/lock (new)
    
    Task-number: QTQAINFRA-7361
    Change-Id: If8d5eacaeb72ea6a36496093bc5c248f61997afb
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- coin/provisioning/common/linux/apt_wait_loop.sh
+++ coin/provisioning/common/linux/apt_wait_loop.sh
@@ -5,9 +5,13 @@
 set -ex
 
 function waitLoop {
+    while sudo fuser /var/lib/dpkg/lock >/dev/null 2>&1; do
+        echo "Waiting for other software managers to finish... 1/2"
+        sleep 0.5
+    done
 
-while sudo fuser /var/lib/dpkg/lock >/dev/null 2>&1 ; do
-    echo "Waiting for other software managers to finish..."
-    sleep 0.5
-done
+    while sudo fuser /var/lib/apt/lists/lock >/dev/null 2>&1; do
+        echo "Waiting for other software managers to finish... 2/2"
+        sleep 0.5
+    done
 }
