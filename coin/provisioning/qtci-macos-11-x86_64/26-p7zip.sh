1126034d5b90f7dc2ea3f024d3dc15ea98712bc3
Aku Pietikäinen <aku.pietikainen@qt.io>
2025-09-05 12:05:17 +0300

    Use common p7zip install script and remove unnecessary brew update
    Change-Id: Id704953bf2eb9a2cfb11dbdb47451aed7bbc736b
    Task-number: QTQAINFRA-7381
    Pick-to: 6.10 6.9 6.8 6.5
    Reviewed-by: Simo Fält <simo.falt@qt.io>

--- coin/provisioning/qtci-macos-11-x86_64/26-p7zip.sh
+++ coin/provisioning/qtci-macos-11-x86_64/26-p7zip.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
-#Copyright (C) 2023 The Qt Company Ltd
+#Copyright (C) 2025 The Qt Company Ltd
 #SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
-# Install 7z to be used from command line
-
 set -ex
 
-brew update
-brew install p7zip
+BASEDIR=$(dirname "$0")
+"$BASEDIR/../common/macos/p7zip.sh"
