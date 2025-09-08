202622c54108f7436ec7ec260285b81f61547c64
Simo Fält <simo.falt@qt.io>
2025-09-08 08:53:44 +0300

    COIN: Fix Debian bookworm pgp key in Ubuntu 22.04 for debootstrap
    Pick-to: 6.9 6.8
    Change-Id: Ie06c37987cf21ee0f8f60528af5d70cbf8b5490d
    Reviewed-by: Jani Heikkinen <jani.heikkinen@qt.io>
    (cherry picked from commit e6eaa4f3ef46243dede9b714ffd5d3f4db8a182b)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- coin/provisioning/qtci-linux-Ubuntu-22.04-x86_64/92-sbuild.sh
+++ coin/provisioning/qtci-linux-Ubuntu-22.04-x86_64/92-sbuild.sh
@@ -39,8 +39,13 @@ EOF
 sudo sbuild-adduser "$LOGNAME"
 newgrp sbuild
 
+# For debian repo we need to update the bookworm release pgp key
+mkdir -p /home/qt/.debian_key_ring
+wget https://ftp-master.debian.org/keys/archive-key-12.asc -O /home/qt/.debian_key_ring/archive-key-12.asc
+gpg --no-default-keyring --keyring=/home/qt/.debian_key_ring/debian_chroot.gpg --import /home/qt/.debian_key_ring/archive-key-12.asc
+
 # Create chroot
-sudo sbuild-createchroot --include=eatmydata,ccache,gnupg,ca-certificates bookworm /srv/chroot/stable-amd64 http://deb.debian.org/debian
+sudo sbuild-createchroot --include=eatmydata,ccache,gnupg,ca-certificates bookworm /srv/chroot/stable-amd64 http://deb.debian.org/debian --keyring=/home/qt/.debian_key_ring/debian_chroot.gpg
 
 # For ubuntu 22.04
 echo "Create chroot for Ubuntu Jammy"
