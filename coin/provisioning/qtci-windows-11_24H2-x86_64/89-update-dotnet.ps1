c969215383d4660728a8eceebe5c3d15bd10a992
Simo Fält <simo.falt@qt.io>
2025-09-10 10:08:25 +0300

    Provisioning: Update dotnet installation
    Install missing workload module.
    
    Pick-to: 6.10
    Change-Id: I8ec4e5389000f043d296d87e7c3fed8e06451cf3
    Reviewed-by: Elias Toivola <elias.toivola@qt.io>
    Reviewed-by: Tero Heikkinen <tero.heikkinen@qt.io>

---
+++ coin/provisioning/qtci-windows-11_24H2-x86_64/89-update-dotnet.ps1
@@ -0,0 +1,6 @@
+# Copyright (C) 2025 The Qt Company Ltd
+# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+. "$PSScriptRoot\..\common\windows\helpers.ps1"
+
+Start-Process -FilePath "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vs_installer.exe" -ArgumentList 'modify --installPath "C:\Program Files\Microsoft Visual Studio\2022\Professional" --add Microsoft.VisualStudio.Workload.ManagedDesktop --quiet' -Wait
