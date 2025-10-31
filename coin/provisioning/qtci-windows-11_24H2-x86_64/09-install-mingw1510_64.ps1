9840747bd7a80024b4e3dc1142d058244df270cd
Tero Heikkinen <tero.heikkinen@qt.io>
2025-10-31 10:21:21 +0200

    Update MinGW to v15.1.0 (x86_64)
    GCC = 15.1.0 MinGW = 12.0.0
    
    Note: Win10 doesn't have MinGW targets anymore as those were moved
    recently into Win11 (ddebcf8fd96e8d6bae67e2d6ecd77e48cd782b71)
    
    But we still keep MinGW 15.1.0 installation as part of the Win10
    provisioning as we did with Win11 before it had MinGW targets.
    
    Reason is to keep installation working for just in case it's needed.
    
    Task-number: QTQAINFRA-7483
    Change-Id: I2cb2e2ef180b11cad61a5131ec47ea2ad207f338
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    Reviewed-by: Tim Blechmann <tim.blechmann@qt.io>
    (cherry picked from commit 295083dd9b4209b3140b05df16d43a28fdc83edf)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

---
+++ coin/provisioning/qtci-windows-11_24H2-x86_64/09-install-mingw1510_64.ps1
@@ -0,0 +1,13 @@
+# Copyright (C) 2025 The Qt Company Ltd
+# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+. "$PSScriptRoot\..\common\windows\install-mingw.ps1"
+
+# This script will install 64-bit MinGW 15.1.0
+# Note! MinGW version is 12.0.0 but the GCC version is 15.1 which is used with the naming of MinGW
+
+$release = "MinGW-w64-x86_64-15.1.0-release-posix-seh-msvcrt-rt_v12-rev0"
+
+$sha1    = "2fbec5f4a0e46099a850b11780500d0208b10591"
+
+InstallMinGW $release $sha1
