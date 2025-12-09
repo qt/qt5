dd0dae532fa60fd52c2fea51c719f3170a4898f2
Simo Fält <simo.falt@qt.io>
2025-12-09 16:32:09 +0200

    Provisioning: Install Workload.NativeDesktop .NET package
    Change-Id: If28badaf1ddb6befca3bacd355f19c4298b96193
    Reviewed-by: Elias Toivola <elias.toivola@qt.io>
    Reviewed-by: Tero Heikkinen <tero.heikkinen@qt.io>

--- coin/provisioning/qtci-windows-11_24H2-x86_64/89-update-dotnet.ps1
+++ coin/provisioning/qtci-windows-11_24H2-x86_64/89-update-dotnet.ps1
@@ -4,3 +4,4 @@
 . "$PSScriptRoot\..\common\windows\helpers.ps1"
 
 Start-Process -FilePath "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vs_installer.exe" -ArgumentList 'modify --installPath "C:\Program Files\Microsoft Visual Studio\2022\Professional" --add Microsoft.VisualStudio.Workload.ManagedDesktop --quiet' -Wait
+Start-Process -FilePath "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vs_installer.exe" -ArgumentList 'modify --installPath "C:\Program Files\Microsoft Visual Studio\2022\Professional" --add Microsoft.VisualStudio.Workload.NativeDesktop --quiet' -Wait
