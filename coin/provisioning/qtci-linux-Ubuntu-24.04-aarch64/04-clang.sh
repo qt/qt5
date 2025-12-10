e9a003f0c3c303e9a4167bcf1e1db84c11945086
Tim Blechmann <tim.blechmann@qt.io>
2025-12-10 13:10:38 +0800

    provisioning: do not install libc++ on ubuntu
    installing libc++-20-dev and libgstreamer1.0-dev on the same system
    causes problems:
    * using the llvm apt repo, it will uninstall libgstreamer1.0-dev
    * using ubuntu's repo, it will break gstreamer's pkg-config integration
    
    reason is that libgstreamer1.0-dev depends on libunwind-dev, but llvm's
    libunwind-20-dev does not provide the pkg-config files.
    as temporary workaround we basically revert
    e5b56d1d37f98bd8780947aca1378bfb4ce4a624.
    
    Pick-to: 6.11
    Change-Id: Ib4626d17339256d56c4764e3167f4e9fe4b9c909
    Reviewed-by: Tero Heikkinen <tero.heikkinen@qt.io>
    Reviewed-by: Liang Qi <liang.qi@qt.io>

--- coin/provisioning/qtci-linux-Ubuntu-24.04-aarch64/04-clang.sh
+++ coin/provisioning/qtci-linux-Ubuntu-24.04-aarch64/04-clang.sh
@@ -14,4 +14,11 @@ curl -L https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
 sudo apt-add-repository 'deb http://apt.llvm.org/noble/ llvm-toolchain-noble-20 main'
 
 sudo apt update
-sudo apt -y install clang-20 lldb-20 lld-20 libc++-20-dev
+sudo apt -y install clang-20 lldb-20 lld-20
+
+# note: installing the libc++ development files conflicts with libgstreamer1.0-dev
+# * installing libunwind-20-dev from apt.llvm.org (as dependency of libc++-20-dev) will
+#   uninstall libgstreamer1.0-dev
+# * installing libunwind-20-dev from the Ubuntu repository will break gstreamer's pkg-config
+#   integration: https://bugs.launchpad.net/ubuntu/+source/llvm-toolchain-20/+bug/2134518
+# sudo apt -y libc++-20-dev
