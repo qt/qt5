3a7915481f0033b11562122233de86477a6ccf51
Elias Toivola <elias.toivola@qt.io>
2026-01-14 17:27:44 +0200

    RHEL 8 & 9: Take rhc into use with subscription activation
    Both of the subscription activation options needs to run
    with sudo as non-root user cannot connect system.
    
    Task-number: QTQAINFRA-7637
    Pick-to: 6.11 6.10 6.8 6.5
    Change-Id: I05ea39e4c4a40b5294cc659e4d2e506ee0118848
    Reviewed-by: Toni Saario <toni.saario@qt.io>

--- coin/provisioning/qtci-linux-RHEL-8.10-x86_64/01-refresh-subscription-manager.sh
+++ coin/provisioning/qtci-linux-RHEL-8.10-x86_64/01-refresh-subscription-manager.sh
@@ -5,14 +5,11 @@
 
 set -e
 
-curl --retry 5 --retry-delay 10 --retry-max-time 60 http://ci-files01-hki.ci.qt.io/input/semisecure/redhat_ak_all_versions.sh -o "/tmp/redhat_ak.sh" &>/dev/null
+curl --retry 5 --retry-delay 10 --retry-max-time 60 http://ci-files01-hki.ci.qt.io/input/semisecure/redhat_ak_all_versions_rhc.sh -o "/tmp/redhat_ak.sh" &>/dev/null
 sudo chmod 755 /tmp/redhat_ak.sh
-/tmp/redhat_ak.sh
+sudo /tmp/redhat_ak.sh
 
 # refresh local certificates
 sudo subscription-manager refresh
-
-# Attach available subscriptions to system. This is needed when subscriptions are renewed.
-sudo subscription-manager attach --auto
 
 sudo rm -f /tmp/redhat_ak.sh
