08cef8ffb2c1b90e31af9fd75d193e6f02c7939b
Tero Heikkinen <tero.heikkinen@qt.io>
2026-01-16 11:15:31 +0200

    Provisioning: Take repo-clones into use in RHEL 10
    Task-number: QTQAINFRA-7614
    Change-Id: If766b511e367a64691a8eb0683c1fdc698d3fa05
    Reviewed-by: Ville-Pekka Karhu <ville-pekka.karhu@qt.io>
    (cherry picked from commit 28f912071803a7b2340eeed93c6468be372af736)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- coin/provisioning/qtci-linux-RHEL-10.0-x86_64/03-enable-repos.sh
+++ coin/provisioning/qtci-linux-RHEL-10.0-x86_64/03-enable-repos.sh
@@ -14,7 +14,7 @@ while sudo fuser /usr/libexec/packagekitd >/dev/null 2>&1; do
 done
 sudo yum -y remove PackageKit gnome-software
 
-sudo subscription-manager config --rhsm.manage_repos=1
+sudo subscription-manager config --rhsm.manage_repos=0
 sudo subscription-manager refresh
 
 # List available RHEL versions and bind with correct one
@@ -25,10 +25,35 @@ sudo subscription-manager release --list
 sudo subscription-manager release --set=10.0
 sudo yum clean all
 
-# sudo yum config-manager --enable 'rhceph-4-tools-for-rhel-8-x86_64-rpms'
-sudo yum config-manager --enable 'codeready-builder-for-rhel-10-x86_64-rpms'
-sudo yum config-manager --enable 'rhel-10-for-x86_64-baseos-rpms'
-sudo yum config-manager --enable 'rhel-10-for-x86_64-appstream-rpms'
+sudo tee "/etc/yum.repos.d/local.repo" > /dev/null <<EOC
+[rhel-10-for-x86_64-baseos-rpms]
+metadata_expire = 86400
+baseurl = http://repo-clones-rhel10.ci.qt.io/rhel-10-for-x86_64-baseos-rpms
+ui_repoid_vars = releasever basearch
+name = Qt Red Hat Enterprise Linux 10 Base OS (RPMs)
+gpgkey = file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release
+enabled = 1
+gpgcheck = 1
+
+[rhel-10-for-x86_64-appstream-rpms]
+metadata_expire = 86400
+baseurl = http://repo-clones-rhel10.ci.qt.io/rhel-10-for-x86_64-appstream-rpms
+ui_repoid_vars = releasever basearch
+name = Qt Red Hat Enterprise Linux 10 Appstream (RPMs)
+gpgkey = file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release
+enabled = 1
+gpgcheck = 1
+
+[codeready-builder-for-rhel-10-x86_64-rpms]
+metadata_expire = 86400
+baseurl = http://repo-clones-rhel10.ci.qt.io/codeready-builder-for-rhel-10-x86_64-rpms
+ui_repoid_vars = releasever basearch
+name = Qt Red Hat Enterprise Linux Codeready Builder (RPMs)
+gpgkey = file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release
+enabled = 1
+gpgcheck = 1
+EOC
+
 # Epel is required for 'double-conversion-devel', 'libsqlite3x' and 'p7zip'
 sudo dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-10.noarch.rpm
 
