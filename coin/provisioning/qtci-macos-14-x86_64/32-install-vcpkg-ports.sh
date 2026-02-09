fd5f44a57bd61805952b9a6a18a64b97409c9537
Tero Heikkinen <tero.heikkinen@qt.io>
2026-02-09 12:24:40 +0200

    Fix macOS 13 install-vcpkg-ports.sh failing in provisioning
    Fix vcpkg install to use correct architecture (arm64/x64)
    as a host when using --host-triplet argument.
    
    Pick-to: 6.11
    Task-number: QTQAINFRA-7691
    Change-Id: I899f6521b47e407e1c9bde1c0f2b83e560894794
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- coin/provisioning/qtci-macos-14-x86_64/32-install-vcpkg-ports.sh
+++ coin/provisioning/qtci-macos-14-x86_64/32-install-vcpkg-ports.sh
@@ -3,7 +3,7 @@
 set -ex
 
 BASEDIR=$(dirname "$0")
-"$BASEDIR/../common/unix/install-vcpkg-ports.sh" arm64-osx-qt --host-triplet=arm64-osx
+"$BASEDIR/../common/unix/install-vcpkg-ports.sh" arm64-osx-qt --host-triplet=x64-osx
 "$BASEDIR/../common/unix/install-vcpkg-ports.sh" x64-osx-qt --host-triplet=x64-osx
 
 python3 -m lipomerge $VCPKG_ROOT/installed/arm64-osx-qt $VCPKG_ROOT/installed/x64-osx-qt $VCPKG_ROOT/installed/universal-osx-qt
