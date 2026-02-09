e41fdd0e356aad6930f751b58c600580fa989ec6
Nils Petter Skålerud <nils.petter.skalerud@qt.io>
2026-02-09 13:35:34 +0100

    Move install-ffmpeg-ios.sh into macos folder
    Currently this script is stored under unix, but it's only ever going to
    be used on macos. This patch moves the script.
    
    No functional changes.
    
    There was a minor conflict resolution in this patch due to macOS 26 not
    being present in this branch.
    
    Pick-to: 6.8
    Change-Id: Iba3d101d360e90ffaa3478689bb002b4e5f65bc1
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    (cherry picked from commit ebe3124831a386b986b462fbd500edc22533e401)
    Reviewed-by: Tero Heikkinen <tero.heikkinen@qt.io>
    (cherry picked from commit 1d998afee6fd2ada529e631e29cd7d3d4ef16e08)

--- coin/provisioning/qtci-macos-15-x86_64/90-install-ffmpeg.sh
+++ coin/provisioning/qtci-macos-15-x86_64/90-install-ffmpeg.sh
@@ -6,4 +6,4 @@ set -ex
 
 source "${BASH_SOURCE%/*}/../common/macos/install-ffmpeg-macos.sh" "macos-universal"
 source "${BASH_SOURCE%/*}/../common/unix/install-ffmpeg-android.sh" "android-arm64" "use_16kb_page_size"
-source "${BASH_SOURCE%/*}/../common/unix/install-ffmpeg-ios.sh"
+source "${BASH_SOURCE%/*}/../common/macos/install-ffmpeg-ios.sh"
