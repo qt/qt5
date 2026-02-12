888f401dda3a17cdad9f08da91606f2a762ccd21
Tero Heikkinen <tero.heikkinen@qt.io>
2026-02-12 08:48:03 +0200

    Fix incorrect external link for ninja package for x64 and arm
    x64: ninja-linux.zip
    arm: ninja-linux-aarch64.zip
    
    Pick-to: 6.8 6.5
    Fixes: QTQAINFRA-7701
    Change-Id: I36465f6413ae80754f4fa39abb04ce492ed445b7
    Reviewed-by: Simo Fält <simo.falt@qt.io>
    (cherry picked from commit 3480c00adbd864fb1cfa120854982d199c575093)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 87b82c5a62a00e3007aa2d83b138039ea0c495df)

--- coin/provisioning/common/linux/install-ninja.sh
+++ coin/provisioning/common/linux/install-ninja.sh
@@ -15,18 +15,20 @@ case "$uname_m" in
     x86_64|amd64)
         sha256="6f98805688d19672bd699fbbfa2c2cf0fc054ac3df1f0e6a47664d963d530255"
         pkgname="ninja-$version-linux-x64.zip"
+        pkgnameExt="ninja-linux.zip"
         dirname="ninja-$version-linux-x64"
         ;;
     arm64|aarch64)
         sha256="5c25c6570b0155e95fce5918cb95f1ad9870df5768653afe128db822301a05a1"
         pkgname="ninja-$version-linux-arm64.zip"
+        pkgnameExt="ninja-linux-aarch64.zip"
         dirname="ninja-$version-linux-arm64"
         ;;
     *) fatal "Unknown architecture in uname: $uname_m" 43 ;;
 esac
 
 internalUrl="http://ci-files01-hki.ci.qt.io/input/ninja/$pkgname"
-externalUrl="https://github.com/ninja-build/ninja/releases/download/v$version/$pkgname"
+externalUrl="https://github.com/ninja-build/ninja/releases/download/v$version/$pkgnameExt"
 
 targetFile="$HOME/$pkgname"
 DownloadURL "$internalUrl" "$externalUrl" "$sha256" "$targetFile"
