3bf43a0827e4242adbd58298856419f0e348aaa2
Simo Fält <simo.falt@qt.io>
2026-03-11 14:48:55 +0200

    Provisioning: Install libicu to /usr/local on Ubuntu 24.04 x86_64
    Build configs are not using correct version from the /opt/icu.
    
    Pick-to: 6.11
    Task-number: QTBRIDGES-172
    Change-Id: Ic90560036067de1e9d324adfb0c0e765fc521a90
    Reviewed-by: Tero Heikkinen <tero.heikkinen@qt.io>

--- coin/provisioning/qtci-linux-Ubuntu-24.04-x86_64/30-install_icu.sh
+++ coin/provisioning/qtci-linux-Ubuntu-24.04-x86_64/30-install_icu.sh
@@ -10,8 +10,8 @@ set -ex
 # This script will install ICU
 
 icuVersion="73.2"
-icuLocationLib="/opt/icu/lib64"
-icuLocationInclude="/opt/icu/include"
+icuLocationLib="/usr/local/lib"
+icuLocationInclude="/usr/local/include"
 sha1="5699987afcceb0390e52fb860bb3b4ab8b39cabe"
 baseBinaryPackageURL="http://ci-files01-hki.ci.qt.io/input/icu/$icuVersion/icu-linux-g++-Ubuntu22.04-x64.7z"
 baseBinaryPackageExternalURL="http://master.qt.io/development_releases/prebuilt/icu/prebuilt/$icuVersion/icu-linux-g++-Ubuntu22.04-x64.7z"
@@ -20,7 +20,7 @@ sha1Dev="6b9da2fa5fd88db88e9957ee5e3cf9dbcd08fe6b"
 develPackageURL="http://ci-files01-hki.ci.qt.io/input/icu/$icuVersion/icu-linux-g++-Ubuntu22.04-x64-devel.7z"
 develPackageExternalURL="http://master.qt.io/development_releases/prebuilt/icu/prebuilt/$icuVersion/icu-linux-g++-Ubuntu22.04-x64-devel.7z"
 
-echo "Installing custom ICU $icuVersion $sha1 packages on CentOS to $icuLocationLib"
+echo "Installing custom ICU $icuVersion $sha1 packages on Ubuntu to $icuLocationLib"
 
 targetFile=$(mktemp)
 sudo mkdir -p "$icuLocationLib"
@@ -29,7 +29,7 @@ DownloadURL "$baseBinaryPackageURL" "$baseBinaryPackageExternalURL" "$sha1" "$ta
 sudo 7z x -y -o$icuLocationLib "$targetFile"
 sudo rm "$targetFile"
 
-echo "Installing custom ICU devel packages on CentOS"
+echo "Installing custom ICU devel packages on Ubuntu"
 
 tempDir=$(mktemp -d)
 
@@ -38,7 +38,8 @@ DownloadURL "$develPackageURL" "$develPackageExternalURL" "$sha1Dev" "$targetFil
 7z x -y -o"$tempDir" "$targetFile"
 
 sudo cp -a "$tempDir"/lib/* "$icuLocationLib"
-sudo cp -a "$tempDir"/* /opt/icu/
+sudo cp -a "$tempDir"/include/* "$icuLocationInclude"
+
 
 sudo rm "$targetFile"
 sudo rm -fr "$tempDir"
