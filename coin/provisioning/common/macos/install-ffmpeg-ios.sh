cce1fcb98698e1f3212816c950d0d441c5492489
Nils Petter Skålerud <nils.petter.skalerud@qt.io>
2026-03-12 15:17:52 +0100

    FFmpeg, iOS: Don't pass '--enable-shared' and '--disable-static' twice
    These flags are already provided by the function
    'get_ffmpeg_config_options'. In the current we pass them again
    explicitly during FFmpeg configuration.
    
    This patch makes us rely on the function's return value.
    
    Pick-to: 6.8
    Change-Id: I4b257331cedafd7241df4360694e4ad1637267fe
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    (cherry picked from commit 16af59b8aca8a53843f01fca8afa5f223bd7e343)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- coin/provisioning/common/macos/install-ffmpeg-ios.sh
+++ coin/provisioning/common/macos/install-ffmpeg-ios.sh
@@ -32,7 +32,8 @@ if [ ! -n "$ffmpeg_version" ]; then
     echo "Error. Unable to determine FFmpeg version."
     exit 1
 fi
-ffmpeg_config_options=$(get_ffmpeg_config_options "shared")
+ffmpeg_build_type="shared"
+ffmpeg_config_options=$(get_ffmpeg_config_options "$ffmpeg_build_type")
 default_prefix="/usr/local/ios/ffmpeg"
 prefix="${1:-$default_prefix}"
 
@@ -100,8 +101,6 @@ build_ffmpeg_ios() {
         --extra-cxxflags="${common_arch_flags}"
         --extra-ldflags="${common_arch_flags}"
         --target-os=darwin
-        --enable-shared
-        --disable-static
         --install-name-dir="@rpath"
         --disable-audiotoolbox
 
