8804b39aa5c125f14cba1bbeeff5a99fd93ec4db
Tony Sarajärvi <tony.sarajarvi@qt.io>
2026-03-24 07:45:22 +0000

    Update llvm-mingw to 22.1.7
    Also have to update libclang-llvmorg to a version built with llvm-mingw 22.1.7
    
    Pick-to: 6.12
    Task-number: QTQAINFRA-7552
    Change-Id: I7e579eacb146718b34ec486598323fa85cf3bfb4
    Reviewed-by: Tim Blechmann <tim.blechmann@qt.io>
    Reviewed-by: Simo Fält <simo.falt@qt.io>

--- coin/provisioning/qtci-windows-11_23H2-aarch64/25-llvm-mingw.ps1
+++ coin/provisioning/qtci-windows-11_23H2-aarch64/25-llvm-mingw.ps1
@@ -1,4 +1,4 @@
-# Copyright (C) 2024 The Qt Company Ltd.
+# Copyright (C) 2026 The Qt Company Ltd.
 # SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 . "$PSScriptRoot\..\common\windows\helpers.ps1"
@@ -6,15 +6,15 @@
 # This script installs LLVM-Mingw by mstorsjo
 # https://github.com/mstorsjo/llvm-mingw/releases/tag/20240320
 
-$zip = Get-DownloadLocation "llvm-mingw-20240320-ucrt-aarch64.zip"
-$url_cache = "http://ci-files01-hki.ci.qt.io/input/windows/llvm-mingw-20240320-ucrt-aarch64.zip"
-$url_official = "https://github.com/mstorsjo/llvm-mingw/releases/download/20240320/llvm-mingw-20240320-ucrt-aarch64.zip"
+$zip = Get-DownloadLocation "llvm-mingw-20260602-ucrt-aarch64.zip"
+$url_cache = "http://ci-files01-hki.ci.qt.io/input/windows/llvm-mingw-20260602-ucrt-aarch64.zip"
+$url_official = "https://github.com/mstorsjo/llvm-mingw/releases/download/20260602/llvm-mingw-20260602-ucrt-aarch64.zip"
 
 Download $url_official $url_cache $zip
-Verify-Checksum $zip "1ea4870551a6aaf0d51332be1ea10ce776ee3b42"
+Verify-Checksum $zip "fc4c67cb7e8f96c1bfbad6904ac9a5ce04aaeff3"
 Extract-7Zip $zip C:\
 
-Rename-Item C:\llvm-mingw-20240320-ucrt-aarch64 C:\llvm-mingw
+Rename-Item C:\llvm-mingw-20260602-ucrt-aarch64 C:\llvm-mingw
 
-Write-Output "llvm-mingw = 18.1.2" >> ~/versions.txt
+Write-Output "llvm-mingw = 22.1.7" >> ~/versions.txt
 Remove-Item -Path $zip
--- coin/provisioning/qtci-windows-11_24H2-x86_64/25-llvm-mingw.ps1
+++ coin/provisioning/qtci-windows-11_24H2-x86_64/25-llvm-mingw.ps1
@@ -1,7 +1,4 @@
-# Copyright (C) 2025 The Qt Company Ltd
-# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
-
-# Copyright (C) 2022 The Qt Company Ltd.
+# Copyright (C) 2026 The Qt Company Ltd
 # SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 . "$PSScriptRoot\..\common\windows\helpers.ps1"
@@ -9,15 +6,15 @@
 # This script installs LLVM-Mingw by mstorsjo
 # https://github.com/mstorsjo/llvm-mingw/releases/tag/20220323
 
-$zip = Get-DownloadLocation "llvm-mingw-20231017-ucrt-x86_64.zip"
-$url_cache = "http://ci-files01-hki.ci.qt.io/input/windows/llvm-mingw-20231017-ucrt-x86_64.zip"
-$url_official = "https://github.com/mstorsjo/llvm-mingw/releases/download/20231017/llvm-mingw-20231017-ucrt-x86_64.zip"
+$zip = Get-DownloadLocation "llvm-mingw-20260602-ucrt-x86_64.zip"
+$url_cache = "http://ci-files01-hki.ci.qt.io/input/windows/llvm-mingw-20260602-ucrt-x86_64.zip"
+$url_official = "https://github.com/mstorsjo/llvm-mingw/releases/download/20260602/llvm-mingw-20260602-ucrt-x86_64.zip"
 
 Download $url_official $url_cache $zip
-Verify-Checksum $zip "1a39032b5add2f06fe0dfa56d0279b313b974da3"
+Verify-Checksum $zip "90ec03103d62573d994598a27b914e01e4902a21"
 Extract-7Zip $zip C:\
 
-Rename-Item C:\llvm-mingw-20231017-ucrt-x86_64 C:\llvm-mingw
+Rename-Item C:\llvm-mingw-20260602-ucrt-x86_64 C:\llvm-mingw
 
-Write-Output "llvm-mingw = 17.0.3" >> ~/versions.txt
+Write-Output "llvm-mingw = 22.1.7" >> ~/versions.txt
 Remove-Item -Path $zip
