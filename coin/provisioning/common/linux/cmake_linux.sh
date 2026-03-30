c73ed0d8d57a72b165be7ad6e03c89bdeacf88d0
Alexandru Croitor <alexandru.croitor@qt.io>
2026-03-30 16:51:58 +0200

    CMake: Update provisioned CMake to version 4.3.2
    Update provisioned cmake to 4.3.2.
    
    Adjust the linux provisioning script to be architecture-aware.
    
    Add the script to the provisioning of the Ubuntu-24.04-aarch64
    platform, which was previously missing it and instead relied on the
    system 3.28.3 CMake.
    
    Pick-to: 6.11 6.8
    Fixes: QTQAINFRA-7785
    Change-Id: Ib2b6a55d7b994c95299ddf66546d9362b04823df
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>
    (cherry picked from commit f408aa690a05e9d24f72351b006c89faa573e07b)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- coin/provisioning/common/linux/cmake_linux.sh
+++ coin/provisioning/common/linux/cmake_linux.sh
@@ -11,13 +11,26 @@ source "${BASH_SOURCE%/*}/../unix/InstallFromCompressedFileFromURL.sh"
 # shellcheck source=../unix/SetEnvVar.sh
 source "${BASH_SOURCE%/*}/../unix/SetEnvVar.sh"
 
-majorminorversion="3.30"
-version="3.30.5"
-PrimaryUrl="http://ci-files01-hki.ci.qt.io/input/cmake/cmake-$version-linux-x86_64.tar.gz"
-AltUrl="https://cmake.org/files/v$majorminorversion/cmake-$version-linux-x86_64.tar.gz"
-SHA1="61b339cac5f110d5ed2934f66a26c2e991945e01"
+majorminorversion="4.3"
+version="4.3.2"
+
+uname_m="$(uname -m)"
+case "$uname_m" in
+    x86_64|amd64)
+        SHA1="736c0722ed8393b3eb74fd573f6c9c245770206a"
+        arch="x86_64"
+        ;;
+    arm64|aarch64)
+        SHA1="a173eb910c808f561422feb45fe1fe57c3e346fa"
+        arch="aarch64"
+        ;;
+    *) fatal "Unknown architecture in uname: $uname_m" 43 ;;
+esac
+
+PrimaryUrl="http://ci-files01-hki.ci.qt.io/input/cmake/cmake-$version-linux-$arch.tar.gz"
+AltUrl="https://cmake.org/files/v$majorminorversion/cmake-$version-linux-$arch.tar.gz"
 targetFolder="/opt/cmake-$version"
-appPrefix="cmake-$version-linux-x86_64"
+appPrefix="cmake-$version-linux-$arch"
 
 InstallFromCompressedFileFromURL "$PrimaryUrl" "$AltUrl" "$SHA1" "$targetFolder" "$appPrefix"
 
