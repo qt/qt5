e1b2031f689a4da0df760b9becb214509ef797d4
Tero Heikkinen <tero.heikkinen@qt.io>
2026-04-07 08:33:01 +0300

    Fix openSUSE 16.0 dependency issue with cargo 1.94 installation
    Task-number: QTQAINFRA-7793
    Change-Id: Ia5ad818dc599317b7f46697732fa9434d5a0eace
    Reviewed-by: Tony Sarajärvi <tony.sarajarvi@qt.io>
    (cherry picked from commit daecc2a3f245511ec4ce33474d5366195dd3dfae)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- coin/provisioning/qtci-linux-openSUSE-16.0-x86_64/04-zypperpackages.sh
+++ coin/provisioning/qtci-linux-openSUSE-16.0-x86_64/04-zypperpackages.sh
@@ -46,7 +46,8 @@ sudo zypper -nq install libicu-devel
 sudo zypper -nq install alsa-devel dbus-1-devel libxkbfile-devel \
          libXcomposite-devel libXcursor-devel libXrandr-devel libXtst-devel \
          mozilla-nspr-devel mozilla-nss-devel glproto-devel nodejs \
-         libxshmfence-devel libXdamage-devel cargo rust-bindgen
+         libxshmfence-devel libXdamage-devel \
+         'cargo<1.94' rust-bindgen
 
 # qtwebkit
 sudo zypper -nq install libxml2-devel libxslt-devel
