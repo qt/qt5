18f42f3a87d158c0a60800c7bc7b57caba43e27f
Tuomas Vaarala <tuomas.vaarala@qt.io>
2026-04-22 09:41:28 +0300

    Coin: QNX 8.0: add Windows provisioning script
    Mirror the existing Windows qnx710.ps1 for the 20260410 SDP
    patchset and pull the 2026a tzdata overlay alongside it, same
    pattern as the Linux common/linux/qnx_800.sh script.
    
    Pick-to: 6.10 6.11
    Change-Id: I5b718de8274ba2e1dc174199736c26ecdb5ea44e
    Reviewed-by: Tony Sarajärvi <tony.sarajarvi@qt.io>

---
+++ coin/provisioning/common/windows/qnx800.ps1
@@ -0,0 +1,64 @@
+# Copyright (C) 2026 The Qt Company Ltd.
+# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+# This script installs QNX 8.
+
+. "$PSScriptRoot\helpers.ps1"
+
+$tempFolder = "c:\Windows\Temp"
+
+function DownloadAndExtract($1, $2, $3, $4, $5) {
+    $url = $1
+    $sha = $2
+    $file = $3
+    $folder = $4
+    $cachedUrl = $5
+
+    Download $url $cachedUrl "$tempFolder\$file"
+    Verify-Checksum "$tempFolder\$file" "$sha"
+    Extract-tar_gz "$tempFolder\$file" $folder
+}
+
+$aarch64le_toolchain = "$PSScriptRoot\..\shared\cmake_toolchain_files\qnx-toolchain-aarch64le.cmake"
+$armv7le_toolchain = "$PSScriptRoot\..\shared\cmake_toolchain_files\qnx-toolchain-armv7le.cmake"
+$x8664_toolchain = "$PSScriptRoot\..\shared\cmake_toolchain_files\qnx-toolchain-x8664.cmake"
+
+$targetFolder = "C:\Utils\"
+$folderName = "qnx800"
+$targetPath = "$targetFolder$folderName"
+
+# QNX SDP
+$cachedUrl = "\\ci-files01-hki.ci.qt.io\provisioning\qnx\qnx800-windows-linux-20260410.tar.xz"
+$sourceFile = "http://ci-files01-hki.ci.qt.io/input/qnx/qnx800-windows-linux-20260410.tar.xz"
+$targetFile = "qnx800.tar.xz"
+$sha1 = "a3fdcd98e12bc5054d00f18e4088af26288fd020"
+DownloadAndExtract $sourceFile $sha1 $targetFile $targetFolder $cachedUrl
+
+# IANA timezone database overlay. Packaged separately from the SDP so it can be
+# bumped without rebuilding the SDP tarball. Archive root is qnx800/target/qnx/
+# so it overlays onto $targetFolder (C:\Utils\) directly.
+$cachedUrl = "\\ci-files01-hki.ci.qt.io\provisioning\qnx\zoneinfo-2026a.tar.xz"
+$sourceFile = "http://ci-files01-hki.ci.qt.io/input/qnx/zoneinfo-2026a.tar.xz"
+$targetFile = "zoneinfo.tar.xz"
+$sha1 = "8c1678ff673bb588f63fc9277497cd38e0ea2253"
+DownloadAndExtract $sourceFile $sha1 $targetFile $targetFolder $cachedUrl
+
+Copy-Item $aarch64le_toolchain $targetPath
+Copy-Item $armv7le_toolchain $targetPath
+Copy-Item $x8664_toolchain $targetPath
+
+cmd /c "dir $targetPath"
+
+# Verify that we have last files in tars
+if (-not (test-path $targetPath\qnxsdp-env.bat)) {
+    throw "QNX SDP installation failed!"
+}
+if (-not (test-path $targetPath\qnx-toolchain-x8664.cmake)) {
+    throw "QNX toolchain installation failed!"
+}
+
+# Set env variables
+Set-EnvironmentVariable "QNX_800" "$targetPath"
+Set-EnvironmentVariable "QNX_800_CMAKE" "C:/Utils/$folderName"
+
+Write-Output "QNX SDP = 8.0.0" >> ~\versions.txt
