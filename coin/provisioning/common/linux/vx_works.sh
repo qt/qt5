4d6a473bed76de0c99f357c30d76b7e99c2fed58
Karim Pinter <karim.pinter@qt.io>
2026-04-30 15:37:56 +0300

    Update coin to VxWorks 26.03
    VSB and VIP updated to VxWorks 26.03. NFS updated to v3 UDP only.
    Qt home, cache, tmp directory moved to ramfs with HRFS formatting.
    
    Task-number: QTBUG-145458
    Change-Id: I954bac54584797d77429937a2e3edec6da5a73fd
    Pick-to: 6.12
    Reviewed-by: Simo Fält <simo.falt@qt.io>

--- coin/provisioning/common/linux/vx_works.sh
+++ coin/provisioning/common/linux/vx_works.sh
@@ -22,51 +22,51 @@ SetEnvVar "VXWORKS_HOME" "/opt/vxworks"
 SetEnvVar "VXWORKS_SSH" "WindRiver@172.31.1.10"
 
 # IMX 6
-VXWORKS_BUILD_VER="16-04-2025"
+VXWORKS_BUILD_VER="12-05-2026_2"
 ######### VXworks toolchain #########
-# Installs to /opt/fsl_imx6_2_0_6_2_VSB
+# Installs to /opt/fsl_imx6_2_0_6_3_VSB
 PrimaryUrl="http://ci-files01-hki.ci.qt.io/input/vxworks/vxworks_arm_vsb_${VXWORKS_BUILD_VER}.tar.gz"
 AltUrl=""
-sha1="88f0f139ac62e8c0a4c5477d880a1ff345826479"
+sha1="a1553f377ae84a722c8ad258fccb684f9198717c"
 targetFolder="/opt/"
 InstallFromCompressedFileFromURL "$PrimaryUrl" "$AltUrl" "$sha1" "$targetFolder" ""
-SetEnvVar "WIND_CC_SYSROOT" "/opt/fsl_imx6_2_0_6_2_VSB"
+SetEnvVar "WIND_CC_SYSROOT" "/opt/fsl_imx6_2_0_6_3_VSB"
 
 ######### VXworks VIP kernel #########
-# Installs to /opt/fsl_imx6_2_0_6_2_VIP_QEMU
+# Installs to /opt/fsl_imx6_2_0_6_3_VIP_QEMU
 PrimaryUrl="http://ci-files01-hki.ci.qt.io/input/vxworks/vxworks_arm_vip_${VXWORKS_BUILD_VER}.tar.gz"
 AltUrl=""
-sha1="96056c0f9b1234160db2d9038a9e6fc24a2bbbd7"
+sha1="487a056d389a764717142100fb408e65ea720f92"
 targetFolder="/opt/"
 InstallFromCompressedFileFromURL "$PrimaryUrl" "$AltUrl" "$sha1" "$targetFolder" ""
 
 # IMX 8
-# Installs to /opt/nxp_imx8_1_0_6_3_VSB
+# Installs to /opt/nxp_imx8_1_0_7_1_VSB
 PrimaryUrl="http://ci-files01-hki.ci.qt.io/input/vxworks/vxworks_arm_imx8_vsb_${VXWORKS_BUILD_VER}.tar.gz"
 AltUrl=""
-sha1="cf6194b78bf08b894397fd5ce2a57abd25cf497b"
+sha1="b312f38a7ca670398c940c25d20ed2a26155160d"
 targetFolder="/opt/"
 InstallFromCompressedFileFromURL "$PrimaryUrl" "$AltUrl" "$sha1" "$targetFolder" ""
 
 # Installs to /opt/itl_generic_skylake_VSB
 PrimaryUrl="http://ci-files01-hki.ci.qt.io/input/vxworks/vxworks_intel_vsb_${VXWORKS_BUILD_VER}.tar.gz"
 AltUrl=""
-sha1="4c75222364d3907db9fb1605cabe0c92616d9ec9"
+sha1="9fddd5aa0b9599113c22670fcc1221004978ee8a"
 targetFolder="/opt/"
 InstallFromCompressedFileFromURL "$PrimaryUrl" "$AltUrl" "$sha1" "$targetFolder" ""
 # Installs to /opt/itl_generic_skylake_VIP_QEMU
 PrimaryUrl="http://ci-files01-hki.ci.qt.io/input/vxworks/vxworks_intel_vip_${VXWORKS_BUILD_VER}.tar.gz"
 AltUrl=""
-sha1="c3af7da92832ea71201dcf1022d7404966291f5c"
+sha1="cb366addbea7a1fd34aa49b739dcc0b480350fce"
 targetFolder="/opt/"
 InstallFromCompressedFileFromURL "$PrimaryUrl" "$AltUrl" "$sha1" "$targetFolder" ""
 
 ######### VXworks fonts and certs #########
-# Installs to /opt/fsl_imx6_2_0_6_2_VSB
+# Installs to /opt/fsl_imx6_2_0_6_3_VSB
 PrimaryUrl="http://ci-files01-hki.ci.qt.io/input/vxworks/vxworks_misc.tar.gz"
 AltUrl=""
 sha1="1bc529b90b35b0b249f219e47d5798225a9b68d8"
-targetFolder="/opt/fsl_imx6_2_0_6_2_VSB/"
+targetFolder="/opt/fsl_imx6_2_0_6_3_VSB/"
 InstallFromCompressedFileFromURL "$PrimaryUrl" "$AltUrl" "$sha1" "$targetFolder" ""
 ######### VXworks fonts and certs #########
 # Installs to /opt/itl_generic_skylake_VSB
@@ -75,13 +75,6 @@ AltUrl=""
 sha1="1bc529b90b35b0b249f219e47d5798225a9b68d8"
 targetFolder="/opt/itl_generic_skylake_VSB/"
 InstallFromCompressedFileFromURL "$PrimaryUrl" "$AltUrl" "$sha1" "$targetFolder" ""
-
-# Setup NFS exports that are needed by VxWorks qemu
-sudo bash -c "echo '/home/qt/work 172.31.1.10/24(rw,sync,root_squash,no_subtree_check,anonuid=2001,anongid=100)' >> /etc/exports"
-sudo bash -c "echo '/opt/fsl_imx6_2_0_6_2_VSB 172.31.1.10/24(rw,sync,root_squash,no_subtree_check,anonuid=2001,anongid=100)' >> /etc/exports"
-sudo bash -c "echo '/opt/itl_generic_skylake_VSB 172.31.1.10/24(rw,sync,root_squash,no_subtree_check,anonuid=2001,anongid=100)' >> /etc/exports"
-
-sudo exportfs -a
 
 # Enable ipv4 routing from vxWorks to Qt DNS
 sudo sed -i s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g /etc/sysctl.conf
