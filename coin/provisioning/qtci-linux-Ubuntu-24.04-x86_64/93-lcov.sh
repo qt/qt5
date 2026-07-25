6f4085c84d1e5ca8de7fb69edd471f9fa496e92a
Robert Griebl <robert.griebl@qt.io>
2026-07-25 13:37:49 +0200

    Add an Ubuntu 24.04 lcov configuration
    Also update lcov to a modern 2.4 release.
    
    Change-Id: Iea14af47a3dbd509fdd735e60f314c3101bbc3b4
    Reviewed-by: Simo Fält <simo.falt@qt.io>

--- coin/provisioning/qtci-linux-Ubuntu-24.04-x86_64/93-lcov.sh
+++ coin/provisioning/qtci-linux-Ubuntu-24.04-x86_64/93-lcov.sh
@@ -2,8 +2,8 @@
 # Copyright (C) 2024 The Qt Company Ltd.
 # SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
-# Install lcov 2.0 from 24.04/noble numbat
+# Install lcov 2.4 from 26.04/resolute raccoon
 
 echo "Installing lcov"
-wget https://ci-files01-hki.ci.qt.io/input/lcov/lcov_2.0-4ubuntu1_all.deb
-sudo DEBIAN_FRONTEND=noninteractive apt-get -q -y -o DPkg::Lock::Timeout=300 install -f ./lcov_2.0-4ubuntu1_all.deb
+wget https://ci-files01-hki.ci.qt.io/input/lcov/lcov_2.4-3_all.deb
+sudo DEBIAN_FRONTEND=noninteractive apt-get -q -y -o DPkg::Lock::Timeout=300 install -f ./lcov_2.4-3_all.deb
