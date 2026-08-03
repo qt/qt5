065d6d06d996509c60f5e7dda398ea10668a451b
Elias Toivola <elias.toivola@qt.io>
2026-08-03 12:18:12 +0000

    Revert^2 "Provisioning: Take repo-clones into use in RHEL 9"
    This reverts commit 72a7e5c4dbb140f1f6c87d26ca29edbbd2e8a1b5.
    
    Reason for revert: Minor version specific internal repos are now
    available
    
    Pick-to: 6.12 6.11 6.8
    Task-number: QTQAINFRA-7810
    Change-Id: Ibdf7d847929175fce2aa3a1b4473695346821a96
    Reviewed-by: Simo Fält <simo.falt@qt.io>

--- coin/provisioning/qtci-linux-RHEL-9.4-x86_64/03-enable-repos.sh
+++ coin/provisioning/qtci-linux-RHEL-9.4-x86_64/03-enable-repos.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Copyright (C) 2023 The Qt Company Ltd.
+# Copyright (C) 2026 The Qt Company Ltd.
 # SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 set -ex
@@ -14,7 +14,7 @@ while sudo fuser /usr/libexec/packagekitd >/dev/null 2>&1; do
 done
 sudo yum -y remove PackageKit gnome-software
 
-sudo subscription-manager config --rhsm.manage_repos=1
+sudo subscription-manager config --rhsm.manage_repos=0
 sudo subscription-manager refresh
 
 # List available RHEL versions and bind with correct one
@@ -22,10 +22,35 @@ sudo subscription-manager release --list
 sudo subscription-manager release --set=9.4
 sudo yum clean all
 
-# sudo yum config-manager --enable 'rhceph-4-tools-for-rhel-8-x86_64-rpms'
-sudo yum config-manager --enable 'codeready-builder-for-rhel-9-x86_64-rpms'
-sudo yum config-manager --enable 'rhel-9-for-x86_64-baseos-rpms'
-sudo yum config-manager --enable 'rhel-9-for-x86_64-appstream-rpms'
+sudo tee "/etc/yum.repos.d/local.repo" > /dev/null <<EOC
+[rhel-9-for-x86_64-baseos-rpms]
+metadata_expire = 86400
+baseurl = http://repo-clones-rhel9.ci.qt.io/rhel-9-for-x86_64-baseos-rpms
+ui_repoid_vars = releasever basearch
+name = Qt Red Hat Enterprise Linux 9 Base OS (RPMs)
+gpgkey = file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release
+enabled = 1
+gpgcheck = 1
+
+[rhel-9-for-x86_64-appstream-rpms]
+metadata_expire = 86400
+baseurl = http://repo-clones-rhel9.ci.qt.io/rhel-9-for-x86_64-appstream-rpms
+ui_repoid_vars = releasever basearch
+name = Qt Red Hat Enterprise Linux 9 Appstream (RPMs)
+gpgkey = file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release
+enabled = 1
+gpgcheck = 1
+
+[codeready-builder-for-rhel-9-x86_64-rpms]
+metadata_expire = 86400
+baseurl = http://repo-clones-rhel9.ci.qt.io/codeready-builder-for-rhel-9-x86_64-rpms
+ui_repoid_vars = releasever basearch
+name = Qt Red Hat Enterprise Linux Codeready Builder (RPMs)
+gpgkey = file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release
+enabled = 1
+gpgcheck = 1
+EOC
+
 # Epel is required for 'double-conversion-devel', 'libsqlite3x' and 'p7zip'
 sudo dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
 
